074a1040923ee49e1dc11a1e132c6726a6505743
Raúl Kripalani <raul@protocol.ai>
2022-02-20 14:54:44 +0100

    implement a bundler utility to generate CAR files containing Wasm bytecode.

--- bundle/src/lib.rs
+++ bundle/src/lib.rs
@@ -1,9 +1,10 @@
-/// This constant inlines the CAR bundle containing the bytecode for
-/// canonical actors.
-///
-/// The CAR contains a multiroot header, enumerating the CIDs of the
-/// bytecode blocks that follow.
-///
-/// For now, each bytecode entry is a single IPLD slab; content is not chunked
-/// or laid out in a DAG.
-pub const BUNDLE_CAR: &[u8] = include_bytes!(concat!(env!("OUT_DIR"), "/bundle.car"));
+// TODO
+// This constant inlines the CAR bundle containing the bytecode for
+// canonical actors.
+//
+// The CAR contains a multiroot header, enumerating the CIDs of the
+// bytecode blocks that follow.
+//
+// For now, each bytecode entry is a single IPLD slab; content is not chunked
+// or laid out in a DAG.
+// pub const BUNDLE_CAR: &[u8] = include_bytes!(concat!(env!("OUT_DIR"), "/bundle.car"));
