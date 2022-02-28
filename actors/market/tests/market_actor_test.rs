11e6efbdea18c4f6e0ed36070d3c4db8c37e7dcb
Raúl Kripalani <raul@protocol.ai>
2022-02-28 19:39:31 +0000

    build actors as cdylib and lib; fix clippy.

--- actors/market/tests/market_actor_test.rs
+++ actors/market/tests/market_actor_test.rs
@@ -185,7 +185,7 @@ fn add_non_provider_funds() {
 
             let amount = TokenAmount::from(test_case.0 as u64);
             rt.set_value(amount);
-            rt.expect_validate_caller_type(*CALLER_TYPES_SIGNABLE);
+            rt.expect_validate_caller_type((*CALLER_TYPES_SIGNABLE).clone());
 
             assert!(rt
                 .call::<MarketActor>(
