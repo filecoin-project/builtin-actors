bb5abe172d783b6986299431991f1b0d6b451f5d
zenground0 <ZenGround0@users.noreply.github.com>
2022-03-04 12:45:31 -0700

    Lint

--- actors/multisig/tests/multisig_actor_test.rs
+++ actors/multisig/tests/multisig_actor_test.rs
@@ -1,19 +1,12 @@
-
-
 // use cid::Cid;
-use fil_actor_multisig::{
-    Actor as MultisigActor, ConstructorParams, Method,
-};
+use fil_actor_multisig::{Actor as MultisigActor, ConstructorParams, Method};
 use fil_actors_runtime::test_utils::*;
-use fil_actors_runtime::{
-    SYSTEM_ACTOR_ADDR, INIT_ACTOR_ADDR,
-};
+use fil_actors_runtime::{INIT_ACTOR_ADDR, SYSTEM_ACTOR_ADDR};
 use fvm_shared::address::Address;
 // use fvm_shared::econ::TokenAmount;
 use fvm_shared::encoding::RawBytes;
 use fvm_shared::error::ExitCode;
 // use serde::Serialize;
-
 
 fn construct_runtime() -> MockRuntime {
     MockRuntime {
@@ -30,7 +23,7 @@ fn test_construction_fail_to_construct_multisig_actor_with_0_signers() {
     let zero_signer_params = ConstructorParams {
         signers: Vec::new(),
         num_approvals_threshold: 1,
-        unlock_duration: 1, 
+        unlock_duration: 1,
         start_epoch: 0,
     };
     rt.expect_validate_caller_addr(vec![*INIT_ACTOR_ADDR]);
@@ -48,5 +41,4 @@ fn test_construction_fail_to_construct_multisig_actor_with_0_signers() {
         ExitCode::ErrIllegalArgument,
         "Exit Code that is returned is not ErrIllegalArgument"
     );
-
 }
