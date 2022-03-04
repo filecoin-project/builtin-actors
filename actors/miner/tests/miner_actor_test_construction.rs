fc20a5abbc645b6d6b60bf861df679430c9f311d
vyzo <vyzo@hackzen.org>
2022-03-04 18:43:31 +0200

    rustfmt

--- actors/miner/tests/miner_actor_test_construction.rs
+++ actors/miner/tests/miner_actor_test_construction.rs
@@ -1,18 +1,18 @@
 use fil_actors_runtime::test_utils::*;
 use fil_actors_runtime::INIT_ACTOR_ADDR;
 
-use fil_actor_miner::{Actor, State, MinerConstructorParams as ConstructorParams, Method,
-                      Deadlines, Deadline,
-                      WPOST_CHALLENGE_WINDOW,
-                      WPOST_PERIOD_DEADLINES};
-use fil_actor_account::{Method as AccountMethod};
+use fil_actor_account::Method as AccountMethod;
+use fil_actor_miner::{
+    Actor, Deadline, Deadlines, Method, MinerConstructorParams as ConstructorParams, State,
+    WPOST_CHALLENGE_WINDOW, WPOST_PERIOD_DEADLINES,
+};
 
 use fvm_shared::address::Address;
-use fvm_shared::encoding::{blake2b_256, BytesDe, RawBytes};
-use fvm_shared::sector::{RegisteredPoStProof,SectorSize};
-use fvm_shared::econ::TokenAmount;
-use fvm_shared::error::ExitCode;
 use fvm_shared::blockstore::CborStore;
+use fvm_shared::econ::TokenAmount;
+use fvm_shared::encoding::{blake2b_256, BytesDe, RawBytes};
+use fvm_shared::error::ExitCode;
+use fvm_shared::sector::{RegisteredPoStProof, SectorSize};
 
 use cid::Cid;
 
@@ -36,17 +36,25 @@ fn prepare_env() -> TestEnv {
         owner: Address::new_id(100),
         worker: Address::new_id(101),
         worker_key: util::new_bls_addr(0),
-        control_addrs: vec!(Address::new_id(999), Address::new_id(998)),
-        peer_id: vec!(1,2,3),
-        multiaddrs: vec!(BytesDe(vec!(1,2,3))),
+        control_addrs: vec![Address::new_id(999), Address::new_id(998)],
+        peer_id: vec![1, 2, 3],
+        multiaddrs: vec![BytesDe(vec![1, 2, 3])],
         rt: MockRuntime::default(),
     };
 
     env.rt.receiver = env.receiver;
-    env.rt.actor_code_cids.insert(env.owner, *ACCOUNT_ACTOR_CODE_ID);
-    env.rt.actor_code_cids.insert(env.worker, *ACCOUNT_ACTOR_CODE_ID);
-    env.rt.actor_code_cids.insert(env.control_addrs[0], *ACCOUNT_ACTOR_CODE_ID);
-    env.rt.actor_code_cids.insert(env.control_addrs[1], *ACCOUNT_ACTOR_CODE_ID);
+    env.rt
+        .actor_code_cids
+        .insert(env.owner, *ACCOUNT_ACTOR_CODE_ID);
+    env.rt
+        .actor_code_cids
+        .insert(env.worker, *ACCOUNT_ACTOR_CODE_ID);
+    env.rt
+        .actor_code_cids
+        .insert(env.control_addrs[0], *ACCOUNT_ACTOR_CODE_ID);
+    env.rt
+        .actor_code_cids
+        .insert(env.control_addrs[1], *ACCOUNT_ACTOR_CODE_ID);
     env.rt.hash_func = Box::new(blake2b_256);
     env.rt.caller = *INIT_ACTOR_ADDR;
     env.rt.caller_type = *INIT_ACTOR_CODE_ID;
@@ -69,7 +77,7 @@ fn simple_construction() {
     let mut env = prepare_env();
     let params = constructor_params(&env);
 
-    env.rt.expect_validate_caller_addr(vec!(*INIT_ACTOR_ADDR));
+    env.rt.expect_validate_caller_addr(vec![*INIT_ACTOR_ADDR]);
     env.rt.expect_send(
         env.worker,
         AccountMethod::PubkeyAddress as u64,
@@ -79,7 +87,13 @@ fn simple_construction() {
         ExitCode::Ok,
     );
 
-    let result = env.rt.call::<Actor>(Method::Constructor as u64, &RawBytes::serialize(params).unwrap()).unwrap();
+    let result = env
+        .rt
+        .call::<Actor>(
+            Method::Constructor as u64,
+            &RawBytes::serialize(params).unwrap(),
+        )
+        .unwrap();
     assert_eq!(result.bytes().len(), 0);
     env.rt.verify();
 
@@ -91,7 +105,10 @@ fn simple_construction() {
     assert_eq!(env.control_addrs, info.control_addresses);
     assert_eq!(env.peer_id, info.peer_id);
     assert_eq!(env.multiaddrs, info.multi_address);
-    assert_eq!(RegisteredPoStProof::StackedDRGWindow32GiBV1, info.window_post_proof_type);
+    assert_eq!(
+        RegisteredPoStProof::StackedDRGWindow32GiBV1,
+        info.window_post_proof_type
+    );
     assert_eq!(SectorSize::_32GiB, info.sector_size);
     assert_eq!(2349, info.window_post_partition_sectors);
 
@@ -107,16 +124,21 @@ fn simple_construction() {
     let dl_idx = (env.rt.epoch - proving_period_start) / WPOST_CHALLENGE_WINDOW;
     assert_eq!(dl_idx as u64, state.current_deadline);
 
-    let deadlines = env.rt.store.get_cbor::<Deadlines>(&state.deadlines).unwrap().unwrap();
-     for i in 0..WPOST_PERIOD_DEADLINES {
-         let c = deadlines.due[i as usize];
-         let deadline = env.rt.store.get_cbor::<Deadline>(&c).unwrap().unwrap();
-         assert_ne!(Cid::default(), deadline.partitions);
-         assert_ne!(Cid::default(), deadline.expirations_epochs);
-         assert!(deadline.partitions_posted.is_empty());
-         assert!(deadline.early_terminations.is_empty());
-         assert_eq!(0, deadline.live_sectors);
-     }
+    let deadlines = env
+        .rt
+        .store
+        .get_cbor::<Deadlines>(&state.deadlines)
+        .unwrap()
+        .unwrap();
+    for i in 0..WPOST_PERIOD_DEADLINES {
+        let c = deadlines.due[i as usize];
+        let deadline = env.rt.store.get_cbor::<Deadline>(&c).unwrap().unwrap();
+        assert_ne!(Cid::default(), deadline.partitions);
+        assert_ne!(Cid::default(), deadline.expirations_epochs);
+        assert!(deadline.partitions_posted.is_empty());
+        assert!(deadline.early_terminations.is_empty());
+        assert_eq!(0, deadline.live_sectors);
+    }
 
     assert!(state.early_terminations.is_empty());
 
@@ -124,31 +146,19 @@ fn simple_construction() {
 }
 
 #[test]
-fn control_addresses_are_resolved_during_construction() {
-
-}
+fn control_addresses_are_resolved_during_construction() {}
 
 #[test]
-fn fails_if_control_address_is_not_an_acount_actor() {
-
-}
+fn fails_if_control_address_is_not_an_acount_actor() {}
 
 #[test]
-fn test_construct_with_invalid_peer_id() {
-
-}
+fn test_construct_with_invalid_peer_id() {}
 
 #[test]
-fn fails_if_control_addresses_exceeds_maximum_length() {
-
-}
+fn fails_if_control_addresses_exceeds_maximum_length() {}
 
 #[test]
-fn test_construct_with_large_multiaddr() {
-
-}
+fn test_construct_with_large_multiaddr() {}
 
 #[test]
-fn test_construct_with_empty_multiaddr() {
-
-}
+fn test_construct_with_empty_multiaddr() {}
