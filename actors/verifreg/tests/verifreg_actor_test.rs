59d3346fdf9fde7a5e66a558afacebfff547fed3
anorth <445306+anorth@users.noreply.github.com>
2022-03-16 14:29:03 +1100

    Symbolic constants for common addresses

--- actors/verifreg/tests/verifreg_actor_test.rs
+++ actors/verifreg/tests/verifreg_actor_test.rs
@@ -1,5 +1,6 @@
 #![deny(unused_must_use)] // Force unwrapping Result<_, Err>
 
+use fvm_shared::address::Address;
 use lazy_static::lazy_static;
 
 use fil_actor_verifreg::{DataCap, MINIMUM_VERIFIED_DEAL_SIZE};
@@ -9,6 +10,12 @@ mod harness;
 lazy_static! {
     static ref VERIFIER_ALLOWANCE: DataCap = MINIMUM_VERIFIED_DEAL_SIZE.clone() + DataCap::from(42);
     static ref CLIENT_ALLOWANCE: DataCap = VERIFIER_ALLOWANCE.clone() - DataCap::from(1);
+    static ref VERIFIER: Address = Address::new_id(201);
+    static ref VERIFIER2: Address = Address::new_id(202);
+    static ref CLIENT: Address = Address::new_id(301);
+    static ref CLIENT2: Address = Address::new_id(302);
+    static ref CLIENT3: Address = Address::new_id(303);
+    static ref CLIENT4: Address = Address::new_id(304);
 }
 
 mod construction {
@@ -21,13 +28,13 @@ mod construction {
     use fil_actors_runtime::test_utils::*;
     use fil_actors_runtime::SYSTEM_ACTOR_ADDR;
 
-    use crate::harness;
+    use crate::*;
     use harness::*;
 
     #[test]
     fn construct_with_root_id() {
         let mut rt = new_runtime();
-        let h = Harness { root: Address::new_id(101) };
+        let h = Harness { root: *ROOT_ADDR };
         h.construct_and_verify(&mut rt, &h.root);
         h.check_state();
     }
@@ -35,7 +42,7 @@ mod construction {
     #[test]
     fn construct_resolves_non_id() {
         let mut rt = new_runtime();
-        let h = Harness { root: Address::new_id(101) };
+        let h = Harness { root: *ROOT_ADDR };
         let root_pubkey = Address::new_bls(&[7u8; BLS_PUB_LEN]).unwrap();
         rt.id_addresses.insert(root_pubkey, h.root);
         h.construct_and_verify(&mut rt, &root_pubkey);
@@ -70,7 +77,7 @@ mod verifiers {
     };
     use fil_actors_runtime::test_utils::*;
 
-    use crate::{harness, VERIFIER_ALLOWANCE};
+    use crate::*;
     use harness::*;
 
     #[test]
@@ -98,7 +105,7 @@ mod verifiers {
         let allowance = MINIMUM_VERIFIED_DEAL_SIZE.clone() - DataCap::from(1);
         expect_abort(
             ExitCode::ErrIllegalArgument,
-            h.add_verifier(&mut rt, &Address::new_id(201), &allowance),
+            h.add_verifier(&mut rt, &VERIFIER, &allowance),
         );
         h.check_state();
     }
@@ -116,17 +123,16 @@ mod verifiers {
     #[test]
     fn add_verifier_rejects_client() {
         let (h, mut rt) = new_harness();
-        let client = Address::new_id(202);
         h.add_verifier_and_client(
             &mut rt,
-            &Address::new_id(201),
-            &client,
+            &VERIFIER,
+            &CLIENT,
             &VERIFIER_ALLOWANCE,
             &VERIFIER_ALLOWANCE,
         );
         expect_abort(
             ExitCode::ErrIllegalArgument,
-            h.add_verifier(&mut rt, &client, &VERIFIER_ALLOWANCE),
+            h.add_verifier(&mut rt, &CLIENT, &VERIFIER_ALLOWANCE),
         );
         h.check_state();
     }
@@ -155,7 +161,7 @@ mod verifiers {
     #[test]
     fn add_verifier_id_address() {
         let (h, mut rt) = new_harness();
-        h.add_verifier(&mut rt, &Address::new_id(201), &VERIFIER_ALLOWANCE).unwrap();
+        h.add_verifier(&mut rt, &VERIFIER, &VERIFIER_ALLOWANCE).unwrap();
         h.check_state();
     }
 
@@ -163,7 +169,7 @@ mod verifiers {
     fn add_verifier_resolves_address() {
         let (h, mut rt) = new_harness();
         let pubkey_addr = Address::new_secp256k1(&[0u8; 65]).unwrap();
-        rt.id_addresses.insert(pubkey_addr, Address::new_id(201));
+        rt.id_addresses.insert(pubkey_addr, *VERIFIER);
 
         h.add_verifier(&mut rt, &pubkey_addr, &VERIFIER_ALLOWANCE).unwrap();
         h.check_state();
@@ -172,8 +178,7 @@ mod verifiers {
     #[test]
     fn remove_requires_root() {
         let (h, mut rt) = new_harness();
-        let verifier = Address::new_id(201);
-        h.add_verifier(&mut rt, &verifier, &VERIFIER_ALLOWANCE).unwrap();
+        h.add_verifier(&mut rt, &VERIFIER, &VERIFIER_ALLOWANCE).unwrap();
 
         let caller = Address::new_id(501);
         rt.expect_validate_caller_addr(vec![h.root]);
@@ -183,7 +188,7 @@ mod verifiers {
             ExitCode::SysErrForbidden,
             rt.call::<VerifregActor>(
                 Method::RemoveVerifier as MethodNum,
-                &RawBytes::serialize(verifier).unwrap(),
+                &RawBytes::serialize(*VERIFIER).unwrap(),
             ),
         );
         h.check_state();
@@ -192,17 +197,15 @@ mod verifiers {
     #[test]
     fn remove_requires_verifier_exists() {
         let (h, mut rt) = new_harness();
-        let verifier = Address::new_id(501);
-        expect_abort(ExitCode::ErrIllegalArgument, h.remove_verifier(&mut rt, &verifier));
+        expect_abort(ExitCode::ErrIllegalArgument, h.remove_verifier(&mut rt, &VERIFIER));
         h.check_state();
     }
 
     #[test]
     fn remove_verifier() {
         let (h, mut rt) = new_harness();
-        let verifier = Address::new_id(201);
-        h.add_verifier(&mut rt, &verifier, &VERIFIER_ALLOWANCE).unwrap();
-        h.remove_verifier(&mut rt, &verifier).unwrap();
+        h.add_verifier(&mut rt, &VERIFIER, &VERIFIER_ALLOWANCE).unwrap();
+        h.remove_verifier(&mut rt, &VERIFIER).unwrap();
         h.check_state();
     }
 
@@ -210,12 +213,11 @@ mod verifiers {
     fn remove_verifier_id_address() {
         let (h, mut rt) = new_harness();
         let verifier_pubkey = Address::new_bls(&[1u8; BLS_PUB_LEN]).unwrap();
-        let verifier_id = Address::new_id(201);
-        rt.id_addresses.insert(verifier_pubkey, verifier_id);
+        rt.id_addresses.insert(verifier_pubkey, *VERIFIER);
         // Add using pubkey address.
-        h.add_verifier(&mut rt, &verifier_pubkey, &VERIFIER_ALLOWANCE).unwrap();
+        h.add_verifier(&mut rt, &VERIFIER, &VERIFIER_ALLOWANCE).unwrap();
         // Remove using ID address.
-        h.remove_verifier(&mut rt, &verifier_id).unwrap();
+        h.remove_verifier(&mut rt, &VERIFIER).unwrap();
         h.check_state();
     }
 }
@@ -233,58 +235,48 @@ mod clients {
     };
     use fil_actors_runtime::test_utils::*;
 
-    use crate::{harness, CLIENT_ALLOWANCE, VERIFIER_ALLOWANCE};
+    use crate::*;
     use harness::*;
 
     #[test]
     fn many_verifiers_and_clients() {
         let (h, mut rt) = new_harness();
-        let verifier1 = Address::new_id(201);
-        let verifier2 = Address::new_id(202);
-
         // Each verifier has enough allowance for two clients.
         let verifier_allowance = CLIENT_ALLOWANCE.clone() + CLIENT_ALLOWANCE.clone();
-        h.add_verifier(&mut rt, &verifier1, &verifier_allowance).unwrap();
-        h.add_verifier(&mut rt, &verifier2, &verifier_allowance).unwrap();
+        h.add_verifier(&mut rt, &VERIFIER, &verifier_allowance).unwrap();
+        h.add_verifier(&mut rt, &VERIFIER2, &verifier_allowance).unwrap();
 
-        let client1 = Address::new_id(301);
-        let client2 = Address::new_id(302);
-        h.add_client(&mut rt, &verifier1, &client1, &CLIENT_ALLOWANCE, &CLIENT_ALLOWANCE).unwrap();
-        h.add_client(&mut rt, &verifier1, &client2, &CLIENT_ALLOWANCE, &CLIENT_ALLOWANCE).unwrap();
+        h.add_client(&mut rt, &VERIFIER, &CLIENT, &CLIENT_ALLOWANCE, &CLIENT_ALLOWANCE).unwrap();
+        h.add_client(&mut rt, &VERIFIER, &CLIENT2, &CLIENT_ALLOWANCE, &CLIENT_ALLOWANCE).unwrap();
 
-        let client3 = Address::new_id(303);
-        let client4 = Address::new_id(304);
-        h.add_client(&mut rt, &verifier2, &client3, &CLIENT_ALLOWANCE, &CLIENT_ALLOWANCE).unwrap();
-        h.add_client(&mut rt, &verifier2, &client4, &CLIENT_ALLOWANCE, &CLIENT_ALLOWANCE).unwrap();
+        h.add_client(&mut rt, &VERIFIER2, &CLIENT3, &CLIENT_ALLOWANCE, &CLIENT_ALLOWANCE).unwrap();
+        h.add_client(&mut rt, &VERIFIER2, &CLIENT4, &CLIENT_ALLOWANCE, &CLIENT_ALLOWANCE).unwrap();
 
         // all clients should exist and verifiers should have no more allowance left
-        h.assert_client_allowance(&rt, &client1, &CLIENT_ALLOWANCE);
-        h.assert_client_allowance(&rt, &client2, &CLIENT_ALLOWANCE);
-        h.assert_client_allowance(&rt, &client3, &CLIENT_ALLOWANCE);
-        h.assert_client_allowance(&rt, &client4, &CLIENT_ALLOWANCE);
-        h.assert_verifier_allowance(&rt, &verifier1, &DataCap::from(0));
-        h.assert_verifier_allowance(&rt, &verifier2, &DataCap::from(0));
+        h.assert_client_allowance(&rt, &CLIENT, &CLIENT_ALLOWANCE);
+        h.assert_client_allowance(&rt, &CLIENT2, &CLIENT_ALLOWANCE);
+        h.assert_client_allowance(&rt, &CLIENT3, &CLIENT_ALLOWANCE);
+        h.assert_client_allowance(&rt, &CLIENT4, &CLIENT_ALLOWANCE);
+        h.assert_verifier_allowance(&rt, &VERIFIER, &DataCap::from(0));
+        h.assert_verifier_allowance(&rt, &VERIFIER2, &DataCap::from(0));
         h.check_state();
     }
 
     #[test]
     fn verifier_allowance_exhausted() {
         let (h, mut rt) = new_harness();
-        let verifier = Address::new_id(201);
         // Verifier only has allowance for one client.
-        h.add_verifier(&mut rt, &verifier, &CLIENT_ALLOWANCE).unwrap();
+        h.add_verifier(&mut rt, &VERIFIER, &CLIENT_ALLOWANCE).unwrap();
 
-        let client1 = Address::new_id(301);
-        h.add_client(&mut rt, &verifier, &client1, &CLIENT_ALLOWANCE, &CLIENT_ALLOWANCE).unwrap();
-        let client2 = Address::new_id(302);
+        h.add_client(&mut rt, &VERIFIER, &CLIENT, &CLIENT_ALLOWANCE, &CLIENT_ALLOWANCE).unwrap();
         expect_abort(
             ExitCode::ErrIllegalArgument,
-            h.add_client(&mut rt, &verifier, &client2, &CLIENT_ALLOWANCE, &CLIENT_ALLOWANCE),
+            h.add_client(&mut rt, &VERIFIER, &CLIENT2, &CLIENT_ALLOWANCE, &CLIENT_ALLOWANCE),
         );
 
         // One client should exist and verifier should have no more allowance left.
-        h.assert_client_allowance(&rt, &client1, &CLIENT_ALLOWANCE);
-        h.assert_verifier_allowance(&rt, &verifier, &DataCap::from(0));
+        h.assert_client_allowance(&rt, &CLIENT, &CLIENT_ALLOWANCE);
+        h.assert_verifier_allowance(&rt, &VERIFIER, &DataCap::from(0));
         h.check_state();
     }
 
@@ -293,19 +285,17 @@ mod clients {
         let (h, mut rt) = new_harness();
 
         let client_pubkey = Address::new_bls(&[7u8; BLS_PUB_LEN]).unwrap();
-        let client_id = Address::new_id(301);
-        rt.id_addresses.insert(client_pubkey, client_id);
+        rt.id_addresses.insert(client_pubkey, *CLIENT);
 
-        let verifier = Address::new_id(201);
-        h.add_verifier(&mut rt, &verifier, &VERIFIER_ALLOWANCE).unwrap();
-        h.add_client(&mut rt, &verifier, &client_pubkey, &CLIENT_ALLOWANCE, &CLIENT_ALLOWANCE)
+        h.add_verifier(&mut rt, &VERIFIER, &VERIFIER_ALLOWANCE).unwrap();
+        h.add_client(&mut rt, &VERIFIER, &client_pubkey, &CLIENT_ALLOWANCE, &CLIENT_ALLOWANCE)
             .unwrap();
 
         // Adding another verified client with the same ID address increments
         // the data cap which has already been granted.
-        h.add_verifier(&mut rt, &verifier, &VERIFIER_ALLOWANCE).unwrap();
+        h.add_verifier(&mut rt, &VERIFIER, &VERIFIER_ALLOWANCE).unwrap();
         let expected_allowance = CLIENT_ALLOWANCE.clone() + CLIENT_ALLOWANCE.clone();
-        h.add_client(&mut rt, &verifier, &client_id, &CLIENT_ALLOWANCE, &expected_allowance)
+        h.add_client(&mut rt, &VERIFIER, &CLIENT, &CLIENT_ALLOWANCE, &expected_allowance)
             .unwrap();
         h.check_state();
     }
@@ -313,20 +303,17 @@ mod clients {
     #[test]
     fn minimum_allowance_ok() {
         let (h, mut rt) = new_harness();
-        let verifier = Address::new_id(201);
-        h.add_verifier(&mut rt, &verifier, &VERIFIER_ALLOWANCE).unwrap();
+        h.add_verifier(&mut rt, &VERIFIER, &VERIFIER_ALLOWANCE).unwrap();
 
-        let client = Address::new_id(301);
         let allowance = MINIMUM_VERIFIED_DEAL_SIZE.clone();
-        h.add_client(&mut rt, &verifier, &client, &allowance, &allowance).unwrap();
+        h.add_client(&mut rt, &VERIFIER, &CLIENT, &allowance, &allowance).unwrap();
         h.check_state();
     }
 
     #[test]
     fn rejects_unresolved_address() {
         let (h, mut rt) = new_harness();
-        let verifier = Address::new_id(201);
-        h.add_verifier(&mut rt, &verifier, &VERIFIER_ALLOWANCE).unwrap();
+        h.add_verifier(&mut rt, &VERIFIER, &VERIFIER_ALLOWANCE).unwrap();
 
         let client = Address::new_bls(&[7u8; BLS_PUB_LEN]).unwrap();
         // Expect runtime to attempt to create the actor, but don't add it to the mock's
@@ -342,7 +329,7 @@ mod clients {
 
         expect_abort(
             ExitCode::ErrIllegalState,
-            h.add_client(&mut rt, &verifier, &client, &CLIENT_ALLOWANCE, &CLIENT_ALLOWANCE),
+            h.add_client(&mut rt, &VERIFIER, &client, &CLIENT_ALLOWANCE, &CLIENT_ALLOWANCE),
         );
         h.check_state();
     }
@@ -350,14 +337,12 @@ mod clients {
     #[test]
     fn rejects_allowance_below_minimum() {
         let (h, mut rt) = new_harness();
-        let verifier = Address::new_id(201);
-        h.add_verifier(&mut rt, &verifier, &VERIFIER_ALLOWANCE).unwrap();
+        h.add_verifier(&mut rt, &VERIFIER, &VERIFIER_ALLOWANCE).unwrap();
 
-        let client = Address::new_id(301);
         let allowance = MINIMUM_VERIFIED_DEAL_SIZE.clone() - DataCap::from(1);
         expect_abort(
             ExitCode::ErrIllegalArgument,
-            h.add_client(&mut rt, &verifier, &client, &allowance, &allowance),
+            h.add_client(&mut rt, &VERIFIER, &CLIENT, &allowance, &allowance),
         );
         h.check_state();
     }
@@ -365,15 +350,13 @@ mod clients {
     #[test]
     fn rejects_non_verifier_caller() {
         let (h, mut rt) = new_harness();
-        let verifier = Address::new_id(201);
-        h.add_verifier(&mut rt, &verifier, &VERIFIER_ALLOWANCE).unwrap();
+        h.add_verifier(&mut rt, &VERIFIER, &VERIFIER_ALLOWANCE).unwrap();
 
-        let client = Address::new_id(301);
         let caller = Address::new_id(209);
         rt.set_caller(*ACCOUNT_ACTOR_CODE_ID, caller);
         rt.expect_validate_caller_any();
         let params =
-            AddVerifierClientParams { address: client, allowance: CLIENT_ALLOWANCE.clone() };
+            AddVerifierClientParams { address: *CLIENT, allowance: CLIENT_ALLOWANCE.clone() };
         expect_abort(
             ExitCode::ErrNotFound,
             rt.call::<VerifregActor>(
@@ -387,13 +370,12 @@ mod clients {
     #[test]
     fn rejects_allowance_greater_than_verifier_cap() {
         let (h, mut rt) = new_harness();
-        let verifier = Address::new_id(201);
-        h.add_verifier(&mut rt, &verifier, &VERIFIER_ALLOWANCE).unwrap();
+        h.add_verifier(&mut rt, &VERIFIER, &VERIFIER_ALLOWANCE).unwrap();
 
         let allowance = VERIFIER_ALLOWANCE.clone() + DataCap::from(1);
         expect_abort(
             ExitCode::ErrIllegalArgument,
-            h.add_client(&mut rt, &verifier, &h.root, &allowance, &allowance),
+            h.add_client(&mut rt, &VERIFIER, &h.root, &allowance, &allowance),
         );
         h.check_state();
     }
@@ -401,11 +383,10 @@ mod clients {
     #[test]
     fn rejects_root_as_client() {
         let (h, mut rt) = new_harness();
-        let verifier = Address::new_id(201);
-        h.add_verifier(&mut rt, &verifier, &VERIFIER_ALLOWANCE).unwrap();
+        h.add_verifier(&mut rt, &VERIFIER, &VERIFIER_ALLOWANCE).unwrap();
         expect_abort(
             ExitCode::ErrIllegalArgument,
-            h.add_client(&mut rt, &verifier, &h.root, &CLIENT_ALLOWANCE, &CLIENT_ALLOWANCE),
+            h.add_client(&mut rt, &VERIFIER, &h.root, &CLIENT_ALLOWANCE, &CLIENT_ALLOWANCE),
         );
         h.check_state();
     }
@@ -413,21 +394,19 @@ mod clients {
     #[test]
     fn rejects_verifier_as_client() {
         let (h, mut rt) = new_harness();
-        let verifier = Address::new_id(201);
-        h.add_verifier(&mut rt, &verifier, &VERIFIER_ALLOWANCE).unwrap();
+        h.add_verifier(&mut rt, &VERIFIER, &VERIFIER_ALLOWANCE).unwrap();
         expect_abort(
             ExitCode::ErrIllegalArgument,
-            h.add_client(&mut rt, &verifier, &verifier, &CLIENT_ALLOWANCE, &CLIENT_ALLOWANCE),
+            h.add_client(&mut rt, &VERIFIER, &VERIFIER, &CLIENT_ALLOWANCE, &CLIENT_ALLOWANCE),
         );
 
-        let another_verifier = Address::new_id(202);
-        h.add_verifier(&mut rt, &another_verifier, &VERIFIER_ALLOWANCE).unwrap();
+        h.add_verifier(&mut rt, &VERIFIER2, &VERIFIER_ALLOWANCE).unwrap();
         expect_abort(
             ExitCode::ErrIllegalArgument,
             h.add_client(
                 &mut rt,
-                &verifier,
-                &another_verifier,
+                &VERIFIER,
+                &VERIFIER2,
                 &CLIENT_ALLOWANCE,
                 &CLIENT_ALLOWANCE,
             ),
