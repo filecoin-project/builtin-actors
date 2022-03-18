2e22f51240fe51e38139aee81636f5718306808e
Alex <445306+anorth@users.noreply.github.com>
2022-03-18 15:19:00 +1100

    Build configuration to deny unused results (#121)

--- actors/verifreg/tests/harness/mod.rs
+++ actors/verifreg/tests/harness/mod.rs
@@ -1,5 +1,3 @@
-#![deny(unused_must_use)] // Force unwrapping Result<_, Err>
-
 use fvm_shared::address::Address;
 use fvm_shared::bigint::bigint_ser::BigIntDe;
 use fvm_shared::blockstore::MemoryBlockstore;
