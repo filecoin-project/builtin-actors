a188d7f256bf4f61f6f0ff1d6e91a6e35b8b2047
zenground0 <ZenGround0@users.noreply.github.com>
2022-04-01 16:44:22 -0600

    WIP -- taking shape

--- test_vm/src/lib.rs
+++ test_vm/src/lib.rs
@@ -1,29 +1,22 @@
 
 use cid::{multihash, Cid};
+use fil_actors_runtime::builtin::HAMT_BIT_WIDTH;
 use fvm_shared::econ::TokenAmount;
-use fil_actors_runtime::{
-    //actor_error, make_empty_map, make_map_with_root, resolve_to_id_addr, wasm_trampoline,
-    Map,
-};
 use fvm_shared::encoding::tuple::*;
+use fvm_shared::address::{Address, Protocol};
 use fvm_shared::encoding::de::Deserialize;
 use fvm_shared::bigint::{bigint_ser, Integer};
-use fvm_ipld_hamt::{Hamt, BytesKey};
-use std::cell::RefCell;
-use std::collections::HashMap;
-use fvm_shared::blockstore::{MemoryBlockstore};
-use anyhow;
+use fvm_ipld_hamt::{Hamt, BytesKey, Sha256};
+use fvm_shared::blockstore::{MemoryBlockstore, Blockstore};
+use std::error::Error;
+use std::fmt;
 
 
-use core::fmt::Error;
-
-
-pub struct VM<'bs> {
+pub struct VM<'bs>{
     store: &'bs MemoryBlockstore,
-
     state_root: Cid,
     actors_dirty: bool,
-    actors: Hamt<MemoryBlockstore, Actor, BytesKey>,
+    actors: Hamt<&'bs MemoryBlockstore, Actor, BytesKey>,
 } 
 
 
@@ -40,26 +33,46 @@ impl<'bs> VM<'bs> {
         // 3. call method
     }
 
-    pub fn normalize_address() {
+    // pub fn normalize_address(addr: Address) -> Result<(Address, bool), Error> {
+    //     match addr.protocol() {
+    //         Protocol::ID => Ok((addr, true)),
+    //         _ => {
 
+    //         }
+
+    //     }
+    // }
+
+    pub fn get_actor(&self, addr: Address) -> Result<Actor, TestVMError> {
+        match self.actors.get(&addr.to_bytes())? {
+            None => Err(vm_err("failed to get addr")),
+            Some(a)=> Ok(a.clone()),
+        }
     }
 
-    pub fn get_actor() {
-
-    }
-
-    pub fn set_actor() {
-
+    // blindly overwrite the actor at this address whether it previously existed or not
+    pub fn set_actor(&mut self, key: Address, a: Actor) -> Result<(), fvm_ipld_hamt::Error> {
+        let _ = self.actors.set(key.to_bytes().into(), a)?;
+        Ok(())
     }
 
     fn checkpoint(&mut self) ->
      Result<Cid, fvm_ipld_hamt::Error> {
-        self.actors.flush()
+        self.state_root = self.actors.flush()?;
+        self.actors_dirty = false;
+        Ok(self.state_root)
     }
+
+    fn rollback(&mut self, root: Cid) -> Result<(), fvm_ipld_hamt::Error> {
+        self.actors = Hamt::<&'bs MemoryBlockstore, Actor, BytesKey, Sha256>::load(&root, &self.store)?;
+        self.state_root = root;
+        self.actors_dirty = false;
+        Ok(())
+    } 
 }
 
 
-#[derive(Serialize_tuple, Deserialize_tuple, Clone)]
+#[derive(Serialize_tuple, Deserialize_tuple, Clone, PartialEq)]
 pub struct Actor{
     code: Cid,  // Might want to mock this out to avoid dealing with the annoying bundler
     head: Cid, 
@@ -67,6 +80,8 @@ pub struct Actor{
     #[serde(with = "bigint_ser")]
     balance: TokenAmount,
 }
+
+
 
 // struct invocation_context{
 //     msg: internal_message,
@@ -77,3 +92,31 @@ pub struct Actor{
 // impl invocation_context{
 
 // }
+#[derive(Debug)]
+pub struct TestVMError {
+    msg: String
+}
+
+impl fmt::Display for TestVMError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f,"{}",self.msg)
+    }
+}
+
+impl Error for TestVMError {
+    fn description(&self) -> &str {
+        &self.msg
+    }
+}
+
+impl From<fvm_ipld_hamt::Error> for TestVMError {
+    fn from(h_err: fvm_ipld_hamt::Error) -> Self {
+        vm_err(h_err.to_string().as_str())
+    }
+}
+
+pub fn vm_err(msg: &str) -> TestVMError {
+    TestVMError{msg: msg.to_string()}
+}
+
+const Cid::
--- test_vm/tests/test_vm_test.rs
+++ test_vm/tests/test_vm_test.rs
@@ -4,5 +4,33 @@ use test_vm::{
 
 #[test]
 fn sanity_test() {
-    let v = VM{};
+    let v = VM{
+        store: 
+    };
+
+    // set actor
+
+    // rollback removes
+
+    // set actor
+
+    // checkpoint
+
+    // actor stays
+
+}
+
+#[test]
+fn normalize_addr() {
+    // set up init actor
+
+    // set up account actor
+
+    // set up system actor
+
+    // normalize system actor => nop
+
+    // normalize account actor => nop
+
+    // normalize account actor by id addr => resolves 
 }
