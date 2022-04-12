88753cb8751bc0f13955492de371eabdfb22bba9
vyzo <vyzo@hackzen.org>
2022-04-12 14:14:44 +0300

    add deploy method to init actor

--- actors/init/src/lib.rs
+++ actors/init/src/lib.rs
@@ -1,17 +1,18 @@
 // Copyright 2019-2022 ChainSafe Systems
 // SPDX-License-Identifier: Apache-2.0, MIT
 
-use cid::Cid;
+//use cid::Cid;
 use fil_actors_runtime::runtime::{ActorCode, Runtime};
 use fil_actors_runtime::{actor_error, cbor, ActorDowncast, ActorError, SYSTEM_ACTOR_ADDR};
-use fvm_ipld_blockstore::Blockstore;
+use fvm_ipld_blockstore::{Blockstore, Block};
 use fvm_ipld_encoding::RawBytes;
-use fvm_shared::actor::builtin::Type;
+//use fvm_shared::actor::builtin::Type;
 use fvm_shared::address::Address;
 use fvm_shared::error::ExitCode;
 use fvm_shared::{ActorID, MethodNum, METHOD_CONSTRUCTOR};
 use num_derive::FromPrimitive;
 use num_traits::FromPrimitive;
+use cid::multihash::Code;
 
 pub use self::state::State;
 pub use self::types::*;
@@ -30,6 +31,7 @@ fil_actors_runtime::wasm_trampoline!(Actor);
 pub enum Method {
     Constructor = METHOD_CONSTRUCTOR,
     Exec = 2,
+    Deploy = 3,
 }
 
 /// Init actor
@@ -62,18 +64,19 @@ impl Actor {
 
         log::trace!("called exec; params.code_cid: {:?}", &params.code_cid);
 
-        let caller_code = rt.get_actor_code_cid(&rt.message().caller()).ok_or_else(|| {
-            actor_error!(illegal_state, "no code for caller as {}", rt.message().caller())
-        })?;
+        // TODO figure out ACLs -- allow exec anything for now
+        // let caller_code = rt.get_actor_code_cid(&rt.message().caller()).ok_or_else(|| {
+        //     actor_error!(illegal_state, "no code for caller as {}", rt.message().caller())
+        // })?;
 
-        log::trace!("caller code CID: {:?}", &caller_code);
+        // log::trace!("caller code CID: {:?}", &caller_code);
 
-        if !can_exec(rt, &caller_code, &params.code_cid) {
-            return Err(actor_error!(forbidden;
-                    "called type {} cannot exec actor type {}",
-                    &caller_code, &params.code_cid
-            ));
-        }
+        // if !can_exec(rt, &caller_code, &params.code_cid) {
+        //     return Err(actor_error!(forbidden;
+        //             "called type {} cannot exec actor type {}",
+        //             &caller_code, &params.code_cid
+        //     ));
+        // }
 
         // Compute a re-org-stable address.
         // This address exists for use by messages coming from outside the system, in order to
@@ -105,6 +108,47 @@ impl Actor {
 
         Ok(ExecReturn { id_address: Address::new_id(id_address), robust_address })
     }
+
+    pub fn deploy<BS, RT>(rt: &mut RT, params: DeployParams) -> Result<DeployReturn, ActorError>
+    where
+        BS: Blockstore,
+        RT: Runtime<BS>,
+    {
+        rt.validate_immediate_caller_accept_any()?;
+
+        let code_cid = rt.transaction(|st: &mut State, rt| {
+            let code = params.code.bytes();
+            let code_cid = rt.store().put(Code::Blake2b256, &Block::new(0x55, code))
+                .map_err(|e| {
+                    e.downcast_default(
+                        ExitCode::USR_SERIALIZATION,
+                        "failed to put code into the bockstore",
+                    )
+                })?;
+
+            if st.is_deployed_actor(rt.store(), &code_cid).map_err(|e| {
+                    e.downcast_default(
+                        ExitCode::USR_ILLEGAL_STATE,
+                        "failed to check state for deployed actor",
+                    )
+                })? {
+                return Ok(code_cid);
+            }
+
+            // TODO Actually deploy via syscall
+
+            st.add_deployed_actor(rt.store(), code_cid.clone()).map_err(|e| {
+                e.downcast_default(
+                    ExitCode::USR_ILLEGAL_STATE,
+                    "failed to add deployed actor to state",
+                )
+            })?;
+            Ok(code_cid)
+        })?;
+
+        Ok(DeployReturn{ code_cid })
+    }
+
 }
 
 impl ActorCode for Actor {
@@ -126,21 +170,25 @@ impl ActorCode for Actor {
                 let res = Self::exec(rt, cbor::deserialize_params(params)?)?;
                 Ok(RawBytes::serialize(res)?)
             }
+            Some(Method::Deploy) => {
+                let res = Self::exec(rt, cbor::deserialize_params(params)?)?;
+                Ok(RawBytes::serialize(res)?)
+            }
             None => Err(actor_error!(unhandled_message; "Invalid method")),
         }
     }
 }
 
-fn can_exec<BS, RT>(rt: &RT, caller: &Cid, exec: &Cid) -> bool
-where
-    BS: Blockstore,
-    RT: Runtime<BS>,
-{
-    rt.resolve_builtin_actor_type(exec)
-        .map(|typ| match typ {
-            Type::Multisig | Type::PaymentChannel => true,
-            Type::Miner if rt.resolve_builtin_actor_type(caller) == Some(Type::Power) => true,
-            _ => false,
-        })
-        .unwrap_or(false)
-}
+// fn can_exec<BS, RT>(rt: &RT, caller: &Cid, exec: &Cid) -> bool
+// where
+//     BS: Blockstore,
+//     RT: Runtime<BS>,
+// {
+//     rt.resolve_builtin_actor_type(exec)
+//         .map(|typ| match typ {
+//             Type::Multisig | Type::PaymentChannel => true,
+//             Type::Miner if rt.resolve_builtin_actor_type(caller) == Some(Type::Power) => true,
+//             _ => false,
+//         })
+//         .unwrap_or(false)
+// }
