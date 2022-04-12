891bf7ac00a6d200ac54f8bc0faae5f2347d287f
vyzo <vyzo@hackzen.org>
2022-04-12 14:40:13 +0300

    fix test_utils

--- actors/runtime/src/test_utils.rs
+++ actors/runtime/src/test_utils.rs
@@ -1192,7 +1192,7 @@ impl Verifier for MockRuntime {
         exp.result
     }
 
-    fn deploy_actor(&self, _code_cid: &Cid) -> Result<(), Error> {
+    fn deploy_actor(&self, _code_cid: &Cid) -> anyhow::Result<(), anyhow::Error> {
         Ok(())
     }
 
