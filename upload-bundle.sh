e3b151d8622e489e0ba17cfeeb1e1c2e0e8b07a9
vyzo <vyzo@hackzen.org>
2022-04-12 12:49:51 +0300

    output sha256sum for uploaded bundle (#213)
    * output sha256sum for uploaded bundle
    
    * include sha256sum in the bundle
    
    * use shasum -a 256

--- upload-bundle.sh
+++ upload-bundle.sh
@@ -3,5 +3,9 @@
 set -e
 
 bundle=output/builtin-actors.car
+
+shasum -a 256 $bundle > $bundle.sha256sum
+cat $bundle.sha256sum
+
 curl -k -X POST -F "data=@${bundle};type=application/octet-stream;filename=\"${bundle}\"" -H "Authorization: Bearer $ESTUARY_TOKEN" -H "Content-Type: multipart/form-data" https://shuttle-4.estuary.tech/content/add > output/upload.json
 cat output/upload.json
