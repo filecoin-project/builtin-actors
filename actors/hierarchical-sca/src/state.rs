7ab94af266d859c32f6d9846a7d28648fa1c0367
Alfonso de la Rocha <adlrocha@tutamail.com>
2022-04-13 12:54:47 +0200

    wip: subnetID

--- actors/hierarchical-sca/src/state.rs
+++ actors/hierarchical-sca/src/state.rs
@@ -1,55 +1,47 @@
 // Copyright: ConsensusLab
 //
+use anyhow::anyhow;
 use cid::Cid;
-use anyhow::{anyhow, /*Context*/};
 use fil_actors_runtime::{
-    // actor_error,
-    Array, make_empty_map, 
+    make_empty_map,
     // make_map_with_root, make_map_with_root_and_bitwidth,
     // ActorDowncast, ActorError, Map, Multimap,
+    // actor_error,
+    Array,
 };
 use fvm_ipld_blockstore::Blockstore;
 use fvm_ipld_encoding::tuple::*;
-use fvm_ipld_encoding::{Cbor, /*RawBytes*/};
-use fvm_shared::econ::TokenAmount;
-use fvm_ipld_hamt::BytesKey;
+use fvm_ipld_encoding::Cbor;
 use fvm_shared::bigint::{bigint_ser, BigInt};
 use fvm_shared::clock::ChainEpoch;
+use fvm_shared::econ::TokenAmount;
 use fvm_shared::HAMT_BIT_WIDTH;
 use lazy_static::lazy_static;
-use integer_encoding::VarInt;
 
+use super::subnet::*;
 use super::types::*;
 
 /// Storage power actor state
 #[derive(Default, Serialize_tuple, Deserialize_tuple)]
 pub struct State {
-    pub network_name: String, 
+    pub network_name: String,
     pub total_subnets: u64,
     #[serde(with = "bigint_ser")]
-    pub min_stake: TokenAmount, 
+    pub min_stake: TokenAmount,
     pub subnets: Cid, // HAMT[cid.Cid]Subnet
     pub check_period: ChainEpoch,
-    pub checkpoints: Cid, // HAMT[epoch]Checkpoint
+    pub checkpoints: Cid,        // HAMT[epoch]Checkpoint
     pub check_msg_registry: Cid, // HAMT[cid]CrossMsgs
     pub nonce: u64,
     pub bottom_up_nonce: u64,
-    pub bottom_up_msg_meta: Cid,// AMT[schema.CrossMsgs] from child subnets to apply.
+    pub bottom_up_msg_meta: Cid, // AMT[schema.CrossMsgs] from child subnets to apply.
     pub applied_bottomup_nonce: u64,
     pub applied_topdown_nonce: u64,
     pub atomic_exec_registry: Cid, // HAMT[cid]AtomicExec
 }
 
-pub enum Status {
-    Active,
-    Inactive,
-    Killed,
-}
-
 lazy_static! {
-    /// TODO: Comment
-    // static ref MIN_SUBNET_STAKE: BigInt = BigInt::from(10_i64.pow(18));
-    static ref MIN_SUBNET_STAKE: BigInt = TokenAmount::from(10_i64.pow(18));
+    static ref MIN_SUBNET_COLLATERAL: BigInt = TokenAmount::from(MIN_COLLATERAL_AMOUNT);
 }
 
 impl Cbor for State {}
@@ -74,9 +66,9 @@ impl State {
                 .map_err(|e| anyhow!("Failed to create empty proposals array: {}", e))?;
         Ok(State {
             network_name: params.network_name,
-            min_stake: MIN_SUBNET_STAKE.clone(),
+            min_stake: MIN_SUBNET_COLLATERAL.clone(),
             check_period: match params.checkpoint_period > DEFAULT_CHECKPOINT_PERIOD {
-                true => params.checkpoint_period, 
+                true => params.checkpoint_period,
                 false => DEFAULT_CHECKPOINT_PERIOD,
             },
             subnets: empty_sn_map,
@@ -89,8 +81,18 @@ impl State {
         })
     }
 
-}
-
-#[cfg(test)]
-mod test {
+    // pub fn get_subnet<BS: Blockstore>(
+    //     &self,
+    //     store: &BS,
+    //     id: String,
+    // ) -> anyhow::Result<Option<Subnet>> {
+    //      let claims =
+    //          make_map_with_root_and_bitwidth::<_, Claim>(&self.claims, store, HAMT_BIT_WIDTH)
+    //              .map_err(|e| {
+    //                  e.downcast_default(ExitCode::USR_ILLEGAL_STATE, "failed to load claims")
+    //              })?;
+    //
+    //      let claim = get_claim(&claims, miner)?;
+    //      Ok(claim.cloned())
+    // }
 }
