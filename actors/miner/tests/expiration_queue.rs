47dae266709778491c686a6c04ec19afc118bb65
David Himmelstrup <lemmih@gmail.com>
2022-05-09 14:50:39 +0200

    Implement reschedules_sectors_as_faults

--- actors/miner/tests/expiration_queue.rs
+++ actors/miner/tests/expiration_queue.rs
@@ -351,61 +351,66 @@ fn quantizes_added_sectors_by_expiration() {
     assert_eq!(set.on_time_sectors, mk_bitfield([5, 6]));
     assert_eq!(queue.amt.count(), 0);
 }
+
+#[test]
+fn reschedules_sectors_as_faults() {
+    let h = ActorHarness::new(0);
+    let rt = h.new_runtime();
+
+    // Create 3 expiration sets with 2 sectors apiece
+    let mut queue = empty_expiration_queue_with_quantizing(&rt, QuantSpec { unit: 4, offset: 1 });
+    let (_sec_nums, _power, _pledge) = queue.add_active_sectors(&sectors(), SECTOR_SIZE).unwrap();
+
+    let _ = queue.amt.flush().unwrap();
+
+    // Fault middle sectors to expire at epoch 6
+    // This faults one sector from the first set, all of the second set and one from the third.
+    // Faulting at epoch 6 means the first 3 will expire on time, but the last will be early and
+    // moved to the second set
+    let power_delta = queue.reschedule_as_faults(6, &sectors()[1..5], SECTOR_SIZE).unwrap();
+    assert_eq!(power_delta, power_for_sectors(SECTOR_SIZE, &sectors()[1..5]));
+
+    let _ = queue.amt.flush().unwrap();
+
+    // expect first set to contain first two sectors but with the seconds power moved to faulty power
+    require_no_expiration_groups_before(5, &mut queue);
+    let set = queue.pop_until(5).unwrap();
+
+    assert_eq!(set.on_time_sectors, mk_bitfield([1, 2]));
+    assert!(set.early_sectors.is_empty());
+
+    assert_eq!(set.on_time_pledge, TokenAmount::from(2001));
+    assert_eq!(set.active_power, power_for_sectors(SECTOR_SIZE, &sectors()[0..1]));
+    assert_eq!(set.faulty_power, power_for_sectors(SECTOR_SIZE, &sectors()[1..2]));
+
+    // expect the second set to have all faulty power and now contain 5th sector as an early sector
+    require_no_expiration_groups_before(9, &mut queue);
+    let set = queue.pop_until(9).unwrap();
+
+    assert_eq!(set.on_time_sectors, mk_bitfield([3, 4]));
+    assert_eq!(set.early_sectors, mk_bitfield([5]));
+
+    // pledge is kept from original 2 sectors. Pledge from new early sector is NOT added.
+    assert_eq!(set.on_time_pledge, TokenAmount::from(2005));
+
+    assert_eq!(set.active_power, PowerPair::zero());
+    assert_eq!(set.faulty_power, power_for_sectors(SECTOR_SIZE, &sectors()[2..5]));
+
+    // expect last set to only contain non faulty sector
+    require_no_expiration_groups_before(13, &mut queue);
+    let set = queue.pop_until(13).unwrap();
+
+    assert_eq!(set.on_time_sectors, mk_bitfield([6]));
+    assert!(set.early_sectors.is_empty());
+
+    // Pledge from sector moved from this set is dropped
+    assert_eq!(set.on_time_pledge, TokenAmount::from(1005));
+
+    assert_eq!(set.active_power, power_for_sectors(SECTOR_SIZE, &sectors()[5..]));
+    assert_eq!(set.faulty_power, PowerPair::zero());
+}
 /*
     t.Run("reschedules sectors as faults", func(t *testing.T) {
-        // Create 3 expiration sets with 2 sectors apiece
-        queue := emptyExpirationQueueWithQuantizing(t, builtin.NewQuantSpec(4, 1), testAmtBitwidth)
-        _, _, _, err := queue.AddActiveSectors(sectors, sectorSize)
-        require.NoError(t, err)
-
-        _, err = queue.Root()
-        require.NoError(t, err)
-
-        // Fault middle sectors to expire at epoch 6
-        // This faults one sector from the first set, all of the second set and one from the third.
-        // Faulting at epoch 6 means the first 3 will expire on time, but the last will be early and
-        // moved to the second set
-        powerDelta, err := queue.RescheduleAsFaults(abi.ChainEpoch(6), sectors[1:5], sectorSize)
-        require.NoError(t, err)
-        assert.True(t, powerDelta.Equals(miner.PowerForSectors(sectorSize, sectors[1:5])))
-
-        _, err = queue.Root()
-        require.NoError(t, err)
-
-        // expect first set to contain first two sectors but with the seconds power moved to faulty power
-        requireNoExpirationGroupsBefore(t, 5, queue)
-        set, err := queue.PopUntil(5)
-        require.NoError(t, err)
-
-        assertBitfieldEquals(t, set.OnTimeSectors, 1, 2)
-        assertBitfieldEmpty(t, set.EarlySectors)
-
-        assert.Equal(t, big.NewInt(2001), set.OnTimePledge)
-        assert.True(t, set.ActivePower.Equals(miner.PowerForSectors(sectorSize, sectors[0:1])))
-        assert.True(t, set.FaultyPower.Equals(miner.PowerForSectors(sectorSize, sectors[1:2])))
-
-        // expect the second set to have all faulty power and now contain 5th sector as an early sector
-        requireNoExpirationGroupsBefore(t, 9, queue)
-        set, err = queue.PopUntil(9)
-        require.NoError(t, err)
-
-        assertBitfieldEquals(t, set.OnTimeSectors, 3, 4)
-        assertBitfieldEquals(t, set.EarlySectors, 5)
-
-        // pledge is kept from original 2 sectors. Pledge from new early sector is NOT added.
-        assert.Equal(t, big.NewInt(2005), set.OnTimePledge)
-
-        assert.True(t, set.ActivePower.Equals(miner.NewPowerPairZero()))
-        assert.True(t, set.FaultyPower.Equals(miner.PowerForSectors(sectorSize, sectors[2:5])))
-
-        // expect last set to only contain non faulty sector
-        requireNoExpirationGroupsBefore(t, 13, queue)
-        set, err = queue.PopUntil(13)
-        require.NoError(t, err)
-
-        assertBitfieldEquals(t, set.OnTimeSectors, 6)
-        assertBitfieldEmpty(t, set.EarlySectors)
-
         // Pledge from sector moved from this set is dropped
         assert.Equal(t, big.NewInt(1005), set.OnTimePledge)
 
@@ -751,6 +756,16 @@ fn test_sector(
         sealed_cid: make_sealed_cid(format!("commR-{}", sector_number).as_bytes()),
         ..Default::default()
     }
+}
+
+fn require_no_expiration_groups_before(
+    epoch: ChainEpoch,
+    queue: &mut ExpirationQueue<'_, MemoryBlockstore>,
+) {
+    queue.amt.flush().unwrap();
+
+    let set = queue.pop_until(epoch - 1).unwrap();
+    assert!(set.is_empty());
 }
 
 fn empty_expiration_queue_with_quantizing<'a>(
