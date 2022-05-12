4f41b598f9d6263dfbc49221ad986471e6297262
Josh Jones <joshua.jones@chainsafe.io>
2022-05-12 15:27:42 -0500

    convert quantizes_added_epochs... tests

--- actors/miner/tests/miner_actor_test_bitfield_queue.rs
+++ actors/miner/tests/miner_actor_test_bitfield_queue.rs
@@ -69,6 +69,31 @@ fn adds_bitfield_to_empty_queue() {
     bq_expectation.add(epoch, ranges[0].clone().map(u64::from).collect::<Vec<u64>>()).equals(queue);
 }
 
+#[test]
+fn quantizes_added_epochs_according_to_quantization_spec() {
+    let h = ActorHarness::new(0);
+    let rt = h.new_runtime();
+    let mut queue = empty_bitfield_queue_with_quantizing(
+        &rt,
+        QuantSpec { unit: 5, offset: 3 },
+        TEST_AMT_BITWIDTH,
+    );
+
+    let range: Vec<u64> = vec![0, 2, 3, 4, 7, 8, 9];
+    for val in range {
+        queue.add_to_queue_values(val as i64, [val]).unwrap();
+    }
+
+    let expected: HashMap<_, Vec<u64>> = HashMap::new();
+    let mut bq_expectation = BQExpectation { expected };
+    // expect values to only be set on quantization boundaries
+    bq_expectation
+        .add(ChainEpoch::from(3), [0, 2, 3].to_vec())
+        .add(ChainEpoch::from(8), [4, 7, 8].to_vec())
+        .add(ChainEpoch::from(13), [9].to_vec())
+        .equals(queue);
+}
+
 fn empty_bitfield_queue_with_quantizing(
     rt: &MockRuntime,
     quant: QuantSpec,
