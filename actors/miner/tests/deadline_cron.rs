0d276a1ad0bf29d5f97943f2aff86e88ac9ea830
David Himmelstrup <lemmih@gmail.com>
2022-05-18 12:20:52 +0200

    Implement detects_and_penalizes_faults

--- actors/miner/tests/deadline_cron.rs
+++ actors/miner/tests/deadline_cron.rs
@@ -1,6 +1,12 @@
-use fil_actor_miner::power_for_sectors;
-use fvm_shared::clock::ChainEpoch;
+use fil_actor_miner::Deadline;
+use fil_actor_miner::{pledge_penalty_for_continued_fault, power_for_sectors, SectorOnChainInfo};
+use fil_actors_runtime::test_utils::MessageAccumulator;
+use fil_actors_runtime::test_utils::MockRuntime;
+use fvm_ipld_bitfield::BitField;
+use fvm_shared::bigint::Zero;
+use fvm_shared::clock::{ChainEpoch, QuantSpec};
 use fvm_shared::econ::TokenAmount;
+use fvm_shared::sector::SectorSize;
 use std::ops::Neg;
 
 mod util;
@@ -12,6 +18,7 @@ const DEFAULT_SECTOR_EXPIRATION: ChainEpoch = 220;
 
 const PERIOD_OFFSET: ChainEpoch = 100;
 const BIG_BALANCE: u128 = 1_000_000_000_000_000_000_000_000u128;
+const BIG_REWARDS: u128 = 1_000 * 1e18 as u128;
 
 #[test]
 fn cron_on_inactive_state() {
@@ -135,81 +142,95 @@ fn sector_expires_and_repays_fee_debt() {
     assert!(!st.deadline_cron_active);
     check_state_invariants(&rt);
 }
-// 		advanceDeadline(rt, actor, &cronConfig{
-// 			noEnrollment:              true,
-// 			expiredSectorsPowerDelta:  &powerDelta,
-// 			expiredSectorsPledgeDelta: initialPledge.Neg(),
-// 			repaidFeeDebt:             initialPledge, // We repay unlocked IP as fees
-// 		})
-// 		st = getState(rt)
-// 		assert.False(t, st.DeadlineCronActive)
-// 		actor.checkState(rt)
-// 	})
 
-// 	t.Run("detects and penalizes faults", func(t *testing.T) {
-// 		rt := builder.Build(t)
-// 		actor.constructAndVerify(rt)
+#[test]
+fn detects_and_penalizes_faults() {
+    let mut h = ActorHarness::new(PERIOD_OFFSET);
+    let mut rt = h.new_runtime();
+    rt.set_balance(TokenAmount::from(BIG_BALANCE));
+    h.construct_and_verify(&mut rt);
 
-// 		activeSectors := actor.commitAndProveSectors(rt, 2, defaultSectorExpiration, nil, true)
-// 		// advance cron to activate power.
-// 		advanceAndSubmitPoSts(rt, actor, activeSectors...)
-// 		activePower := miner.PowerForSectors(actor.sectorSize, activeSectors)
+    let active_sectors =
+        h.commit_and_prove_sectors(&mut rt, 2, DEFAULT_SECTOR_EXPIRATION as u64, vec![], true);
+    // advance cron to activate power.
+    h.advance_and_submit_posts(&mut rt, &active_sectors);
+    let active_power = power_for_sectors(h.sector_size, &active_sectors);
 
-// 		unprovenSectors := actor.commitAndProveSectors(rt, 1, defaultSectorExpiration, nil, false)
-// 		unprovenPower := miner.PowerForSectors(actor.sectorSize, unprovenSectors)
+    let unproven_sectors =
+        h.commit_and_prove_sectors(&mut rt, 1, DEFAULT_SECTOR_EXPIRATION as u64, vec![], false);
+    let unproven_power = power_for_sectors(h.sector_size, &unproven_sectors);
 
-// 		totalPower := unprovenPower.Add(activePower)
-// 		allSectors := append(activeSectors, unprovenSectors...)
+    let total_power = &unproven_power + &active_power;
+    let all_sectors = [active_sectors.clone(), unproven_sectors].concat();
 
-// 		// add lots of funds so penalties come from vesting funds
-// 		actor.applyRewards(rt, bigRewards, big.Zero())
+    // add lots of funds so penalties come from vesting funds
+    h.apply_rewards(&mut rt, TokenAmount::from(BIG_REWARDS), TokenAmount::zero());
 
-// 		st := getState(rt)
-// 		dlIdx, pIdx, err := st.FindSector(rt.AdtStore(), activeSectors[0].SectorNumber)
-// 		require.NoError(t, err)
+    let st = h.get_state(&rt);
+    let (dl_idx, p_idx) =
+        st.find_sector(&rt.policy, &rt.store, active_sectors[0].sector_number).unwrap();
 
-// 		// advance to next deadline where we expect the first sectors to appear
-// 		dlinfo := actor.deadline(rt)
-// 		for dlinfo.Index != dlIdx {
-// 			dlinfo = advanceDeadline(rt, actor, &cronConfig{})
-// 		}
+    // advance to next deadline where we expect the first sectors to appear
+    let mut dl_info = h.deadline(&rt);
+    while dl_info.index != dl_idx {
+        dl_info = h.advance_deadline(&mut rt, CronConfig::default());
+    }
 
-// 		// Skip to end of the deadline, cron detects sectors as faulty
-// 		activePowerDelta := activePower.Neg()
-// 		advanceDeadline(rt, actor, &cronConfig{
-// 			detectedFaultsPowerDelta: &activePowerDelta,
-// 		})
+    // Skip to end of the deadline, cron detects sectors as faulty
+    let active_power_delta = active_power.neg();
+    h.advance_deadline(
+        &mut rt,
+        CronConfig { detected_faults_power_delta: Some(active_power_delta), ..Default::default() },
+    );
 
-// 		// expect faulty power to be added to state
-// 		deadline := actor.getDeadline(rt, dlIdx)
-// 		assert.True(t, totalPower.Equals(deadline.FaultyPower))
+    // expect faulty power to be added to state
+    let deadline = h.get_deadline(&rt, dl_idx);
+    assert_eq!(total_power, deadline.faulty_power);
 
-// 		// advance 3 deadlines
-// 		advanceDeadline(rt, actor, &cronConfig{})
-// 		advanceDeadline(rt, actor, &cronConfig{})
-// 		dlinfo = advanceDeadline(rt, actor, &cronConfig{})
+    // advance 3 deadlines
+    h.advance_deadline(&mut rt, CronConfig::default());
+    h.advance_deadline(&mut rt, CronConfig::default());
+    dl_info = h.advance_deadline(&mut rt, CronConfig::default());
 
-// 		actor.declareRecoveries(rt, dlIdx, pIdx, sectorInfoAsBitfield(allSectors[1:]), big.Zero())
+    h.declare_recoveries(
+        &mut rt,
+        dl_idx,
+        p_idx,
+        sector_info_as_bitfield(&all_sectors[1..]),
+        TokenAmount::zero(),
+    );
 
-// 		// Skip to end of proving period for sectors, cron detects all sectors as faulty
-// 		for dlinfo.Index != dlIdx {
-// 			dlinfo = advanceDeadline(rt, actor, &cronConfig{})
-// 		}
+    // Skip to end of proving period for sectors, cron detects all sectors as faulty
+    while dl_info.index != dl_idx {
+        dl_info = h.advance_deadline(&mut rt, CronConfig::default());
+    }
 
-// 		// Un-recovered faults (incl failed recovery) are charged as ongoing faults
-// 		ongoingPwr := miner.PowerForSectors(actor.sectorSize, allSectors)
-// 		ongoingPenalty := miner.PledgePenaltyForContinuedFault(actor.epochRewardSmooth, actor.epochQAPowerSmooth, ongoingPwr.QA)
+    // Un-recovered faults (incl failed recovery) are charged as ongoing faults
+    let ongoing_pwr = power_for_sectors(h.sector_size, &all_sectors);
+    let ongoing_penalty = pledge_penalty_for_continued_fault(
+        &h.epoch_reward_smooth,
+        &h.epoch_qa_power_smooth,
+        &ongoing_pwr.qa,
+    );
 
-// 		advanceDeadline(rt, actor, &cronConfig{
-// 			continuedFaultsPenalty: ongoingPenalty,
-// 		})
+    h.advance_deadline(
+        &mut rt,
+        CronConfig { continued_faults_penalty: ongoing_penalty, ..Default::default() },
+    );
 
-// 		// recorded faulty power is unchanged
-// 		deadline = actor.getDeadline(rt, dlIdx)
-// 		assert.True(t, totalPower.Equals(deadline.FaultyPower))
-// 		checkDeadlineInvariants(t, rt.AdtStore(), deadline, st.QuantSpecForDeadline(dlIdx), actor.sectorSize, allSectors)
-// 		actor.checkState(rt)
-// 	})
+    // recorded faulty power is unchanged
+    let deadline = h.get_deadline(&rt, dl_idx);
+    assert_eq!(total_power, deadline.faulty_power);
+
+    check_deadline_invariants(
+        &rt,
+        st.quant_spec_for_deadline(&rt.policy, dl_idx),
+        h.sector_size,
+        &all_sectors,
+        &deadline,
+    );
+    check_state_invariants(&rt);
+}
 
 // 	t.Run("test cron run trigger faults", func(t *testing.T) {
 // 		rt := builder.Build(t)
@@ -252,3 +273,35 @@ fn sector_expires_and_repays_fee_debt() {
 // 		actor.checkState(rt)
 // 	})
 // }
+
+fn sector_info_as_bitfield(sectors: &[SectorOnChainInfo]) -> BitField {
+    let mut bf = BitField::new();
+    for sector in sectors {
+        bf.set(sector.sector_number);
+    }
+    bf
+}
+
+// check the deadline's invariants, returning all contained sectors, faults,
+// recoveries, terminations, and partition/sector assignments.
+pub fn check_deadline_invariants(
+    rt: &MockRuntime,
+    quant_spec: QuantSpec,
+    sector_size: SectorSize,
+    sectors: &[SectorOnChainInfo],
+    deadline: &Deadline,
+) -> DeadlineStateSummary {
+    let acc = MessageAccumulator::default();
+    let summary = check_deadline_state_invariants(
+        deadline,
+        &rt.store,
+        quant_spec,
+        sector_size,
+        &sectors_as_map(sectors),
+        &acc,
+    );
+
+    assert!(acc.is_empty(), "{}", acc.messages().join("\n"));
+
+    summary
+}
