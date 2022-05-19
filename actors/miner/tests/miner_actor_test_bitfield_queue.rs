c974d4e50d672df88b06421fa20c13db8e7fbb49
Josh Jones <joshua.jones@chainsafe.io>
2022-05-19 10:17:19 -0500

    changes requested from code review

--- actors/miner/tests/miner_actor_test_bitfield_queue.rs
+++ actors/miner/tests/miner_actor_test_bitfield_queue.rs
@@ -13,6 +13,7 @@ use util::*;
 
 const TEST_AMT_BITWIDTH: u32 = 3;
 
+#[derive(Default)]
 pub struct BQExpectation {
     expected: HashMap<ChainEpoch, Vec<u64>>,
 }
@@ -47,8 +48,7 @@ fn adds_values_to_empty_queue() {
 
     queue.add_to_queue_values(epoch, values).unwrap();
 
-    let expected: HashMap<_, Vec<u64>> = HashMap::new();
-    let mut bq_expectation = BQExpectation { expected };
+    let mut bq_expectation = BQExpectation::default();
     bq_expectation.add(epoch, values.to_vec()).equals(&mut queue);
 }
 
@@ -58,17 +58,13 @@ fn adds_bitfield_to_empty_queue() {
     let rt = h.new_runtime();
     let mut queue = empty_bitfield_queue(&rt, TEST_AMT_BITWIDTH);
 
-    let ranges: Vec<Range<u64>> = vec![Range { start: 1, end: 5 }];
-    let values = BitField::from_ranges(Ranges::new(ranges.iter().cloned()));
+    let values = BitField::from_ranges(Ranges::new([1..5]));
     let epoch = ChainEpoch::from(42);
 
     queue.add_to_queue(epoch, &values).unwrap();
 
-    let expected: HashMap<_, Vec<u64>> = HashMap::new();
-    let mut bq_expectation = BQExpectation { expected };
-    bq_expectation
-        .add(epoch, ranges[0].clone().map(u64::from).collect::<Vec<u64>>())
-        .equals(&mut queue);
+    let mut bq_expectation = BQExpectation::default();
+    bq_expectation.add(epoch, values.iter().collect()).equals(&mut queue);
 }
 
 #[test]
@@ -86,8 +82,7 @@ fn quantizes_added_epochs_according_to_quantization_spec() {
         queue.add_to_queue_values(val as i64, [val]).unwrap();
     }
 
-    let expected: HashMap<_, Vec<u64>> = HashMap::new();
-    let mut bq_expectation = BQExpectation { expected };
+    let mut bq_expectation = BQExpectation::default();
     // expect values to only be set on quantization boundaries
     bq_expectation
         .add(ChainEpoch::from(3), [0, 2, 3].to_vec())
@@ -107,8 +102,7 @@ fn merges_values_within_same_epoch() {
     queue.add_to_queue_values(epoch, [1, 3].to_vec()).unwrap();
     queue.add_to_queue_values(epoch, [2, 4].to_vec()).unwrap();
 
-    let expected: HashMap<_, Vec<u64>> = HashMap::new();
-    let mut bq_expectation = BQExpectation { expected };
+    let mut bq_expectation = BQExpectation::default();
     bq_expectation.add(epoch, [1, 2, 3, 4].to_vec()).equals(&mut queue);
 }
 
@@ -124,8 +118,7 @@ fn adds_values_to_different_epochs() {
     queue.add_to_queue_values(epoch1, [1, 3].to_vec()).unwrap();
     queue.add_to_queue_values(epoch2, [2, 4].to_vec()).unwrap();
 
-    let expected: HashMap<_, Vec<u64>> = HashMap::new();
-    let mut bq_expectation = BQExpectation { expected };
+    let mut bq_expectation = BQExpectation::default();
     bq_expectation.add(epoch1, [1, 3].to_vec()).add(epoch2, [2, 4].to_vec()).equals(&mut queue);
 }
 
@@ -135,11 +128,13 @@ fn pop_until_from_empty_queue_returns_empty_bitfield() {
     let rt = h.new_runtime();
     let mut queue = empty_bitfield_queue(&rt, TEST_AMT_BITWIDTH);
 
-    let next = queue.pop_until(42).unwrap();
+    let (next, modified) = queue.pop_until(42).unwrap();
 
     //no values are returned
-    let count = next.0.len();
+    let count = next.len();
     assert_eq!(0, count);
+    // modified is false
+    assert!(!modified);
 }
 
 #[test]
@@ -154,14 +149,15 @@ fn pop_until_does_nothing_if_until_parameter_before_first_value() {
     queue.add_to_queue_values(epoch1, [1, 3].to_vec()).unwrap();
     queue.add_to_queue_values(epoch2, [2, 4].to_vec()).unwrap();
 
-    let next = queue.pop_until(epoch1 - 1).unwrap();
+    let (next, modified) = queue.pop_until(epoch1 - 1).unwrap();
 
-    // no values are returned
-    let count = next.0.len();
+    //no values are returned
+    let count = next.len();
     assert_eq!(0, count);
+    // modified is false
+    assert!(!modified);
 
-    let expected: HashMap<_, Vec<u64>> = HashMap::new();
-    let mut bq_expectation = BQExpectation { expected };
+    let mut bq_expectation = BQExpectation::default();
     // queue remains the same
     bq_expectation.add(epoch1, [1, 3].to_vec()).add(epoch2, [2, 4].to_vec()).equals(&mut queue);
 }
@@ -182,35 +178,36 @@ fn pop_until_removes_and_returns_entries_before_and_including_target_epoch() {
     queue.add_to_queue_values(epoch3, [6, 7, 8].to_vec()).unwrap();
     queue.add_to_queue_values(epoch4, [2, 4].to_vec()).unwrap();
 
-    let next = queue.pop_until(epoch2).unwrap();
+    let (next, modified) = queue.pop_until(epoch2).unwrap();
+    // modified should be true to indicate queue has changed
+    assert!(modified);
 
     // values from first two epochs are returned
-    assert_bitfield_equals(&next.0, &[1, 3, 5]);
+    assert_bitfield_equals(&next, &[1, 3, 5]);
 
-    let expected: HashMap<_, Vec<u64>> = HashMap::new();
-    let mut bq_expectation = BQExpectation { expected };
+    let mut bq_expectation = BQExpectation::default();
     // queue only contains remaining values
     bq_expectation.add(epoch3, [6, 7, 8].to_vec()).add(epoch4, [2, 4].to_vec()).equals(&mut queue);
 
     // subsequent call to epoch less than next does nothing
-    let next = queue.pop_until(epoch3 - 1).unwrap();
+    let (next, modified) = queue.pop_until(epoch3 - 1).unwrap();
+    assert!(!modified);
 
     // no values are returned
-    assert_bitfield_equals(&next.0, &[]);
+    assert_bitfield_equals(&next, &[]);
 
-    let expected: HashMap<_, Vec<u64>> = HashMap::new();
-    let mut bq_expectation = BQExpectation { expected };
+    let mut bq_expectation = BQExpectation::default();
     // queue only contains remaining values
     bq_expectation.add(epoch3, [6, 7, 8].to_vec()).add(epoch4, [2, 4].to_vec()).equals(&mut queue);
 
     // popping the rest of the queue gets the rest of the values
-    let next = queue.pop_until(epoch4).unwrap();
+    let (next, modified) = queue.pop_until(epoch4).unwrap();
+    assert!(modified);
 
     // rest of values are returned
-    assert_bitfield_equals(&next.0, &[2, 4, 6, 7, 8]);
+    assert_bitfield_equals(&next, &[2, 4, 6, 7, 8]);
 
-    let expected: HashMap<_, Vec<u64>> = HashMap::new();
-    let mut bq_expectation = BQExpectation { expected };
+    let mut bq_expectation = BQExpectation::default();
     // queue is now empty
     bq_expectation.equals(&mut queue);
 }
@@ -227,17 +224,11 @@ fn cuts_elements() {
     queue.add_to_queue_values(epoch1, [1, 2, 3, 4, 99].to_vec()).unwrap();
     queue.add_to_queue_values(epoch2, [5, 6].to_vec()).unwrap();
 
-    let ranges: Vec<Range<u64>> = vec![
-        Range { start: 2, end: 3 },
-        Range { start: 4, end: 5 },
-        Range { start: 5, end: 6 },
-        Range { start: 6, end: 7 },
-    ];
+    let ranges: Vec<Range<u64>> = vec![Range { start: 2, end: 3 }, Range { start: 4, end: 7 }];
     let to_cut = BitField::from_ranges(Ranges::new(ranges.iter().cloned()));
     queue.cut(&to_cut).unwrap();
 
-    let expected: HashMap<_, Vec<u64>> = HashMap::new();
-    let mut bq_expectation = BQExpectation { expected };
+    let mut bq_expectation = BQExpectation::default();
     // 3 shifts down to 2, 99 down to 95
     bq_expectation.add(epoch1, [1, 2, 95].to_vec()).equals(&mut queue);
 }
@@ -252,8 +243,7 @@ fn adds_empty_bitfield_to_queue() {
 
     queue.add_to_queue(epoch, &BitField::new()).unwrap();
 
-    let expected: HashMap<_, Vec<u64>> = HashMap::new();
-    let mut bq_expectation = BQExpectation { expected };
+    let mut bq_expectation = BQExpectation::default();
     // ensures we don't add an empty entry
     bq_expectation.equals(&mut queue);
 }
