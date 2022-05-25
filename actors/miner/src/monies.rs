4db49f3b21d1d53b41b14c4de1dfa14b553bb91b
Hubert Bugaj <hubert@chainsafe.io>
2022-05-25 15:19:52 +0200

    miner tests part 49

--- actors/miner/src/monies.rs
+++ actors/miner/src/monies.rs
@@ -4,7 +4,7 @@
 use std::cmp::{self, max};
 
 use fil_actors_runtime::network::EPOCHS_IN_DAY;
-use fil_actors_runtime::EXPECTED_LEADERS_PER_EPOCH;
+use fil_actors_runtime::{EXPECTED_LEADERS_PER_EPOCH, ONE_NANO_FIL};
 use fvm_shared::bigint::num_integer::div_floor;
 use fvm_shared::bigint::{BigInt, Integer};
 use fvm_shared::clock::ChainEpoch;
@@ -17,7 +17,7 @@ use lazy_static::lazy_static;
 use num_traits::Zero;
 
 use super::{VestSpec, REWARD_VESTING_SPEC};
-use crate::detail::expected_reward_for_power_clamped_at_atto_fil;
+use crate::detail::*;
 
 /// Projection period of expected sector block reward for deposit required to pre-commit a sector.
 /// This deposit is lost if the pre-commitment is not timely followed up by a commitment proof.
@@ -108,6 +108,10 @@ pub fn expected_reward_for_power(
 
 pub mod detail {
     use super::*;
+
+    lazy_static! {
+        pub static ref BATCH_BALANCER: BigInt = BigInt::from(ONE_NANO_FIL) * BigInt::from(5);
+    }
 
     // BR but zero values are clamped at 1 attofil
     // Some uses of BR (PCD, IP) require a strictly positive value for BR derived values so
@@ -291,9 +295,9 @@ pub fn locked_reward_from_reward(reward: TokenAmount) -> (TokenAmount, &'static 
 
 lazy_static! {
     static ref ESTIMATED_SINGLE_PROVE_COMMIT_GAS_USAGE: BigInt = BigInt::from(49299973);
-    static ref ESTIMATED_SINGLE_PRE_COMMIT_GAS_USAGE: BigInt = BigInt::from(16433324);    static ref BATCH_DISCOUNT_NUM: BigInt = BigInt::from(1);
+    static ref ESTIMATED_SINGLE_PRE_COMMIT_GAS_USAGE: BigInt = BigInt::from(16433324);
+    static ref BATCH_DISCOUNT_NUM: BigInt = BigInt::from(1);
     static ref BATCH_DISCOUNT_DENOM: BigInt = BigInt::from(20);
-    static ref BATCH_BALANCER: BigInt = BigInt::from(1_000_000_000) * BigInt::from(5); // 5 * 1 nanoFIL
 }
 
 pub fn aggregate_prove_commit_network_fee(
