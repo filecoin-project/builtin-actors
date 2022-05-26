38e7345be52071d1796755566f13d64e85a39510
connor mullett <connormullett@gmail.com>
2022-05-26 13:12:56 -0400

    scaffold test cases

---
+++ actors/miner/tests/batch_method_network_fees_test.rs
@@ -0,0 +1,14 @@
+#[test]
+fn insufficient_funds_for_aggregated_prove_commit_network_fee() {}
+
+#[test]
+fn insufficient_funds_for_batch_precommit_network_fee() {}
+
+#[test]
+fn insufficient_funds_for_batch_precommit_in_combination_of_fee_debt_and_network_fee() {}
+
+#[test]
+fn enough_funds_for_fee_debt_and_network_fee_but_not_for_pcd() {}
+
+#[test]
+fn enough_funds_for_everything() {}
