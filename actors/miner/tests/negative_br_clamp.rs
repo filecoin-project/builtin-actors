172401b5b47df6ee185883c1ea59d838841638bd
David Himmelstrup <lemmih@gmail.com>
2022-06-01 15:04:38 +0200

    Assert power estimate has a negative rate of change.

--- actors/miner/tests/negative_br_clamp.rs
+++ actors/miner/tests/negative_br_clamp.rs
@@ -1,7 +1,6 @@
 use fil_actor_miner::expected_reward_for_power;
 use fvm_shared::bigint::{BigInt, Zero};
 use fvm_shared::{econ::TokenAmount, smooth::FilterEstimate};
-use num_traits::sign::Signed;
 use std::ops::Neg;
 
 #[test]
@@ -11,8 +10,8 @@ fn negative_br_clamp() {
     let network_qa_power = TokenAmount::from(1_u64 << 10);
     let power_rate_of_change = TokenAmount::from(1_u64 << 10).neg();
     let reward_estimate = FilterEstimate::new(epoch_target_reward, BigInt::zero());
-    let power_estimate = FilterEstimate::new(network_qa_power, power_rate_of_change);
-    assert!(power_estimate.estimate().is_negative());
+    let power_estimate = FilterEstimate::new(network_qa_power.clone(), power_rate_of_change);
+    assert!(power_estimate.extrapolate(4) < network_qa_power);
 
     let four_br = expected_reward_for_power(&reward_estimate, &power_estimate, &qa_sector_power, 4);
     assert!(four_br.is_zero());
