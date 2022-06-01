e2320ef72ab200e2c3c33bbebaa9ba5192643fbf
David Himmelstrup <lemmih@gmail.com>
2022-06-01 15:02:30 +0200

    Assert is negative.

--- actors/miner/tests/negative_br_clamp.rs
+++ actors/miner/tests/negative_br_clamp.rs
@@ -1,6 +1,7 @@
 use fil_actor_miner::expected_reward_for_power;
 use fvm_shared::bigint::{BigInt, Zero};
 use fvm_shared::{econ::TokenAmount, smooth::FilterEstimate};
+use num_traits::sign::Signed;
 use std::ops::Neg;
 
 #[test]
@@ -11,6 +12,7 @@ fn negative_br_clamp() {
     let power_rate_of_change = TokenAmount::from(1_u64 << 10).neg();
     let reward_estimate = FilterEstimate::new(epoch_target_reward, BigInt::zero());
     let power_estimate = FilterEstimate::new(network_qa_power, power_rate_of_change);
+    assert!(power_estimate.estimate().is_negative());
 
     let four_br = expected_reward_for_power(&reward_estimate, &power_estimate, &qa_sector_power, 4);
     assert!(four_br.is_zero());
