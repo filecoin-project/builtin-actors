0783967984e2fbb5a1d62acaa5908afacaf3080c
zenground0 <ZenGround0@users.noreply.github.com>
2022-06-03 16:12:08 -0400

    post commit happy path wip

---
+++ test_vm/tests/commit_post_test.rs
@@ -0,0 +1,206 @@
+use cid::Cid;
+use fil_actor_init::ExecReturn;
+use fil_actor_miner::{
+    aggregate_pre_commit_network_fee, max_prove_commit_duration, Method as MinerMethod,
+    PreCommitSectorBatchParams, SectorPreCommitInfo, SectorPreCommitOnChainInfo,
+    State as MinerState,
+};
+use fil_actor_multisig::{
+    compute_proposal_hash, Method as MsigMethod, ProposeParams, RemoveSignerParams,
+    State as MsigState, SwapSignerParams, Transaction, TxnID, TxnIDParams,
+};
+use fil_actor_power::{CreateMinerParams, CreateMinerReturn, Method as PowerMethod};
+use fil_actor_reward::Method as RewardMethod;
+use fil_actors_runtime::cbor::serialize;
+use fil_actors_runtime::runtime::{DomainSeparationTag, Policy, Runtime, RuntimePolicy};
+use fil_actors_runtime::{
+    make_map_with_root, INIT_ACTOR_ADDR, REWARD_ACTOR_ADDR, STORAGE_POWER_ACTOR_ADDR,
+    SYSTEM_ACTOR_ADDR,
+};
+use fil_actors_runtime::{test_utils::*, BURNT_FUNDS_ACTOR_ADDR};
+use fvm_ipld_blockstore::MemoryBlockstore;
+use fvm_ipld_encoding::{BytesDe, RawBytes};
+use fvm_shared::address::Address;
+use fvm_shared::bigint::Zero;
+use fvm_shared::clock::{ChainEpoch, QuantSpec, NO_QUANTIZATION};
+use fvm_shared::commcid::{FIL_COMMITMENT_SEALED, FIL_COMMITMENT_UNSEALED};
+use fvm_shared::econ::TokenAmount;
+use fvm_shared::error::ExitCode;
+use fvm_shared::sector::{RegisteredPoStProof, RegisteredSealProof, SectorNumber};
+use fvm_shared::METHOD_SEND;
+use integer_encoding::VarInt;
+use multihash::derive::Multihash;
+use multihash::MultihashDigest;
+use num_traits::sign::Signed;
+use std::collections::HashSet;
+use std::iter::FromIterator;
+use test_vm::util::{apply_code, apply_ok, create_accounts};
+use test_vm::{ExpectInvocation, TEST_FAUCET_ADDR, VM};
+
+#[test]
+fn commit_post_flow_happy_path() {
+    let store = MemoryBlockstore::new();
+    let mut v = VM::new_with_singletons(&store);
+    let addrs = create_accounts(&v, 2, TokenAmount::from(10_000e18 as u64));
+    let seal_proof = RegisteredSealProof::StackedDRG32GiBV1P1;
+    let (owner, worker) = (addrs[0], addrs[0]);
+    let (id_addr, _) =
+        create_miner(&mut v, owner, worker, seal_proof.registered_window_post_proof().unwrap());
+    let mut v = v.with_epoch(200);
+
+    let sector_number_base: SectorNumber = 100;
+    let precommits =
+        precommit_sectors(&mut v, 1, 1, worker, id_addr, seal_proof, sector_number_base, true, -1);
+
+    let balances = v.get_miner_balance(id_addr);
+    assert!(!balances.pre_commit_deposit.is_negative());
+
+    let prove_time =
+        v.get_epoch() + max_prove_commit_duration(&Policy::default(), seal_proof).unwrap();
+}
+
+fn create_miner(
+    v: &mut VM,
+    owner: Address,
+    worker: Address,
+    post_proof_type: RegisteredPoStProof,
+) -> (Address, Address) {
+    let multiaddrs = vec![BytesDe("multiaddr".as_bytes().to_vec())];
+    let peer_id = "miner".as_bytes().to_vec();
+    let params = CreateMinerParams {
+        owner,
+        worker: worker,
+        window_post_proof_type: post_proof_type,
+        peer: peer_id.clone(),
+        multiaddrs: multiaddrs.clone(),
+    };
+
+    let res: CreateMinerReturn = v
+        .apply_message(
+            owner,
+            *STORAGE_POWER_ACTOR_ADDR,
+            TokenAmount::from(1000u32),
+            PowerMethod::CreateMiner as u64,
+            params.clone(),
+        )
+        .unwrap()
+        .ret
+        .deserialize()
+        .unwrap();
+    (res.id_address, res.robust_address)
+}
+
+fn precommit_sectors(
+    v: &mut VM,
+    count: u64,
+    batch_size: i64,
+    worker: Address,
+    maddr: Address,
+    seal_proof: RegisteredSealProof,
+    sector_number_base: SectorNumber,
+    expect_cron_enroll: bool,
+    expiration: ChainEpoch,
+) -> Vec<SectorPreCommitOnChainInfo> {
+    let invocs_common = || -> Vec<ExpectInvocation> {
+        vec![
+            ExpectInvocation {
+                to: *REWARD_ACTOR_ADDR,
+                method: RewardMethod::ThisEpochReward as u64,
+                ..Default::default()
+            },
+            ExpectInvocation {
+                to: *STORAGE_POWER_ACTOR_ADDR,
+                method: PowerMethod::CurrentTotalPower as u64,
+                ..Default::default()
+            },
+        ]
+    };
+    let invoc_first = || -> ExpectInvocation {
+        ExpectInvocation {
+            to: *STORAGE_POWER_ACTOR_ADDR,
+            method: PowerMethod::EnrollCronEvent as u64,
+            ..Default::default()
+        }
+    };
+    let invoc_net_fee = |fee: TokenAmount| -> ExpectInvocation {
+        ExpectInvocation {
+            to: *BURNT_FUNDS_ACTOR_ADDR,
+            method: METHOD_SEND,
+            value: Some(fee),
+            ..Default::default()
+        }
+    };
+
+    let mut sector_idx = 0u64;
+    while sector_idx < count {
+        let msg_sector_idx_base = sector_idx;
+        let mut invocs = invocs_common();
+
+        let mut param_sectors = Vec::<SectorPreCommitInfo>::new();
+        let mut j = 0;
+        while j < batch_size && sector_idx < count {
+            let sector_number = sector_number_base + sector_idx;
+            param_sectors.push(SectorPreCommitInfo {
+                seal_proof,
+                sector_number,
+                sealed_cid: make_sealed_cid(format!("sn: {}", sector_number).as_bytes()),
+                seal_rand_epoch: v.get_epoch() - 1,
+                deal_ids: vec![],
+                ..Default::default()
+            });
+            sector_idx += 1;
+            j += 1;
+        }
+        if param_sectors.len() > 1 {
+            invocs.push(invoc_net_fee(aggregate_pre_commit_network_fee(
+                param_sectors.len() as i64,
+                &TokenAmount::zero(),
+            )));
+        }
+        if expect_cron_enroll && msg_sector_idx_base == 0 {
+            invocs.push(invoc_first());
+        }
+        apply_ok(
+            v,
+            worker,
+            maddr,
+            TokenAmount::zero(),
+            MinerMethod::PreCommitSectorBatch as u64,
+            PreCommitSectorBatchParams { sectors: param_sectors.clone() },
+        );
+        let expect = ExpectInvocation {
+            to: maddr,
+            method: MinerMethod::PreCommitSectorBatch as u64,
+            params: Some(
+                serialize(
+                    &PreCommitSectorBatchParams { sectors: param_sectors },
+                    "precommit batch params",
+                )
+                .unwrap(),
+            ),
+            subinvocs: Some(invocs),
+            ..Default::default()
+        };
+        expect.matches(v.take_invocations().last().unwrap())
+    }
+    // extract chain state
+    let mstate = v.get_state::<MinerState>(maddr).unwrap();
+    (0..count).map(|i| mstate.get_precommitted_sector(v.store, i).unwrap().unwrap()).collect()
+}
+
+// XXX: split this out somwhere shared between here and the miner unit test utils
+// multihash library doesn't support poseidon hashing, so we fake it
+#[derive(Clone, Copy, Debug, Eq, Multihash, PartialEq)]
+#[mh(alloc_size = 64)]
+enum MhCode {
+    #[mh(code = 0xb401, hasher = multihash::Sha2_256)]
+    PoseidonFake,
+    #[mh(code = 0x1012, hasher = multihash::Sha2_256)]
+    Sha256TruncPaddedFake,
+}
+
+fn make_sealed_cid(input: &[u8]) -> Cid {
+    // Note: multihash library doesn't support Poseidon hashing, so we fake it
+    let h = MhCode::PoseidonFake.digest(input);
+    Cid::new_v1(FIL_COMMITMENT_SEALED, h)
+}
