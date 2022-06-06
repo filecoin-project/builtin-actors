24faf97a39e7dd9136a4d3022b5970299ae83071
zenground0 <ZenGround0@users.noreply.github.com>
2022-06-05 22:40:44 -0600

    Test complete

--- test_vm/tests/commit_post_test.rs
+++ test_vm/tests/commit_post_test.rs
@@ -1,33 +1,39 @@
-use fil_actor_miner::{
-    aggregate_pre_commit_network_fee, max_prove_commit_duration, Method as MinerMethod,
-    PreCommitSectorBatchParams, ProveCommitSectorParams,
-    SectorPreCommitInfo, SectorPreCommitOnChainInfo, State as MinerState, DeadlineInfo, new_deadline_info_from_offset_and_epoch,
-    PoStPartition, power_for_sector, PowerPair, SubmitWindowedPoStParams,
-};
-use fil_actor_market::{Method as MarketMethod};
-use fil_actor_power::{CreateMinerParams, CreateMinerReturn, Method as PowerMethod, State as PowerState};
-use fil_actor_reward::Method as RewardMethod;
 use fil_actor_cron::Method as CronMethod;
-use fil_actors_runtime::cbor::serialize;
-use fil_actors_runtime::runtime::{Policy};
-use fil_actors_runtime::{
-    REWARD_ACTOR_ADDR, STORAGE_POWER_ACTOR_ADDR,
-    SYSTEM_ACTOR_ADDR, STORAGE_MARKET_ACTOR_ADDR, CRON_ACTOR_ADDR,
+use fil_actor_market::Method as MarketMethod;
+use fil_actor_miner::{
+    aggregate_pre_commit_network_fee, max_prove_commit_duration,
+    new_deadline_info_from_offset_and_epoch, power_for_sector, DeadlineInfo, Method as MinerMethod,
+    PoStPartition, PowerPair, PreCommitSectorBatchParams, ProveCommitSectorParams,
+    SectorPreCommitInfo, SectorPreCommitOnChainInfo, State as MinerState, SubmitWindowedPoStParams,
 };
+use fil_actor_power::{
+    CreateMinerParams, CreateMinerReturn, Method as PowerMethod, State as PowerState,
+    UpdateClaimedPowerParams,
+};
+use fil_actor_reward::Method as RewardMethod;
+use fil_actors_runtime::cbor::serialize;
+use fil_actors_runtime::runtime::Policy;
 use fil_actors_runtime::{test_utils::*, BURNT_FUNDS_ACTOR_ADDR};
+use fil_actors_runtime::{
+    CRON_ACTOR_ADDR, REWARD_ACTOR_ADDR, STORAGE_MARKET_ACTOR_ADDR, STORAGE_POWER_ACTOR_ADDR,
+    SYSTEM_ACTOR_ADDR,
+};
+use fvm_ipld_bitfield::BitField;
 use fvm_ipld_blockstore::MemoryBlockstore;
 use fvm_ipld_encoding::{BytesDe, RawBytes};
-use fvm_ipld_bitfield::{BitField};
 use fvm_shared::address::Address;
 use fvm_shared::bigint::Zero;
-use fvm_shared::clock::{ChainEpoch};
+use fvm_shared::clock::ChainEpoch;
 use fvm_shared::econ::TokenAmount;
 use fvm_shared::error::ExitCode;
-use fvm_shared::sector::{RegisteredPoStProof, RegisteredSealProof, SectorNumber, StoragePower, PoStProof};
+use fvm_shared::randomness::Randomness;
+use fvm_shared::sector::{
+    PoStProof, RegisteredPoStProof, RegisteredSealProof, SectorNumber, StoragePower,
+};
 use fvm_shared::METHOD_SEND;
 use num_traits::sign::Signed;
 use test_vm::util::{apply_ok, create_accounts};
-use test_vm::{ExpectInvocation, VM};
+use test_vm::{ExpectInvocation, TEST_VM_RAND_STRING, VM};
 
 #[test]
 fn commit_post_flow_happy_path() {
@@ -71,45 +77,71 @@ fn commit_post_flow_happy_path() {
         to: id_addr,
         method: MinerMethod::ProveCommitSector as u64,
         params: Some(prove_params_ser),
-        subinvocs: Some(vec![ExpectInvocation{to: *STORAGE_MARKET_ACTOR_ADDR, method: MarketMethod::ComputeDataCommitment as u64, ..Default::default()},ExpectInvocation{to: *STORAGE_POWER_ACTOR_ADDR, method: PowerMethod::SubmitPoRepForBulkVerify as u64, ..Default::default()}]),
+        subinvocs: Some(vec![
+            ExpectInvocation {
+                to: *STORAGE_MARKET_ACTOR_ADDR,
+                method: MarketMethod::ComputeDataCommitment as u64,
+                ..Default::default()
+            },
+            ExpectInvocation {
+                to: *STORAGE_POWER_ACTOR_ADDR,
+                method: PowerMethod::SubmitPoRepForBulkVerify as u64,
+                ..Default::default()
+            },
+        ]),
         ..Default::default()
-    }.matches(v.take_invocations().last().unwrap());
-    let res = v.apply_message(*SYSTEM_ACTOR_ADDR, *CRON_ACTOR_ADDR, TokenAmount::zero(), CronMethod::EpochTick as u64, RawBytes::default()).unwrap();
+    }
+    .matches(v.take_invocations().last().unwrap());
+    let res = v
+        .apply_message(
+            *SYSTEM_ACTOR_ADDR,
+            *CRON_ACTOR_ADDR,
+            TokenAmount::zero(),
+            CronMethod::EpochTick as u64,
+            RawBytes::default(),
+        )
+        .unwrap();
     assert_eq!(ExitCode::OK, res.code);
     ExpectInvocation {
         to: *CRON_ACTOR_ADDR,
         method: CronMethod::EpochTick as u64,
-        subinvocs: Some(vec![ExpectInvocation{
-            to: *STORAGE_POWER_ACTOR_ADDR,
-            method: PowerMethod::OnEpochTickEnd as u64,
-            subinvocs: Some(vec![
-                ExpectInvocation{
-                    to: *REWARD_ACTOR_ADDR,
-                    method: RewardMethod::ThisEpochReward as u64,
-                    ..Default::default()
-                },
-                ExpectInvocation{
-                    to: id_addr,
-                    method:MinerMethod::ConfirmSectorProofsValid as u64, 
-                    subinvocs: Some(vec![ExpectInvocation{to: *STORAGE_POWER_ACTOR_ADDR, method: PowerMethod::UpdatePledgeTotal as u64, ..Default::default()}]),
-                    ..Default::default()
-                },
-                ExpectInvocation{
-                    to: *REWARD_ACTOR_ADDR, 
-                    method: RewardMethod::UpdateNetworkKPI as u64,
-                    ..Default::default()
-                },
-            ]),
-            ..Default::default()
+        subinvocs: Some(vec![
+            ExpectInvocation {
+                to: *STORAGE_POWER_ACTOR_ADDR,
+                method: PowerMethod::OnEpochTickEnd as u64,
+                subinvocs: Some(vec![
+                    ExpectInvocation {
+                        to: *REWARD_ACTOR_ADDR,
+                        method: RewardMethod::ThisEpochReward as u64,
+                        ..Default::default()
+                    },
+                    ExpectInvocation {
+                        to: id_addr,
+                        method: MinerMethod::ConfirmSectorProofsValid as u64,
+                        subinvocs: Some(vec![ExpectInvocation {
+                            to: *STORAGE_POWER_ACTOR_ADDR,
+                            method: PowerMethod::UpdatePledgeTotal as u64,
+                            ..Default::default()
+                        }]),
+                        ..Default::default()
+                    },
+                    ExpectInvocation {
+                        to: *REWARD_ACTOR_ADDR,
+                        method: RewardMethod::UpdateNetworkKPI as u64,
+                        ..Default::default()
+                    },
+                ]),
+                ..Default::default()
             },
-            ExpectInvocation{
+            ExpectInvocation {
                 to: *STORAGE_MARKET_ACTOR_ADDR,
                 method: MarketMethod::CronTick as u64,
                 ..Default::default()
-            }
+            },
         ]),
         ..Default::default()
-    }.matches(v.take_invocations().last().unwrap());
+    }
+    .matches(v.take_invocations().last().unwrap());
     // pcd is released ip is added
     let balances = v.get_miner_balance(id_addr);
     assert!(balances.initial_pledge > TokenAmount::zero());
@@ -124,14 +156,19 @@ fn commit_post_flow_happy_path() {
     // submit post
     let st = v.get_state::<MinerState>(id_addr).unwrap();
     let sector = st.get_sector(v.store, sector_number).unwrap().unwrap();
-    let partitions = vec![PoStPartition{index: p_idx, skipped: fvm_ipld_bitfield::UnvalidatedBitField::Validated(BitField::new())}];
+    let partitions = vec![PoStPartition {
+        index: p_idx,
+        skipped: fvm_ipld_bitfield::UnvalidatedBitField::Validated(BitField::new()),
+    }];
     let sector_power = power_for_sector(seal_proof.sector_size().unwrap(), &sector);
     submit_windowed_post(&v, worker, id_addr, dline_info, partitions, sector_power);
     let balances = v.get_miner_balance(id_addr);
     assert!(balances.initial_pledge > TokenAmount::zero());
     let p_st = v.get_state::<PowerState>(*STORAGE_POWER_ACTOR_ADDR).unwrap();
-    assert_eq!(StoragePower::from(seal_proof.sector_size().unwrap() as u64), p_st.total_bytes_committed);
-
+    assert_eq!(
+        StoragePower::from(seal_proof.sector_size().unwrap() as u64),
+        p_st.total_bytes_committed
+    );
 }
 
 fn create_miner(
@@ -275,40 +312,66 @@ fn precommit_sectors(
         .collect()
 }
 
-fn advance_by_deadline_to_epoch<'bs>(v: VM<'bs>, maddr: Address, e: ChainEpoch) -> (VM<'bs>, DeadlineInfo) {
-    advance_by_deadline(v, maddr, |dline_info| {dline_info.close <= e})
+fn advance_by_deadline_to_epoch<'bs>(
+    v: VM<'bs>,
+    maddr: Address,
+    e: ChainEpoch,
+) -> (VM<'bs>, DeadlineInfo) {
+    advance_by_deadline(v, maddr, |dline_info| dline_info.close <= e)
 }
 
-fn advance_by_deadline_to_index<'bs>(v: VM<'bs>, maddr: Address, i: u64) -> (VM<'bs>, DeadlineInfo) {
-    advance_by_deadline(v, maddr, |dline_info| {dline_info.index != i})
+fn advance_by_deadline_to_index<'bs>(
+    v: VM<'bs>,
+    maddr: Address,
+    i: u64,
+) -> (VM<'bs>, DeadlineInfo) {
+    advance_by_deadline(v, maddr, |dline_info| dline_info.index != i)
 }
 
-fn advance_to_proving_deadline<'bs>(v: VM<'bs>, maddr: Address, s: SectorNumber) -> (DeadlineInfo, u64, VM<'bs>) {
+fn advance_to_proving_deadline<'bs>(
+    v: VM<'bs>,
+    maddr: Address,
+    s: SectorNumber,
+) -> (DeadlineInfo, u64, VM<'bs>) {
     let (d, p) = sector_deadline(&v, maddr, s);
     let (v, dline_info) = advance_by_deadline_to_index(v, maddr, d);
     let v = v.with_epoch(dline_info.open);
     (dline_info, p, v)
 }
 
-fn advance_by_deadline<'bs, F>(mut v: VM<'bs>, maddr: Address, more: F) -> (VM<'bs>, DeadlineInfo) where
-    F: Fn(DeadlineInfo) -> bool {
+fn advance_by_deadline<'bs, F>(mut v: VM<'bs>, maddr: Address, more: F) -> (VM<'bs>, DeadlineInfo)
+where
+    F: Fn(DeadlineInfo) -> bool,
+{
     loop {
         let dline_info = miner_dline_info(&v, maddr);
         if !more(dline_info) {
-            return (v, dline_info)
+            return (v, dline_info);
         }
         v = v.with_epoch(dline_info.last());
 
-        let res = v.apply_message(*SYSTEM_ACTOR_ADDR, *CRON_ACTOR_ADDR, TokenAmount::zero(), CronMethod::EpochTick as u64, RawBytes::default()).unwrap();
+        let res = v
+            .apply_message(
+                *SYSTEM_ACTOR_ADDR,
+                *CRON_ACTOR_ADDR,
+                TokenAmount::zero(),
+                CronMethod::EpochTick as u64,
+                RawBytes::default(),
+            )
+            .unwrap();
         assert_eq!(ExitCode::OK, res.code);
         let next = v.get_epoch() + 1;
         v = v.with_epoch(next);
-    };
+    }
 }
 
 fn miner_dline_info(v: &VM, m: Address) -> DeadlineInfo {
     let st = v.get_state::<MinerState>(m).unwrap();
-    new_deadline_info_from_offset_and_epoch(&Policy::default(), st.proving_period_start, v.get_epoch())
+    new_deadline_info_from_offset_and_epoch(
+        &Policy::default(),
+        st.proving_period_start,
+        v.get_epoch(),
+    )
 }
 
 fn sector_deadline(v: &VM, m: Address, s: SectorNumber) -> (u64, u64) {
@@ -316,10 +379,48 @@ fn sector_deadline(v: &VM, m: Address, s: SectorNumber) -> (u64, u64) {
     st.find_sector(&Policy::default(), v.store, s).unwrap()
 }
 
-fn submit_windowed_post(v: &VM, worker: Address, maddr: Address, dline_info: DeadlineInfo, partitions: Vec<PoStPartition>, sector: PowerPair) {
-    let params = SubmitWindowedPoStParams{
+fn submit_windowed_post(
+    v: &VM,
+    worker: Address,
+    maddr: Address,
+    dline_info: DeadlineInfo,
+    partitions: Vec<PoStPartition>,
+    sector: PowerPair,
+) {
+    let params = SubmitWindowedPoStParams {
         deadline: dline_info.index,
         partitions,
-        proofs: vec![RegisteredP]
+        proofs: vec![PoStProof {
+            post_proof: RegisteredPoStProof::StackedDRGWindow32GiBV1,
+            proof_bytes: vec![],
+        }],
+        chain_commit_epoch: dline_info.challenge,
+        chain_commit_rand: Randomness(TEST_VM_RAND_STRING.to_owned().into_bytes()),
+    };
+    apply_ok(
+        &v,
+        worker,
+        maddr,
+        TokenAmount::zero(),
+        MinerMethod::SubmitWindowedPoSt as u64,
+        params,
+    );
+
+    let update_power_params = serialize(
+        &UpdateClaimedPowerParams { raw_byte_delta: sector.raw, quality_adjusted_delta: sector.qa },
+        "update claim params",
+    )
+    .unwrap();
+    ExpectInvocation {
+        to: maddr,
+        method: MinerMethod::SubmitWindowedPoSt as u64,
+        subinvocs: Some(vec![ExpectInvocation {
+            to: *STORAGE_POWER_ACTOR_ADDR,
+            method: PowerMethod::UpdateClaimedPower as u64,
+            params: Some(update_power_params),
+            ..Default::default()
+        }]),
+        ..Default::default()
     }
+    .matches(v.take_invocations().last().unwrap());
 }
