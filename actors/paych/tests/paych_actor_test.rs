1534c4ffe4c273fe26ec3007a969555c85f99ae4
Hubert Bugaj <hubert@chainsafe.io>
2022-06-06 12:26:11 +0200

    paych invariant checks

--- actors/paych/tests/paych_actor_test.rs
+++ actors/paych/tests/paych_actor_test.rs
@@ -7,10 +7,12 @@ use std::collections::HashMap;
 use anyhow::anyhow;
 use cid::Cid;
 use derive_builder::Builder;
+use fil_actor_paych::testing::check_state_invariants;
 use fil_actor_paych::{
     Actor as PaychActor, ConstructorParams, LaneState, Merge, Method, ModVerifyParams,
     SignedVoucher, State as PState, UpdateChannelStateParams, MAX_LANE, SETTLE_DELAY,
 };
+use fil_actors_runtime::runtime::Runtime;
 use fil_actors_runtime::test_utils::*;
 use fil_actors_runtime::INIT_ACTOR_ADDR;
 use fvm_ipld_amt::Amt;
@@ -59,6 +61,11 @@ fn get_lane_state(rt: &MockRuntime, cid: &Cid, lane: u64) -> LaneState {
     arr.get(lane).unwrap().unwrap().clone()
 }
 
+fn check_state(rt: &MockRuntime) {
+    let (_, acc) = check_state_invariants(&rt.get_state(), rt.store(), &rt.get_balance());
+    acc.assert_empty();
+}
+
 mod paych_constructor {
     use fvm_shared::METHOD_CONSTRUCTOR;
     use fvm_shared::METHOD_SEND;
@@ -92,6 +99,7 @@ mod paych_constructor {
         let mut rt = construct_runtime();
         rt.actor_code_cids.insert(caller_addr, *ACCOUNT_ACTOR_CODE_ID);
         construct_and_verify(&mut rt, Address::new_id(TEST_PAYER_ADDR), caller_addr);
+        check_state(&rt);
     }
 
     #[test]
@@ -125,6 +133,7 @@ mod paych_constructor {
         rt.id_addresses.insert(payee_non_id, payee_addr);
 
         construct_and_verify(&mut rt, payer_non_id, payee_non_id);
+        check_state(&rt);
     }
 
     #[test]
@@ -462,6 +471,7 @@ mod create_lane_tests {
                 let ls = l_states.get(sv.lane).unwrap().unwrap();
                 assert_eq!(sv.amount, ls.redeemed);
                 assert_eq!(sv.nonce, ls.nonce);
+                check_state(&rt);
             } else {
                 expect_abort(
                     &mut rt,
@@ -558,6 +568,7 @@ mod update_channel_state_redeem {
         assert_eq!(exp_send, state.to_send);
         assert_eq!(sv.amount, ls_updated.redeemed);
         assert_eq!(sv.nonce, ls_updated.nonce);
+        check_state(&rt);
     }
 
     #[test]
@@ -589,6 +600,7 @@ mod update_channel_state_redeem {
         );
 
         rt.verify();
+        check_state(&rt);
     }
 }
 
@@ -784,6 +796,7 @@ mod update_channel_state_extra {
             &RawBytes::serialize(UpdateChannelStateParams::from(sv)).unwrap(),
         );
         rt.verify();
+        check_state(&rt);
     }
 
     #[test]
@@ -796,6 +809,7 @@ mod update_channel_state_extra {
             ExitCode::USR_UNSPECIFIED,
         );
         rt.verify();
+        check_state(&rt);
     }
 }
 
@@ -848,6 +862,7 @@ fn update_channel_settling() {
         assert_eq!(tc.exp_settling_at, new_state.settling_at);
         assert_eq!(tc.exp_min_settle_height, new_state.min_settle_height);
         ucp.sv.nonce += 1;
+        check_state(&rt);
     }
 }
 
@@ -872,6 +887,7 @@ mod secret_preimage {
         call(&mut rt, Method::UpdateChannelState as u64, &RawBytes::serialize(ucp).unwrap());
 
         rt.verify();
+        check_state(&rt);
     }
 
     #[test]
@@ -900,6 +916,7 @@ mod secret_preimage {
         );
 
         rt.verify();
+        check_state(&rt);
     }
 }
 
@@ -921,6 +938,7 @@ mod actor_settle {
         state = rt.get_state();
         assert_eq!(state.settling_at, exp_settling_at);
         assert_eq!(state.min_settle_height, 0);
+        check_state(&rt);
     }
 
     #[test]
@@ -970,6 +988,7 @@ mod actor_settle {
 
         state = rt.get_state();
         assert_eq!(state.settling_at, ucp.sv.min_settle_height);
+        check_state(&rt);
     }
 
     #[test]
@@ -1041,6 +1060,7 @@ mod actor_collect {
         rt.expect_delete_actor(st.from);
         let res = call(&mut rt, Method::Collect as u64, &Default::default());
         assert_eq!(res, RawBytes::default());
+        check_state(&rt);
     }
 
     #[test]
@@ -1099,6 +1119,7 @@ mod actor_collect {
                 &RawBytes::default(),
                 tc.exp_collect_exit,
             );
+            check_state(&rt);
         }
     }
 }
@@ -1210,6 +1231,7 @@ fn verify_state(rt: &MockRuntime, exp_lanes: Option<u64>, expected_state: PState
     } else {
         assert_lane_states_length(rt, &state.lane_states, 0);
     }
+    check_state(rt);
 }
 
 fn assert_lane_states_length(rt: &MockRuntime, cid: &Cid, l: u64) {
