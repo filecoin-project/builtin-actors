d51763085f57263f330a2bb7a77409e42200675c
tyshko5 <74514992+tyshko5@users.noreply.github.com>
2022-06-15 00:01:01 +0200

    TestBatchOnboarding integration test (#476)
    Co-authored-by: Hubert Bugaj <hubert@chainsafe.io>
    Co-authored-by: zenground0 <ZenGround0@users.noreply.github.com>
    Co-authored-by: Rostyslav Tyshko <tyshko1@gmail.com>

--- test_vm/src/lib.rs
+++ test_vm/src/lib.rs
@@ -476,6 +476,20 @@ impl<'bs> VM<'bs> {
     pub fn expect_state_invariants(&self, expected_patterns: &[Regex]) {
         self.check_state_invariants().unwrap().assert_expected(expected_patterns)
     }
+
+    pub fn get_total_actor_balance(
+        &self,
+        store: &MemoryBlockstore,
+    ) -> anyhow::Result<BigInt, anyhow::Error> {
+        let state_tree = Tree::load(store, &self.checkpoint())?;
+
+        let mut total = BigInt::zero();
+        state_tree.for_each(|_, actor| {
+            total += &actor.balance.clone();
+            Ok(())
+        })?;
+        Ok(total)
+    }
 }
 
 #[derive(Clone)]
