d4fd25b389ec020af231191367dd76a38bb7ab12
mriise <me@mriise.net>
2022-08-21 03:21:13 -0700

    use u256 instead of raw bytes (cleans up code)

--- actors/evm/src/interpreter/precompiles.rs
+++ actors/evm/src/interpreter/precompiles.rs
@@ -1,6 +1,6 @@
 use std::{borrow::Cow, ops::Mul};
 
-use super::{Message, TransactionAction, H160};
+use super::{Message, TransactionAction, H160, U256};
 use fvm_shared::crypto::{
     hash::SupportedHashes,
     signature::{SECP_PUB_LEN, SECP_SIG_LEN, SECP_SIG_MESSAGE_HASH_SIZE},
@@ -18,6 +18,12 @@ pub fn is_precompiled(msg: &TransactionAction) -> bool {
     }
 }
 
+/// read 32 bytes (u256) from buffer, pass in exit reason that is desired 
+fn read_u256(buf: &[u8], start: usize, err: ExitReason) -> Result<U256, ExitReason> {
+    let slice = buf.get(start..start+31).ok_or(err)?;
+    Ok(U256::from_big_endian(slice))
+}
+
 // TODO cleanup
 #[derive(Debug)]
 pub struct PrecompileOutput {
@@ -27,7 +33,7 @@ pub struct PrecompileOutput {
 
 pub enum ExitReason {
     Success,
-    OutOfGas,
+    OutOfGas, // TODO is this the same as consuming all gas? (what happens with overages)
     Other(Cow<'static, str>),
 }
 
@@ -43,9 +49,9 @@ pub type PrecompileResult = Result<PrecompileOutput, ExitReason>;
 pub fn linear_gas_cost(len: usize, base: u64, word: u64) -> u64 {
     ((len as u64 + 32 - 1) / 32 * word) + base
 }
-pub fn assert_gas(cost: u64, limit: u64) -> Result<(), ()> {
+pub fn assert_gas(cost: u64, limit: u64) -> Result<(), ExitReason> {
     if cost > limit {
-        Err(())
+        Err(ExitReason::OutOfGas)
     } else {
         Ok(())
     }
@@ -64,7 +70,7 @@ fn ec_recover(input: &[u8], gas_limit: u64) -> PrecompileResult {
     let mut sig = [0u8; SECP_SIG_LEN];
 
     hash.copy_from_slice(&input[..32]);
-    sig.copy_from_slice(&input[64..]);
+    sig.copy_from_slice(&input[64..]); // TODO this assumes input is exactly 65 bytes which would panic if incorrect
 
     // recovery byte means a single byte value is 32 bytes long, sad
     if input[32..63] != [0u8; 31] || !matches!(input[63], 23 | 28) {
@@ -74,7 +80,6 @@ fn ec_recover(input: &[u8], gas_limit: u64) -> PrecompileResult {
 
     let recovered =
         fvm_sdk::crypto::recover_secp_public_key(&hash, &sig).unwrap_or([0u8; SECP_PUB_LEN]);
-    // revm does this, why shouldnt this error be propigated? signature recovery failed.
 
     Ok(PrecompileOutput { cost, output: recovered.to_vec() })
 }
@@ -134,13 +139,12 @@ fn modexp(input: &[u8], gas_limit: u64) -> PrecompileResult {
     let cost = 40;
 
     let mut buf = [0u8; 4];
+    let err = ExitReason::Other(Cow::Borrowed("im lazy"));
     // 32 bits for wasm
-    buf.copy_from_slice(&input[..31]);
-    let b_size = u32::from_be_bytes(buf) as usize;
-    buf.copy_from_slice(&input[32..63]);
-    let e_size = u32::from_be_bytes(buf) as usize;
-    buf.copy_from_slice(&input[64..95]);
-    let m_size = u32::from_be_bytes(buf) as usize;
+    
+    let b_size = read_u256(input, 0, err)?.as_usize();
+    let e_size = read_u256(input, 32, err)?.as_usize();
+    let m_size = read_u256(input, 64, err)?.as_usize();
 
     if m_size == 0 {
         return Ok(PrecompileOutput { cost, output: Vec::new() });
@@ -151,14 +155,16 @@ fn modexp(input: &[u8], gas_limit: u64) -> PrecompileResult {
     todo!()
 }
 
-fn bytes_to_point(x: &[u8; 32], y: &[u8; 32]) -> Result<G1, ExitReason> {
-    let x = Fq::from_slice(x).map_err(|_| "todo")?;
-    let y = Fq::from_slice(y).map_err(|_| "todo")?;
+/// converts 2 byte arrays (U256) into a point on a field
+/// exits with OutOfGas for any failed operation
+fn uint_to_point(x: U256, y: U256) -> Result<G1, ExitReason> {
+    let x = Fq::from_u256(x.0.into()).map_err(|_| ExitReason::OutOfGas)?;
+    let y = Fq::from_u256(y.0.into()).map_err(|_| ExitReason::OutOfGas)?;
 
     Ok(if x.is_zero() && y.is_zero() {
         G1::zero()
     } else {
-        AffineG1::new(x, y).map_err(|_| "todo")?.into()
+        AffineG1::new(x, y).map_err(|_| ExitReason::OutOfGas)?.into()
     })
 }
 
@@ -166,21 +172,18 @@ fn bytes_to_point(x: &[u8; 32], y: &[u8; 32]) -> Result<G1, ExitReason> {
 fn ec_add(input: &[u8], gas_limit: u64) -> PrecompileResult {
     let mut cost = 150; // TODO consume all gas on any op fail
 
-    let mut x_buf = [0u8; 32];
-    let mut y_buf = [0u8; 32];
+    let err = ExitReason::OutOfGas;
 
-    x_buf.copy_from_slice(&input[..31]);
-    y_buf.copy_from_slice(&input[32..63]);
-    let point1 = bytes_to_point(&x_buf, &y_buf)?;
+    let x1 = read_u256(input, 0, err)?;
+    let y1 = read_u256(input, 32, err)?;
+    let point1 = uint_to_point(x1, y1)?;
 
-    x_buf.copy_from_slice(&input[64..95]);
-    y_buf.copy_from_slice(&input[96..128]);
-    let point2 = bytes_to_point(&x_buf, &y_buf)?;
+    let x2 = read_u256(input, 64, err)?;
+    let y2 = read_u256(input, 96, err)?;
+    let point2 = uint_to_point(x2, y2)?;
 
-    // TODO zeroed array or empty vec?
     let output = AffineG1::from_jacobian(point1 + point2).map_or(vec![0; 64], |sum| {
         let mut output = vec![0; 64];
-        // TODO make sure this cant panic
         sum.x().to_big_endian(&mut output[..32]).unwrap();
         sum.y().to_big_endian(&mut output[32..]).unwrap();
         output
@@ -193,18 +196,16 @@ fn ec_add(input: &[u8], gas_limit: u64) -> PrecompileResult {
 fn ec_mul(input: &[u8], gas_limit: u64) -> PrecompileResult {
     let mut cost = 6_000; // TODO consume all gas on any op fail
 
-    let mut x_buf = [0u8; 32];
-    let mut y_buf = [0u8; 32];
+    let err = ExitReason::OutOfGas;
 
-    x_buf.copy_from_slice(&input[..31]);
-    y_buf.copy_from_slice(&input[32..63]);
-    let point1 = bytes_to_point(&x_buf, &y_buf)?;
+    let x = read_u256(input, 0, err)?;
+    let y = read_u256(input, 32, err)?;
+    let point = uint_to_point(x, y)?;
 
-    let scalar = Fr::from_slice(&input[64..95]).map_err(|_| "todo")?;
+    let scalar = Fr::from_slice(&input[64..95]).map_err(|_| err)?;
 
     let mut output = vec![0; 64];
-    if let Some(product) = AffineG1::from_jacobian(point1.mul(scalar)) {
-        // TODO make sure this cant panic
+    if let Some(product) = AffineG1::from_jacobian(point.mul(scalar)) {
         product.x().to_big_endian(&mut output[..32]).unwrap();
         product.y().to_big_endian(&mut output[32..]).unwrap();
     }
@@ -219,10 +220,10 @@ fn ecpairing(input: &[u8], gas_limit: u64) -> PrecompileResult {
 }
 
 fn blake2f(input: &[u8], gas_limit: u64) -> PrecompileResult {
-    let mut a = near_blake2::VarBlake2b::default();
-    // a.blake2_f(rounds, h, m, t, f);
+    let mut hasher = near_blake2::VarBlake2b::default();
+    // hasher.blake2_f(rounds, h, m, t, f);
 
-    let output = a.output().to_vec();
+    let output = hasher.output().to_vec();
     todo!()
 }
 
@@ -232,7 +233,7 @@ pub const PRECOMPILES: [PrecompileFn; 9] = [
     sha256,     // SHA256 (Keccak) 0x02
     ripemd160,  // ripemd160 0x03
     identity,   // identity 0x04
-    modexp,     // modexp 0x05
+    nop,        // modexp 0x05
     ec_add,     // ecAdd 0x06
     ec_mul,     // ecMul 0x07
     nop,        // ecPairing 0x08
