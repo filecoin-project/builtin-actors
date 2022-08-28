e9f449880bfb599c012bda51d250991ba10b2b3f
Alex <445306+anorth@users.noreply.github.com>
2022-08-29 09:15:52 +1000

    Upgrade fungible token library. Use standard exit codes. Use standard token method params/return types. (#580)

--- actors/datacap/src/types.rs
+++ actors/datacap/src/types.rs
@@ -1,15 +1,7 @@
 use fvm_ipld_encoding::tuple::*;
-use fvm_ipld_encoding::{Cbor, RawBytes};
+use fvm_ipld_encoding::Cbor;
 use fvm_shared::address::Address;
 use fvm_shared::bigint::{bigint_ser, BigInt};
-
-#[derive(Clone, Debug, PartialEq, Eq, Serialize_tuple, Deserialize_tuple)]
-pub struct AllowanceParams {
-    pub owner: Address,
-    pub operator: Address,
-}
-
-impl Cbor for AllowanceParams {}
 
 #[derive(Clone, Debug, PartialEq, Eq, Serialize_tuple, Deserialize_tuple)]
 pub struct MintParams {
@@ -28,66 +20,3 @@ pub struct DestroyParams {
 }
 
 impl Cbor for DestroyParams {}
-
-#[derive(Clone, Debug, PartialEq, Eq, Serialize_tuple, Deserialize_tuple)]
-pub struct TransferParams {
-    pub to: Address,
-    #[serde(with = "bigint_ser")]
-    pub amount: BigInt,
-    pub data: RawBytes,
-}
-
-impl Cbor for TransferParams {}
-
-#[derive(Clone, Debug, PartialEq, Eq, Serialize_tuple, Deserialize_tuple)]
-pub struct TransferFromParams {
-    pub from: Address,
-    pub to: Address,
-    #[serde(with = "bigint_ser")]
-    pub amount: BigInt,
-    pub data: RawBytes,
-}
-
-impl Cbor for TransferFromParams {}
-
-#[derive(Clone, Debug, PartialEq, Eq, Serialize_tuple, Deserialize_tuple)]
-pub struct IncreaseAllowanceParams {
-    pub operator: Address,
-    #[serde(with = "bigint_ser")]
-    pub amount: BigInt,
-}
-
-impl Cbor for IncreaseAllowanceParams {}
-
-#[derive(Clone, Debug, PartialEq, Eq, Serialize_tuple, Deserialize_tuple)]
-pub struct DecreaseAllowanceParams {
-    pub operator: Address,
-    #[serde(with = "bigint_ser")]
-    pub amount: BigInt,
-}
-
-impl Cbor for DecreaseAllowanceParams {}
-
-#[derive(Clone, Debug, PartialEq, Eq, Serialize_tuple, Deserialize_tuple)]
-pub struct RevokeAllowanceParams {
-    pub operator: Address,
-}
-
-impl Cbor for RevokeAllowanceParams {}
-
-#[derive(Clone, Debug, PartialEq, Eq, Serialize_tuple, Deserialize_tuple)]
-pub struct BurnParams {
-    #[serde(with = "bigint_ser")]
-    pub amount: BigInt,
-}
-
-impl Cbor for BurnParams {}
-
-#[derive(Clone, Debug, PartialEq, Eq, Serialize_tuple, Deserialize_tuple)]
-pub struct BurnFromParams {
-    pub from: Address,
-    #[serde(with = "bigint_ser")]
-    pub amount: BigInt,
-}
-
-impl Cbor for BurnFromParams {}
