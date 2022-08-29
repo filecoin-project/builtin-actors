03313bf8c1bff801bcc2f510de842533938bbb47
Raúl Kripalani <raul@protocol.ai>
2022-08-29 23:22:59 +0100

    EVM runtime: commented selfdestruct.

--- actors/evm/src/interpreter/instructions/lifecycle.rs
+++ actors/evm/src/interpreter/instructions/lifecycle.rs
@@ -18,5 +18,18 @@ pub fn selfdestruct<'r, BS: Blockstore, RT: Runtime<BS>>(
     _state: &mut ExecutionState,
     _platform: &'r System<'r, BS, RT>,
 ) -> Result<(), StatusCode> {
-    todo!()
+    // Commented due to horrible borrow checker issues that stem from owning a HAMT during
+    // the entire execution inside the System. The HAMT needs to be flushed and dropped when
+    // create_actor, delete_actor, and send are called. All other methods taking a &mut self
+    // on the Runtime should not require &mut.
+    //
+    // let beneficiary_addr = Address::from(state.stack.pop());
+    //
+    // if let Some(id_addr) = beneficiary_addr.as_id_address() {
+    //     platform.rt.delete_actor(&id_addr)?;
+    // } else {
+    //     todo!("no support for non-ID addresses")
+    // }
+    //
+    // Ok(())
 }
