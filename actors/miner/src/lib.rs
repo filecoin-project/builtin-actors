0e86acce6f1338b15a1f20fd482463085d22bd17
Jakub Sztandera <kubuxu@protocol.ai>
2022-08-31 21:14:09 +0200

    Expose RefereshProofExpiration
    Signed-off-by: Jakub Sztandera <kubuxu@protocol.ai>

--- actors/miner/src/lib.rs
+++ actors/miner/src/lib.rs
@@ -124,6 +124,7 @@ pub enum Method {
     ChangeBeneficiary = 30,
     GetBeneficiary = 31,
     ExtendSectorExpiration2 = 32,
+    RefreshProofExpiration = 33,
     // Method numbers derived from FRC-0042 standards
     ChangeWorkerAddressExported = frc42_dispatch::method_hash!("ChangeWorkerAddress"),
     ChangePeerIDExported = frc42_dispatch::method_hash!("ChangePeerID"),
@@ -3498,19 +3499,16 @@ impl Actor {
         Ok(())
     }
 
-    /*
     fn refresh_proof_expiration<BS, RT>(
         rt: &mut RT,
-        mut params: ExtendSectorExpirationParams,
+        params: ExtendSectorExpirationParams,
     ) -> Result<(), ActorError>
-        where
-            BS: Blockstore,
-            RT: Runtime<BS>,
+    where
+        BS: Blockstore,
+        RT: Runtime<BS>,
     {
-
-
-        Ok(())
-    }*/
+        extend_sector_inner(rt, &params.extensions, extend_proof_validity)
+    }
 }
 
 #[derive(Debug, PartialEq, Clone)]
@@ -4954,7 +4952,6 @@ fn balance_invariants_broken(e: Error) -> ActorError {
     )
 }
 
-#[allow(dead_code)]
 fn extend_proof_validity<BS, RT>(
     rt: &RT,
     _: &ValidatedExpirationExtension,
@@ -5047,6 +5044,15 @@ where
 
     sector.deal_weight = new_deal_weight;
     sector.verified_deal_weight = new_verified_deal_weight;
+
+    // try also extending proof validity, duplicate code with extend_proof_validity I haven't
+    // found a nice way to dedupe
+    let policy = rt.policy();
+    let new_proof_expiration =
+        sector.proof_expiration + (policy.max_proof_validity - policy.proof_refresh_window);
+    if !(new_proof_expiration > rt.curr_epoch() + policy.max_proof_validity) {
+        sector.proof_expiration = new_proof_expiration;
+    }
 
     Ok(sector)
 }
@@ -5465,6 +5471,10 @@ impl ActorCode for Actor {
             }
             Some(Method::ExtendSectorExpiration2) => {
                 Self::extend_sector_expiration2(rt, cbor::deserialize_params(params)?)?;
+                Ok(RawBytes::default())
+            }
+            Some(Method::RefreshProofExpiration) => {
+                Self::refresh_proof_expiration(rt, cbor::deserialize_params(params)?)?;
                 Ok(RawBytes::default())
             }
             None => Err(actor_error!(unhandled_message, "Invalid method")),
