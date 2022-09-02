6380d46f3d416003f36f0809f21b24a6c516eaf6
Raúl Kripalani <raul@protocol.ai>
2022-09-02 20:40:52 +0100

    add a test for SELFDESTRUCT.

---
+++ actors/evm/tests/selfdestruct.rs
@@ -0,0 +1,42 @@
+use fil_actor_evm as evm;
+use fil_actors_runtime::test_utils::*;
+use fvm_ipld_encoding::RawBytes;
+use fvm_shared::address::Address;
+
+#[test]
+fn test_selfdestruct() {
+    let mut rt = MockRuntime::default();
+
+    let contract = Address::new_id(100);
+    let beneficiary = Address::new_id(1001);
+
+    let params = evm::ConstructorParams {
+        bytecode: hex::decode(include_str!("selfdestruct.hex")).unwrap().into(),
+        input_data: RawBytes::default(),
+    };
+
+    // invoke constructor
+    rt.actor_code_cids.insert(contract, *EVM_ACTOR_CODE_ID);
+    rt.expect_validate_caller_any();
+    rt.set_origin(contract);
+
+    let result = rt
+        .call::<evm::EvmContractActor>(
+            evm::Method::Constructor as u64,
+            &RawBytes::serialize(params).unwrap(),
+        )
+        .unwrap();
+    expect_empty(result);
+    rt.verify();
+
+    let solidity_params = hex::decode("35f46994").unwrap();
+    let params = evm::InvokeParams { input_data: RawBytes::from(solidity_params) };
+    rt.expect_validate_caller_any();
+    rt.expect_delete_actor(beneficiary);
+
+    rt.call::<evm::EvmContractActor>(
+        evm::Method::InvokeContract as u64,
+        &RawBytes::serialize(params).unwrap(),
+    )
+    .unwrap();
+}
