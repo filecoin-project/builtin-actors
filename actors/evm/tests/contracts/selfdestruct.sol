0d5ee94d6b44fa7b6c4f66ab39198760e8744c84
Akosh Farkash <aakoshh@gmail.com>
2022-09-20 15:16:00 +0100

    EVM-887: Makefile to generate Solidity compilation artifacts.

---
+++ actors/evm/tests/contracts/selfdestruct.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Selfdestruct {
+    function die() public {
+        selfdestruct(
+            payable(address(0xFF000000000000000000000000000000000003E9))
+        );
+    }
+}
