6e85b1a85a43b4d1862989b3eb009753ed6c97f1
Steven Allen <steven@stebalien.com>
2022-09-19 22:54:18 -0700

    fix: remove paste
    It looks like this messes with code coverage.

--- runtime/src/builtin/singletons.rs
+++ runtime/src/builtin/singletons.rs
@@ -3,30 +3,32 @@
 use fvm_shared::address::Address;
 use fvm_shared::ActorID;
 
-use paste::paste;
+pub const SYSTEM_ACTOR_ID: ActorID = 0;
+pub const SYSTEM_ACTOR_ADDR: Address = Address::new_id(SYSTEM_ACTOR_ID);
 
-macro_rules! define_singletons {
-    ($($name:ident = $id:literal,)*) => {
-        $(
-            paste! {
-                pub const [<$name _ID>]: ActorID = $id;
-                pub const [<$name _ADDR>]: Address = Address::new_id([<$name _ID>]);
-            }
-        )*
-    }
-}
+pub const INIT_ACTOR_ID: ActorID = 1;
+pub const INIT_ACTOR_ADDR: Address = Address::new_id(INIT_ACTOR_ID);
 
-define_singletons! {
-    SYSTEM_ACTOR = 0,
-    INIT_ACTOR = 1,
-    REWARD_ACTOR = 2,
-    CRON_ACTOR = 3,
-    STORAGE_POWER_ACTOR = 4,
-    STORAGE_MARKET_ACTOR = 5,
-    VERIFIED_REGISTRY_ACTOR = 6,
-    CHAOS_ACTOR = 98,
-    BURNT_FUNDS_ACTOR = 99,
-}
+pub const REWARD_ACTOR_ID: ActorID = 2;
+pub const REWARD_ACTOR_ADDR: Address = Address::new_id(REWARD_ACTOR_ID);
+
+pub const CRON_ACTOR_ID: ActorID = 3;
+pub const CRON_ACTOR_ADDR: Address = Address::new_id(CRON_ACTOR_ID);
+
+pub const STORAGE_POWER_ACTOR_ID: ActorID = 4;
+pub const STORAGE_POWER_ACTOR_ADDR: Address = Address::new_id(STORAGE_POWER_ACTOR_ID);
+
+pub const STORAGE_MARKET_ACTOR_ID: ActorID = 5;
+pub const STORAGE_MARKET_ACTOR_ADDR: Address = Address::new_id(STORAGE_MARKET_ACTOR_ID);
+
+pub const VERIFIED_REGISTRY_ACTOR_ID: ActorID = 6;
+pub const VERIFIED_REGISTRY_ACTOR_ADDR: Address = Address::new_id(VERIFIED_REGISTRY_ACTOR_ID);
+
+pub const CHAOS_ACTOR_ID: ActorID = 98;
+pub const CHAOS_ACTOR_ADDR: Address = Address::new_id(CHAOS_ACTOR_ID);
+
+pub const BURNT_FUNDS_ACTOR_ID: ActorID = 99;
+pub const BURNT_FUNDS_ACTOR_ADDR: Address = Address::new_id(BURNT_FUNDS_ACTOR_ID);
 
 /// Defines first available ID address after builtin actors
 pub const FIRST_NON_SINGLETON_ADDR: ActorID = 100;
