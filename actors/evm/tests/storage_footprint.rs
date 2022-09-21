f4b12c97f8431cc051bfc0870b5cd0c2ab5c519e
Akosh Farkash <aakoshh@gmail.com>
2022-09-21 13:30:34 +0100

    EVM-888: TestEnv with deploy.

--- actors/evm/tests/storage_footprint.rs
+++ actors/evm/tests/storage_footprint.rs
@@ -2,8 +2,12 @@ use std::sync::Arc;
 
 use ethers::prelude::abigen;
 use ethers::providers::{MockProvider, Provider};
+use fil_actor_evm as evm;
+use fil_actors_runtime::test_utils::{expect_empty, MockRuntime, EVM_ACTOR_CODE_ID};
+use fvm_ipld_encoding::RawBytes;
 use fvm_shared::address::Address;
 
+// The owner doesn't matter in these tests, so just using the same value that the other tests use, for everything.
 const OWNER: Address = Address::new_id(100);
 
 // Generate a statically types interface for the contract.
@@ -32,7 +36,49 @@ impl Default for StorageFootprint<Provider<MockProvider>> {
     }
 }
 
+struct TestEnv {
+    runtime: MockRuntime,
+}
+
+impl TestEnv {
+    /// Create a new test environment where the EVM actor code is already
+    /// loaded under the global owner.
+    pub fn new() -> Self {
+        let mut runtime = MockRuntime::default();
+        runtime.actor_code_cids.insert(OWNER, *EVM_ACTOR_CODE_ID);
+        Self { runtime }
+    }
+
+    /// Deploy a contract into the EVM actor.
+    pub fn deploy(&mut self, contract_hex: &str) {
+        let params = evm::ConstructorParams { bytecode: hex::decode(contract_hex).unwrap().into() };
+        // invoke constructor
+        self.runtime.expect_validate_caller_any();
+        self.runtime.set_origin(OWNER);
+
+        let result = self
+            .runtime
+            .call::<evm::EvmContractActor>(
+                evm::Method::Constructor as u64,
+                &RawBytes::serialize(params).unwrap(),
+            )
+            .unwrap();
+
+        expect_empty(result);
+
+        self.runtime.verify();
+    }
+}
+
+impl Default for TestEnv {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
 #[test]
-fn abi_test() {
+fn basic() {
     let _ = StorageFootprint::default();
+    let mut env = TestEnv::default();
+    env.deploy(include_str!("contracts/StorageFootprint.hex"))
 }
