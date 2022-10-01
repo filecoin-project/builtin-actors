6d800c449c0a59bbc4728a68924b4c64fbff130d
Akosh Farkash <akosh.farkash@protocol.ai>
2022-10-01 13:37:13 +0100

    EVM-889: Visualize measurements (#708)
    * EVM-889: Visualize measurements
    
    * EVM-889: Check in rendered charts.
    
    * EVM-889: Convenience target to run footprint measurements.
    
    * EVM-889: Separate script for plotting

---
+++ actors/evm/tests/measurements/storage-footprint.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Name of the scenario to plot.
+NAME=$1
+
+if [ -z "$NAME" ]; then
+  echo "usage: storage-footprint.sh <scenario-name>"
+  exit 1
+fi
+
+# Separate the two series (the current max) with newlines for gnuplot
+# Ignoring warnings from gnuplot because sometimes we only have 1 series.
+
+rm -f $NAME.dat
+
+for S in 1 2; do
+  cat $NAME.jsonline \
+    | jq -r "select(.series == $S) | [.series, .i, .stats.get_count, .stats.get_bytes, .stats.put_count, .stats.put_bytes] | @tsv" \
+    >> $NAME.dat
+  echo "\n" >> $NAME.dat
+done
+
+gnuplot \
+  -e "scenario='$(echo $NAME | tr _ - )'" \
+  -e "filein='$NAME.dat'" \
+  -e "fileout='$NAME.png'" \
+  storage-footprint.plt \
+  2>/dev/null
+
+rm $NAME.dat
