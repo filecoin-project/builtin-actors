071350aed59e9d3330db4de669c11d80a078a019
shamb0 <r.raajey@gmail.com>
2022-10-07 21:08:55 +0530

    make clippy happy
    Signed-off-by: shamb0 <r.raajey@gmail.com>

--- actors/market/src/state.rs
+++ actors/market/src/state.rs
@@ -231,7 +231,7 @@ impl State {
             .get(deal_id)
             .map_err(|e| e.downcast_wrap(&format!("no such deal proposal {}", deal_id)))?;
 
-        Ok(proposal.cloned())
+        Ok(proposal)
     }
 
     pub(super) fn deal_proposals_delete<BS>(
