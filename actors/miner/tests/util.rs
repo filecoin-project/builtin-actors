c434b6700f464ea55c5a195e1d5ff68508badeba
Geoff Stuart <geoff.vball@gmail.com>
2022-10-18 14:04:40 -0400

    fix: Add runtime caller validation for change_beneficiary (#761) (#763)
    * Add runtime caller validation for change_beneficiary
    * Miner tests: expect_validate_caller_any for calls to change_beneficiary
    * Test VM fix: Fail if caller isn't validated
    
    Co-authored-by: Aayush <arajasek94@gmail.com>
    Co-authored-by: zenground0 <ZenGround0@users.noreply.github.com>

--- actors/miner/tests/util.rs
+++ actors/miner/tests/util.rs
@@ -2266,10 +2266,12 @@ impl ActorHarness {
             new_expiration: beneficiary_term.expiration,
         };
         let raw_bytes = &RawBytes::serialize(param).unwrap();
+        rt.expect_validate_caller_any();
         rt.call::<Actor>(Method::ChangeBeneficiary as u64, raw_bytes)?;
         rt.verify();
 
         rt.set_caller(*ACCOUNT_ACTOR_CODE_ID, beneficiary_id_addr);
+        rt.expect_validate_caller_any();
         rt.call::<Actor>(Method::ChangeBeneficiary as u64, raw_bytes)?;
         rt.verify();
 
@@ -2284,6 +2286,7 @@ impl ActorHarness {
         beneficiary_change: &BeneficiaryChange,
         expect_beneficiary_addr: Option<Address>,
     ) -> Result<RawBytes, ActorError> {
+        rt.expect_validate_caller_any();
         rt.set_address_actor_type(
             beneficiary_change.beneficiary_addr.clone(),
             *ACCOUNT_ACTOR_CODE_ID,
