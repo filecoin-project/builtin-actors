ff5f4f41272bc9a287b08b1400ce98d38418dbfd
vyzo <vyzo@hackzen.org>
2022-10-20 23:35:07 +0300

    delegate call tests

--- test_vm/tests/evm_test.rs
+++ test_vm/tests/evm_test.rs
@@ -248,6 +248,7 @@ fn test_evm_staticcall() {
 }
 
 #[test]
+#[allow(non_snake_case)]
 fn test_evm_delegatecall() {
     // test scenarios:
     // one hop:
@@ -256,4 +257,209 @@ fn test_evm_delegatecall() {
     // two hop with sticky staticcall:
     // A -> staticcall -> B -> delegatecall -> C (read) OK
     // A -> staticcall -> B -> delegatecall -> C (write) FAIL
+
+        let store = MemoryBlockstore::new();
+    let v = VM::new_with_singletons(&store);
+
+    let accounts = create_accounts(&v, 3, TokenAmount::from_whole(10_000));
+
+    let bytecode =
+        hex::decode(include_str!("../../actors/evm/tests/contracts/callvariants.hex")).unwrap();
+
+    let created: Vec<_> = accounts
+        .iter()
+        .map(|account| {
+            let create_result = v
+                .apply_message(
+                    *account,
+                    EAM_ACTOR_ADDR,
+                    TokenAmount::zero(),
+                    fil_actor_eam::Method::Create2 as u64,
+                    fil_actor_eam::Create2Params { initcode: bytecode.clone(), salt: [0u8; 32] },
+                )
+                .unwrap();
+
+            assert!(
+                create_result.code.is_success(),
+                "failed to create the new actor {}",
+                create_result.error_message.unwrap()
+            );
+
+            let create_return: fil_actor_eam::Create2Return =
+                create_result.ret.deserialize().expect("failed to decode results");
+
+            // Make sure we deployed an EVM actor.
+            assert_eq!(
+                &v.get_actor(Address::new_id(create_return.actor_id)).unwrap().code,
+                &*EVM_ACTOR_CODE_ID
+            );
+
+            create_return
+        })
+        .collect();
+
+    // A -> delegatecall -> B (read) OK
+    {
+        let A_act = accounts[0].clone();
+        let A_robust_addr = created[0].robust_address.clone();
+        let B = id_to_eth(created[1].actor_id);
+        let mut params = [0u8; 36];
+        params[3] = 9;
+        params[16..].copy_from_slice(B.as_ref());
+
+        let call_result = v
+            .apply_message(
+                A_act,
+                A_robust_addr,
+                TokenAmount::zero(),
+                fil_actor_evm::Method::InvokeContract as u64,
+                ContractParams(params.to_vec()),
+            )
+            .unwrap();
+        assert!(
+            call_result.code.is_success(),
+            "failed to call the new actor {}",
+            call_result.error_message.unwrap()
+        );
+        let BytesDe(return_value) =
+            call_result.ret.deserialize().expect("failed to deserialize results");
+        assert_eq!(&return_value[12..], &created[0].eth_address.0);
+    }
+
+    // A -> delegatecall -> B (write) -> return (read) OK
+    {
+        let A_act = accounts[0].clone();
+        let A_robust_addr = created[0].robust_address.clone();
+        let B = id_to_eth(created[1].actor_id);
+        let mut params = [0u8; 36];
+        params[3] = 10;
+        params[16..].copy_from_slice(B.as_ref());
+
+        let call_result = v
+            .apply_message(
+                A_act,
+                A_robust_addr,
+                TokenAmount::zero(),
+                fil_actor_evm::Method::InvokeContract as u64,
+                ContractParams(params.to_vec()),
+            )
+            .unwrap();
+        assert!(
+            call_result.code.is_success(),
+            "failed to call the new actor {}",
+            call_result.error_message.unwrap()
+        );
+        let BytesDe(return_value) =
+            call_result.ret.deserialize().expect("failed to deserialize results");
+        assert_eq!(&return_value[28..], &[0xff, 0xff, 0xff, 0x42]);
+    }
+}
+
+#[test]
+#[allow(non_snake_case)]
+fn test_evm_staticcall_delegatecall() {
+    // test scenarios:
+    // one hop:
+    // A -> delegatecall -> B (read) OK
+    // A -> delegatecall -> B (write) -> return (read) OK
+    // two hop with sticky staticcall:
+    // A -> staticcall -> B -> delegatecall -> C (read) OK
+    // A -> staticcall -> B -> delegatecall -> C (write) FAIL
+
+        let store = MemoryBlockstore::new();
+    let v = VM::new_with_singletons(&store);
+
+    let accounts = create_accounts(&v, 3, TokenAmount::from_whole(10_000));
+
+    let bytecode =
+        hex::decode(include_str!("../../actors/evm/tests/contracts/callvariants.hex")).unwrap();
+
+    let created: Vec<_> = accounts
+        .iter()
+        .map(|account| {
+            let create_result = v
+                .apply_message(
+                    *account,
+                    EAM_ACTOR_ADDR,
+                    TokenAmount::zero(),
+                    fil_actor_eam::Method::Create2 as u64,
+                    fil_actor_eam::Create2Params { initcode: bytecode.clone(), salt: [0u8; 32] },
+                )
+                .unwrap();
+
+            assert!(
+                create_result.code.is_success(),
+                "failed to create the new actor {}",
+                create_result.error_message.unwrap()
+            );
+
+            let create_return: fil_actor_eam::Create2Return =
+                create_result.ret.deserialize().expect("failed to decode results");
+
+            // Make sure we deployed an EVM actor.
+            assert_eq!(
+                &v.get_actor(Address::new_id(create_return.actor_id)).unwrap().code,
+                &*EVM_ACTOR_CODE_ID
+            );
+
+            create_return
+        })
+        .collect();
+
+    // A -> staticcall -> B -> delegatecall -> C (read) OK
+    {
+        let A_act = accounts[0].clone();
+        let A_robust_addr = created[0].robust_address.clone();
+        let B = id_to_eth(created[1].actor_id);
+        let C = id_to_eth(created[2].actor_id);
+        let mut params = [0u8; 68];
+        params[3] = 11;
+        params[16..][..20].copy_from_slice(B.as_ref());
+        params[48..].copy_from_slice(C.as_ref());
+
+        let call_result = v
+            .apply_message(
+                A_act,
+                A_robust_addr,
+                TokenAmount::zero(),
+                fil_actor_evm::Method::InvokeContract as u64,
+                ContractParams(params.to_vec()),
+            )
+            .unwrap();
+        assert!(
+            call_result.code.is_success(),
+            "failed to call the new actor {}",
+            call_result.error_message.unwrap()
+        );
+        let BytesDe(return_value) =
+            call_result.ret.deserialize().expect("failed to deserialize results");
+        //assert_eq!(&return_value[12..], &created[1].eth_address.0);
+        println!("return {:?}", return_value)
+    }
+
+    // A -> staticcall -> B -> delegatecall -> C (write) FAIL
+    {
+        let A_act = accounts[0].clone();
+        let A_robust_addr = created[0].robust_address.clone();
+        let B = id_to_eth(created[1].actor_id);
+        let C = id_to_eth(created[2].actor_id);
+        let mut params = [0u8; 68];
+        params[3] = 13;
+        params[16..][..20].copy_from_slice(B.as_ref());
+        params[48..].copy_from_slice(C.as_ref());
+
+        let call_result = v
+            .apply_message(
+                A_act,
+                A_robust_addr,
+                TokenAmount::zero(),
+                fil_actor_evm::Method::InvokeContract as u64,
+                ContractParams(params.to_vec()),
+            )
+            .unwrap();
+        assert!(
+            !call_result.code.is_success(),
+            "static call mutation succeeded",
+        );
+    }
 }
