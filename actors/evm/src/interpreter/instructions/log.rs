a7f261d9f6e1e9da6e7a89f07a96d4abf00d1e73
Steven Allen <steven@stebalien.com>
2022-10-25 23:28:51 +0100

    fix: implement logging as a no-op in production (#783)
    For a better testnet experience.

--- actors/evm/src/interpreter/instructions/log.rs
+++ actors/evm/src/interpreter/instructions/log.rs
@@ -4,11 +4,26 @@ use {
     fvm_ipld_blockstore::Blockstore,
 };
 
-#[inline]
+#[cfg(debug_assertions)]
 pub fn log<'r, BS: Blockstore, RT: Runtime<BS>>(
     _state: &mut ExecutionState,
-    _platform: &'r System<'r, BS, RT>,
+    _system: &'r System<'r, BS, RT>,
     _num_topics: usize,
 ) -> Result<(), StatusCode> {
-    todo!()
+    todo!("unimplemented");
+}
+
+#[cfg(not(debug_assertions))]
+#[inline]
+pub fn log<'r, BS: Blockstore, RT: Runtime<BS>>(
+    state: &mut ExecutionState,
+    _system: &'r System<'r, BS, RT>,
+    num_topics: usize,
+) -> Result<(), StatusCode> {
+    // TODO: Right now, we just drop everything. But we implement this in production anyways so
+    // things work.
+    for _ in 0..num_topics {
+        state.stack.pop();
+    }
+    Ok(())
 }
