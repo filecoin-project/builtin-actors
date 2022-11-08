c5f05f2b064b3832032404268ca0e05127452f69
Alex <445306+anorth@users.noreply.github.com>
2022-11-09 08:59:56 +1100

    Exported API method for market actor escrow/locked balance (#812)

--- actors/market/src/types.rs
+++ actors/market/src/types.rs
@@ -22,7 +22,7 @@ use super::deal::{ClientDealProposal, DealProposal, DealState};
 pub const PROPOSALS_AMT_BITWIDTH: u32 = 5;
 pub const STATES_AMT_BITWIDTH: u32 = 6;
 
-#[derive(Serialize_tuple, Deserialize_tuple)]
+#[derive(Serialize_tuple, Deserialize_tuple, Debug, Clone, Eq, PartialEq)]
 pub struct WithdrawBalanceParams {
     pub provider_or_client: Address,
     pub amount: TokenAmount,
@@ -30,33 +30,32 @@ pub struct WithdrawBalanceParams {
 
 impl Cbor for WithdrawBalanceParams {}
 
-#[derive(Serialize_tuple, Deserialize_tuple)]
+#[derive(Serialize_tuple, Deserialize_tuple, Debug, Clone, Eq, PartialEq)]
 #[serde(transparent)]
 pub struct WithdrawBalanceReturn {
     pub amount_withdrawn: TokenAmount,
 }
 
-#[derive(Serialize_tuple, Deserialize_tuple)]
+#[derive(Serialize_tuple, Deserialize_tuple, Debug, Clone, Eq, PartialEq)]
+pub struct GetBalanceReturn {
+    pub balance: TokenAmount,
+    pub locked: TokenAmount,
+}
+
+#[derive(Serialize_tuple, Deserialize_tuple, Debug, Clone, Eq, PartialEq)]
 pub struct OnMinerSectorsTerminateParams {
     pub epoch: ChainEpoch,
     pub deal_ids: Vec<DealID>,
 }
 
-#[derive(Serialize_tuple)]
-
-pub struct OnMinerSectorsTerminateParamsRef<'a> {
-    pub epoch: ChainEpoch,
-    pub deal_ids: &'a [DealID],
-}
-
-#[derive(Serialize_tuple, Deserialize_tuple)]
+#[derive(Serialize_tuple, Deserialize_tuple, Debug, Clone, Eq, PartialEq)]
 pub struct PublishStorageDealsParams {
     pub deals: Vec<ClientDealProposal>,
 }
 
 impl Cbor for PublishStorageDealsParams {}
 
-#[derive(Serialize_tuple, Deserialize_tuple, Debug)]
+#[derive(Serialize_tuple, Deserialize_tuple, Debug, Clone, PartialEq)] // Add Eq when BitField does
 pub struct PublishStorageDealsReturn {
     pub ids: Vec<DealID>,
     pub valid_deals: BitField,
@@ -65,41 +64,36 @@ pub struct PublishStorageDealsReturn {
 // Changed since V2:
 // - Array of Sectors rather than just one
 // - Removed SectorStart
-#[derive(Serialize_tuple, Deserialize_tuple)]
+#[derive(Serialize_tuple, Deserialize_tuple, Debug, Clone, Eq, PartialEq)]
 pub struct VerifyDealsForActivationParams {
     pub sectors: Vec<SectorDeals>,
 }
 
-#[derive(Serialize_tuple, Deserialize_tuple)]
+#[derive(Serialize_tuple, Deserialize_tuple, Debug, Clone, Eq, PartialEq)]
 pub struct SectorDeals {
     pub sector_type: RegisteredSealProof,
     pub sector_expiry: ChainEpoch,
     pub deal_ids: Vec<DealID>,
 }
 
-#[derive(Serialize_tuple)]
-pub struct VerifyDealsForActivationParamsRef<'a> {
-    pub sectors: &'a [SectorDeals],
-}
-
-#[derive(Serialize_tuple, Deserialize_tuple, Default)]
+#[derive(Serialize_tuple, Deserialize_tuple, Debug, Clone, Eq, PartialEq)]
 pub struct VerifyDealsForActivationReturn {
     pub sectors: Vec<SectorDealData>,
 }
 
-#[derive(Serialize_tuple, Deserialize_tuple, Default, Clone)]
+#[derive(Serialize_tuple, Deserialize_tuple, Debug, Clone, Eq, PartialEq, Default)]
 pub struct SectorDealData {
     /// Option::None signifies commitment to empty sector, meaning no deals.
     pub commd: Option<Cid>,
 }
 
-#[derive(Serialize_tuple, Deserialize_tuple)]
+#[derive(Serialize_tuple, Deserialize_tuple, Debug, Clone, Eq, PartialEq)]
 pub struct ActivateDealsParams {
     pub deal_ids: Vec<DealID>,
     pub sector_expiry: ChainEpoch,
 }
 
-#[derive(Serialize_tuple, Deserialize_tuple, Clone)]
+#[derive(Serialize_tuple, Deserialize_tuple, Debug, Clone, Eq, PartialEq)]
 pub struct VerifiedDealInfo {
     pub client: ActorID,
     pub allocation_id: AllocationID,
@@ -107,13 +101,13 @@ pub struct VerifiedDealInfo {
     pub size: PaddedPieceSize,
 }
 
-#[derive(Serialize_tuple, Deserialize_tuple)]
+#[derive(Serialize_tuple, Deserialize_tuple, Debug, Clone, Eq, PartialEq)]
 pub struct ActivateDealsResult {
     #[serde(with = "bigint_ser")]
     pub nonverified_deal_space: BigInt,
     pub verified_infos: Vec<VerifiedDealInfo>,
 }
-#[derive(Serialize_tuple, Deserialize_tuple, Debug, Clone, Default)]
+#[derive(Serialize_tuple, Deserialize_tuple, Debug, Clone, Eq, PartialEq)]
 pub struct DealSpaces {
     #[serde(with = "bigint_ser")]
     pub deal_space: BigInt,
@@ -121,17 +115,12 @@ pub struct DealSpaces {
     pub verified_deal_space: BigInt,
 }
 
-#[derive(Serialize_tuple, Deserialize_tuple)]
+#[derive(Serialize_tuple, Deserialize_tuple, Debug, Clone, Eq, PartialEq)]
 pub struct ComputeDataCommitmentParams {
     pub inputs: Vec<SectorDataSpec>,
 }
 
-#[derive(Serialize_tuple)]
-pub struct ComputeDataCommitmentParamsRef<'a> {
-    pub inputs: &'a [SectorDataSpec],
-}
-
-#[derive(Serialize_tuple, Deserialize_tuple)]
+#[derive(Serialize_tuple, Deserialize_tuple, Debug, Clone, Eq, PartialEq)]
 pub struct ComputeDataCommitmentReturn {
     pub commds: Vec<Cid>,
 }
@@ -142,7 +131,7 @@ pub type DealArray<'bs, BS> = Array<'bs, DealProposal, BS>;
 /// A specialization of a array to deals.
 pub type DealMetaArray<'bs, BS> = Array<'bs, DealState, BS>;
 
-#[derive(Serialize_tuple, Deserialize_tuple)]
+#[derive(Serialize_tuple, Deserialize_tuple, Debug, Clone, Eq, PartialEq)]
 pub struct SectorDataSpec {
     pub deal_ids: Vec<DealID>,
     pub sector_type: RegisteredSealProof,
