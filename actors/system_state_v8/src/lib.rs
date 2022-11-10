eb8cd21dd1595dd7092b9a598351c3d79bb32a9b
sudo-shashank <shashank@chainsafe.io>
2022-11-10 19:07:21 +0530

    Added system actor v8

---
+++ actors/system_state_v8/src/lib.rs
@@ -0,0 +1,126 @@
+// Copyright 2019-2022 ChainSafe Systems
+// SPDX-License-Identifier: Apache-2.0, MIT
+use anyhow::anyhow;
+use cid::{multihash, Cid};
+use fvm_ipld_blockstore::Blockstore;
+use fvm_ipld_encoding::tuple::*;
+use fvm_ipld_encoding::CborStore;
+use fvm_ipld_encoding::{Cbor, RawBytes};
+use fvm_shared::error::ExitCode;
+use fvm_shared::{MethodNum, METHOD_CONSTRUCTOR};
+use num_derive::FromPrimitive;
+use num_traits::FromPrimitive;
+
+use fil_actors_runtime_common::runtime::{ActorCode, Runtime};
+use fil_actors_runtime_common::{actor_error, ActorDowncast, ActorError, SYSTEM_ACTOR_ADDR};
+
+#[cfg(feature = "fil-actor")]
+fil_actors_runtime_common::wasm_trampoline!(Actor);
+
+// * Updated to specs-actors commit: 845089a6d2580e46055c24415a6c32ee688e5186 (v3.0.0)
+
+/// System actor methods.
+#[derive(FromPrimitive)]
+#[repr(u64)]
+pub enum Method {
+    Constructor = METHOD_CONSTRUCTOR,
+}
+
+/// System actor state.
+#[derive(Default, Deserialize_tuple, Serialize_tuple)]
+pub struct State {
+    // builtin actor registry: Vec<(String, Cid)>
+    pub builtin_actors: Cid,
+}
+impl Cbor for State {}
+
+impl State {
+    pub fn new<BS: Blockstore>(store: &BS) -> anyhow::Result<Self> {
+        let c = store
+            .put_cbor(&Vec::<(String, Cid)>::new(), multihash::Code::Blake2b256)
+            .map_err(|e| anyhow!("failed to put system state to store: {}", e))?;
+        Ok(Self { builtin_actors: c })
+    }
+
+    pub fn get_builtin_actors<B: Blockstore>(
+        &self,
+        store: &B,
+    ) -> Result<Vec<(String, Cid)>, String> {
+        match store.get_cbor(&self.builtin_actors) {
+            Ok(Some(obj)) => Ok(obj),
+            Ok(None) => Err("failed to load builtin actor registry; not found".to_string()),
+            Err(e) => Err(e.to_string()),
+        }
+    }
+}
+
+/// System actor.
+pub struct Actor;
+impl Actor {
+    /// System actor constructor.
+    pub fn constructor<BS, RT>(rt: &mut RT) -> Result<(), ActorError>
+    where
+        BS: Blockstore,
+        RT: Runtime<BS>,
+    {
+        rt.validate_immediate_caller_is(std::iter::once(&SYSTEM_ACTOR_ADDR))?;
+
+        let state = State::new(rt.store()).map_err(|e| {
+            e.downcast_default(ExitCode::USR_ILLEGAL_STATE, "failed to construct state")
+        })?;
+        rt.create(&state)?;
+        Ok(())
+    }
+}
+
+impl ActorCode for Actor {
+    fn invoke_method<BS, RT>(
+        rt: &mut RT,
+        method: MethodNum,
+        _params: &RawBytes,
+    ) -> Result<RawBytes, ActorError>
+    where
+        BS: Blockstore,
+        RT: Runtime<BS>,
+    {
+        match FromPrimitive::from_u64(method) {
+            Some(Method::Constructor) => {
+                Self::constructor(rt)?;
+                Ok(RawBytes::default())
+            }
+            None => Err(actor_error!(unhandled_message; "Invalid method")),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use fvm_ipld_encoding::RawBytes;
+    use fvm_shared::MethodNum;
+
+    use fil_actors_runtime_common::test_utils::{MockRuntime, SYSTEM_ACTOR_CODE_ID};
+    use fil_actors_runtime_common::SYSTEM_ACTOR_ADDR;
+
+    use crate::{Actor, Method, State};
+
+    pub fn new_runtime() -> MockRuntime {
+        MockRuntime {
+            receiver: SYSTEM_ACTOR_ADDR,
+            caller: SYSTEM_ACTOR_ADDR,
+            caller_type: *SYSTEM_ACTOR_CODE_ID,
+            ..Default::default()
+        }
+    }
+
+    #[test]
+    fn construct_with_root_id() {
+        let mut rt = new_runtime();
+        rt.expect_validate_caller_addr(vec![SYSTEM_ACTOR_ADDR]);
+        rt.set_caller(*SYSTEM_ACTOR_CODE_ID, SYSTEM_ACTOR_ADDR);
+        rt.call::<Actor>(Method::Constructor as MethodNum, &RawBytes::default()).unwrap();
+
+        let state: State = rt.get_state();
+        let builtin_actors = state.get_builtin_actors(&rt.store).unwrap();
+        assert!(builtin_actors.is_empty());
+    }
+}
