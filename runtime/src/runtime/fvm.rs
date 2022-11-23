385e9324a7fb919a80762edf484019a035d62aab
vyzo <vyzo@hackzen.org>
2022-11-23 05:34:57 +0200

    fix runtime trampoline to propagate data from errors

--- runtime/src/runtime/fvm.rs
+++ runtime/src/runtime/fvm.rs
@@ -639,7 +639,7 @@ pub fn trampoline<C: ActorCode>(params: u32) -> u32 {
     let mut rt = FvmRuntime::default();
     // Invoke the method, aborting if the actor returns an errored exit code.
     let ret = C::invoke_method(&mut rt, method, &params)
-        .unwrap_or_else(|err| fvm::vm::abort(err.exit_code().value(), Some(err.msg())));
+        .unwrap_or_else(|err| fvm::vm::exit(err.exit_code().value(), err.data(), Some(err.msg())));
 
     // Abort with "assertion failed" if the actor failed to validate the caller somewhere.
     // We do this after handling the error, because the actor may have encountered an error before
