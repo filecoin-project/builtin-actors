265948cabf480c58803be2319328ab3edaa5a55f
Melanie Riise <me@mriise.net>
2022-11-23 16:42:25 -0800

    EVM: Unit tests for some math opcodes (#860)
    * add unit tests for some opcodes
    
    ADD MUL SUB DIV
    
    * fmt

--- actors/evm/src/interpreter/instructions/arithmetic.rs
+++ actors/evm/src/interpreter/instructions/arithmetic.rs
@@ -1,3 +1,11 @@
+//! ## Implementer's notes
+//!
+//! All operations are done with overflowing math
+//! TODO (simple?) add, mul, sub div
+//!
+//! ### Non-critical TODOs
+//! Many operations can simply mutate the last value on stack instead of pop/push.
+
 use {crate::interpreter::stack::Stack, crate::interpreter::uints::*, crate::interpreter::U256};
 
 #[inline]
@@ -25,6 +33,8 @@ pub fn sub(stack: &mut Stack) {
 pub fn div(stack: &mut Stack) {
     let a = stack.pop();
     let b = stack.get_mut(0);
+    // TODO shortcut optimizations from go's lib (our doesn't)
+    // https://github.com/holiman/uint256/blob/6f8ccba90ce6cba9727ad5aa26bb925a25b50d29/uint256.go#L544
     if !b.is_zero() {
         *b = a / *b
     }
@@ -124,6 +134,123 @@ pub fn exp(stack: &mut Stack) {
 
 #[cfg(test)]
 mod test {
+
+    mod basic {
+        use crate::interpreter::{stack::Stack, U256};
+
+        // all operations go a <op> b
+        fn push_2(s: &mut Stack, a: impl Into<U256>, b: impl Into<U256>) {
+            s.push(b.into());
+            s.push(a.into());
+        }
+
+        fn expect_stack_value(s: &mut Stack, e: impl Into<U256>, comment: impl AsRef<str>) {
+            let mut expected = Stack::new();
+            expected.push(e.into());
+
+            // stacks should be _exactly_ the same
+            assert_eq!(s.0, expected.0, "{}", comment.as_ref());
+            s.pop();
+        }
+
+        #[test]
+        fn add() {
+            let mut s = Stack::default();
+            let s = &mut s;
+
+            push_2(s, 0, 0);
+            super::add(s);
+            expect_stack_value(s, 0, "add nothing to nothing");
+
+            // does "math" on all limbs, so it is different than above
+            push_2(s, U256::max_value(), 0);
+            super::add(s);
+            expect_stack_value(s, U256::max_value(), "add nothing to max value");
+
+            push_2(s, 2, 2);
+            super::add(s);
+            expect_stack_value(s, 4, "2 plus 2 equals 5 (???)");
+
+            push_2(s, u64::MAX, 32);
+            super::add(s);
+            expect_stack_value(s, u64::MAX as u128 + 32, "add 32 past a single (u64) limb of u256");
+
+            // wrap to zero
+            push_2(s, U256::max_value(), 1);
+            super::add(s);
+            expect_stack_value(s, 0, "overflow by one");
+
+            // wrap all limbs
+            push_2(s, U256::max_value(), U256::max_value());
+            super::add(s);
+            expect_stack_value(s, U256::max_value() - 1, "overflow by max, should be 2^256-1");
+        }
+
+        #[test]
+        fn mul() {
+            let mut s = Stack::default();
+            let s = &mut s;
+
+            push_2(s, 0, 0);
+            super::mul(s);
+            expect_stack_value(s, 0, "multiply nothing by nothing");
+
+            push_2(s, 2, 3);
+            super::mul(s);
+            expect_stack_value(s, 6, "multiply 2 by 3");
+
+            push_2(s, u64::MAX, 2);
+            super::mul(s);
+            expect_stack_value(s, (u64::MAX as u128) * 2, "2^64 x 2");
+        }
+
+        #[test]
+        fn sub() {
+            let mut s = Stack::default();
+            let s = &mut s;
+
+            push_2(s, 0, 0);
+            super::sub(s);
+            expect_stack_value(s, 0, "subtract nothing by nothing");
+
+            push_2(s, 2, 1);
+            super::sub(s);
+            expect_stack_value(s, 1, "subtract 2 by 1");
+
+            push_2(s, (u64::MAX as u128) + 32, 64);
+            super::sub(s);
+            expect_stack_value(s, u64::MAX - 32, "subtract 64 from a value 32 over a single limb");
+
+            // wrap to max
+            push_2(s, 0, 1);
+            super::sub(s);
+            expect_stack_value(s, U256::max_value(), "wrap around to max by one");
+
+            // wrap all limbs
+            push_2(s, U256::max_value(), U256::max_value());
+            super::sub(s);
+            expect_stack_value(s, 0, "wrap around zero by 2^256");
+        }
+
+        #[test]
+        fn div() {
+            let mut s = Stack::default();
+            let s = &mut s;
+
+            push_2(s, 0, 0);
+            super::div(s);
+            expect_stack_value(s, 0, "divide nothing by nothing (yes)");
+
+            push_2(s, 4, 1);
+            super::div(s);
+            expect_stack_value(s, 4, "divide 4 by 1");
+
+            push_2(s, u128::MAX, 2);
+            super::div(s);
+            expect_stack_value(s, u128::MAX / 2, "divide 2^128 by 2 (uses >1 limb)");
+        }
+    }
+
     use super::*;
     #[test]
     fn test_signextend() {
