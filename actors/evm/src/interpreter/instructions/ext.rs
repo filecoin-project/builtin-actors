e0951d20053e831c5c5f852eac4bb0a48d6459a1
mriise <me@mriise.net>
2022-11-28 15:03:04 -0800

    cleanup implementation

--- actors/evm/src/interpreter/instructions/ext.rs
+++ actors/evm/src/interpreter/instructions/ext.rs
@@ -21,54 +21,30 @@ pub fn extcodesize(
     // TODO (M2.2) we're fetching the entire block here just to get its size. We should instead use
     //  the ipld::block_stat syscall, but the Runtime nor the Blockstore expose it.
     //  Tracked in https://github.com/filecoin-project/ref-fvm/issues/867
-    let len = {
-        // first try to get EVM contract len
-        match get_evm_bytecode_cid(system.rt, addr) {
-            Ok(cid) => {
-                // TODO this is part of account abstraction hack where EOAs are Embryos
-                if cid == system.rt.get_code_cid_for_type(Type::Embryo) {
-                    Ok(0)
-                } else {
-                    get_evm_bytecode(system.rt, &cid).map(|bytecode| bytecode.len())
-                }
-                
-            },
-            Err(_) => {
-                // TODO merge with util methods
-                let id = match EthAddress::from(addr).as_id() {
-                    Some(a) => Some(a),
-                    // not an eth style id address
-                    None => {
-                        // convert address to f4 & resolve
-                        let a = Address::try_from(EthAddress::from(addr))?;
-                        system
-                            .rt
-                            .resolve_address(&a)
-                    }
-                };
-                
-                if let Some(id) = id {
-                    if let Some(cid) = system.rt.get_actor_code_cid(&id) {
-                        if cid == system.rt.get_code_cid_for_type(Type::Account) {
-                            // system account has no code
-                            Ok(0)
-                        } else {
-                            // native actor code
-                            // TODO bikeshed this, needs to be at least non-zero though. 
-                            // https://github.com/filecoin-project/ref-fvm/issues/1134
-                            Ok(1)
-                        }
-                    } else {
-                        // no CID
-                        Ok(0)
-                    }
-                } else {
-                    // actor doesn't exist
-                    Ok(0)
-                }
+    let len = match get_evm_bytecode_cid(system.rt, addr)? {
+        // evm cid
+        Ok(cid) =>
+        // TODO this is part of account abstraction hack where EOAs are Embryos
+        {
+            if cid == system.rt.get_code_cid_for_type(Type::Embryo) {
+                Ok(0)
+            } else {
+                get_evm_bytecode(system.rt, &cid).map(|bytecode| bytecode.len())
             }
-        }?
-    };
+        }
+        // native cid
+        Err(cid) => {
+            if cid == system.rt.get_code_cid_for_type(Type::Account) {
+                // system account has no code (and we want solidity isContract to return false)
+                Ok(0)
+            } else {
+                // native actor code
+                // TODO bikeshed this, needs to be at least non-zero though for solidity isContract.
+                // https://github.com/filecoin-project/ref-fvm/issues/1134
+                Ok(1)
+            }
+        }
+    }?;
 
     Ok(len.into())
 }
@@ -79,7 +55,14 @@ pub fn extcodehash(
     addr: U256,
 ) -> Result<U256, StatusCode> {
     let cid = get_evm_bytecode_cid(system.rt, addr)?;
-    let digest = cid.hash().digest();
+    let digest = if let Ok(cid) = cid {
+        cid.hash().digest()
+    } else {
+        // REMOVEME: instead we could return the hash of the actor, but this may be confusing for EVM contracts
+        return Err(StatusCode::InvalidArgument(
+            "cannot invoke EXTCODEHASH for non-EVM actor".to_string(),
+        ));
+    };
     // Take the first 32 bytes of the Multihash
     let digest_len = digest.len().min(32);
     Ok(digest[..digest_len].into())
@@ -96,16 +79,20 @@ pub fn extcodecopy(
     let ExecutionState { stack, .. } = state;
 
     // TODO err trying to copy native code
-    let bytecode =
-        get_evm_bytecode_cid(system.rt, addr).and_then(|cid| get_evm_bytecode(system.rt, &cid))?;
+    let bytecode = get_evm_bytecode_cid(system.rt, addr).map(|cid| {
+        cid.map(|evm_cid| get_evm_bytecode(system.rt, &evm_cid))
+            // calling EXTCODECOPY on native actors results with a single byte 0xFE which solidtiy uses for its `assert`/`throw` methods
+            // and in general invalid EVM bytecode
+            .unwrap_or(Ok(vec![0xFE]))
+    })??;
 
     copy_to_memory(&mut state.memory, dest_offset, size, data_offset, bytecode.as_slice(), true)
 }
 
-pub fn get_evm_bytecode_cid(rt: &impl Runtime, addr: U256) -> Result<Cid, StatusCode> {
+/// Attempts to get bytecode CID of an evm contract, returning either an error or the CID of the native actor as the error  
+pub fn get_evm_bytecode_cid(rt: &impl Runtime, addr: U256) -> Result<Result<Cid, Cid>, StatusCode> {
     let addr: EthAddress = addr.into();
     let addr: Address = addr.try_into()?;
-    // TODO: just return none in most of these cases?
     let actor_id = rt.resolve_address(&addr).ok_or_else(|| {
         StatusCode::InvalidArgument("failed to resolve address".to_string())
         // TODO better error code
@@ -114,10 +101,13 @@ pub fn get_evm_bytecode_cid(rt: &impl Runtime, addr: U256) -> Result<Cid, Status
     let evm_cid = rt.get_code_cid_for_type(Type::EVM);
     let target_cid = rt.get_actor_code_cid(&actor_id);
 
-    if Some(evm_cid) != target_cid {
-        return Err(StatusCode::InvalidArgument(
-            "cannot invoke EXTCODESIZE for non-EVM actor".to_string(),
-        )); // TODO better error code
+    match target_cid {
+        Some(cid) => {
+            if cid != evm_cid {
+                return Ok(Err(cid));
+            }
+        }
+        None => (),
     }
 
     let cid = rt
