a1b28bacc4c6580fe237114b06d6d931a8d5b1dd
vyzo <vyzo@hackzen.org>
2022-12-01 18:34:59 +0200

    remove unnecessary sticky bit check

--- actors/evm/src/interpreter/instructions/call.rs
+++ actors/evm/src/interpreter/instructions/call.rs
@@ -250,7 +250,7 @@ pub fn call_generic<RT: Runtime>(
                         let params = RawBytes::serialize(BytesSer(input_data))?;
                         let value = TokenAmount::from(&value);
                         let gas_limit = if !gas.is_zero() { Some(gas.to_u64_saturating()) } else { None };
-                        let read_only = system.readonly || kind == CallKind::StaticCall;
+                        let read_only = kind == CallKind::StaticCall;
                         system.send_with_gas(&dst_addr, method, params, value, gas_limit, read_only)
                     }
                 }
