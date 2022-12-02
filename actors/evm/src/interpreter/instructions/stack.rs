63860145145c8040f0adfd72b8c2bab58177a68e
Steven Allen <steven@stebalien.com>
2022-12-02 13:45:09 -0800

    fix: right-pad when pushing at end of bytecode (#903)

--- actors/evm/src/interpreter/instructions/stack.rs
+++ actors/evm/src/interpreter/instructions/stack.rs
@@ -3,15 +3,12 @@ use {crate::interpreter::stack::Stack, crate::interpreter::U256};
 
 #[inline]
 pub(crate) unsafe fn push<const LEN: usize>(stack: &mut Stack, code: &[u8]) -> usize {
-    let pushval = &code[..LEN];
-    stack.push(match pushval.len() {
-        0 => U256::zero(),
-        32 => U256::from_big_endian(pushval),
-        _ => {
-            let mut padded = [0; 32];
-            padded[32 - pushval.len()..].copy_from_slice(pushval);
-            U256::from_big_endian(&padded)
-        }
+    stack.push(if code.len() < LEN {
+        let mut padded = [0; LEN];
+        padded[..code.len()].copy_from_slice(code);
+        U256::from_big_endian(&padded)
+    } else {
+        U256::from_big_endian(&code[..LEN])
     });
     LEN
 }
@@ -29,4 +26,15 @@ pub(crate) unsafe fn swap<const HEIGHT: usize>(stack: &mut Stack) {
 #[inline]
 pub(crate) unsafe fn pop(stack: &mut Stack) {
     stack.pop();
+}
+
+#[test]
+fn test_push_pad_right() {
+    let mut stack = Stack::new();
+    unsafe {
+        assert!(stack.ensure(1));
+        assert_eq!(push::<4>(&mut stack, &[0xde, 0xad]), 4);
+        assert_eq!(stack.len(), 1);
+        assert_eq!(stack.get(0), &U256::from(0xdead0000u64));
+    }
 }
