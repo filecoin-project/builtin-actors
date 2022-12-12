0bbd899332d448151ce6d0f64ffceade0d6e1902
zenground0 <ZenGround0@users.noreply.github.com>
2022-12-12 10:27:03 -0700

    Fix test_vm expectations for verified deals

--- test_vm/src/util.rs
+++ test_vm/src/util.rs
@@ -1152,42 +1152,45 @@ pub fn market_publish_deal(
             }],
             extensions: vec![],
         };
-        expect_publish_invocs.push(ExpectInvocation {
-            to: DATACAP_TOKEN_ACTOR_ADDR,
-            method: DataCapMethod::TransferFromExported as u64,
-            params: Some(
-                RawBytes::serialize(&TransferFromParams {
-                    from: deal_client,
-                    to: VERIFIED_REGISTRY_ACTOR_ADDR,
-                    amount: token_amount.clone(),
-                    operator_data: RawBytes::serialize(&alloc_reqs).unwrap(),
-                })
-                .unwrap(),
-            ),
-            code: Some(ExitCode::OK),
-            subinvocs: Some(vec![ExpectInvocation {
-                to: VERIFIED_REGISTRY_ACTOR_ADDR,
-                method: VerifregMethod::UniversalReceiverHook as u64,
+        expect_publish_invocs.insert(
+            expect_publish_invocs.len() - 1,
+            ExpectInvocation {
+                to: DATACAP_TOKEN_ACTOR_ADDR,
+                method: DataCapMethod::TransferFromExported as u64,
                 params: Some(
-                    RawBytes::serialize(&UniversalReceiverParams {
-                        type_: FRC46_TOKEN_TYPE,
-                        payload: RawBytes::serialize(&FRC46TokenReceived {
-                            from: deal_client.id().unwrap(),
-                            to: VERIFIED_REGISTRY_ACTOR_ADDR.id().unwrap(),
-                            operator: STORAGE_MARKET_ACTOR_ADDR.id().unwrap(),
-                            amount: token_amount,
-                            operator_data: RawBytes::serialize(&alloc_reqs).unwrap(),
-                            token_data: Default::default(),
-                        })
-                        .unwrap(),
+                    RawBytes::serialize(&TransferFromParams {
+                        from: deal_client,
+                        to: VERIFIED_REGISTRY_ACTOR_ADDR,
+                        amount: token_amount.clone(),
+                        operator_data: RawBytes::serialize(&alloc_reqs).unwrap(),
                     })
                     .unwrap(),
                 ),
                 code: Some(ExitCode::OK),
+                subinvocs: Some(vec![ExpectInvocation {
+                    to: VERIFIED_REGISTRY_ACTOR_ADDR,
+                    method: VerifregMethod::UniversalReceiverHook as u64,
+                    params: Some(
+                        RawBytes::serialize(&UniversalReceiverParams {
+                            type_: FRC46_TOKEN_TYPE,
+                            payload: RawBytes::serialize(&FRC46TokenReceived {
+                                from: deal_client.id().unwrap(),
+                                to: VERIFIED_REGISTRY_ACTOR_ADDR.id().unwrap(),
+                                operator: STORAGE_MARKET_ACTOR_ADDR.id().unwrap(),
+                                amount: token_amount,
+                                operator_data: RawBytes::serialize(&alloc_reqs).unwrap(),
+                                token_data: Default::default(),
+                            })
+                            .unwrap(),
+                        })
+                        .unwrap(),
+                    ),
+                    code: Some(ExitCode::OK),
+                    ..Default::default()
+                }]),
                 ..Default::default()
-            }]),
-            ..Default::default()
-        })
+            },
+        )
     }
     ExpectInvocation {
         to: STORAGE_MARKET_ACTOR_ADDR,
