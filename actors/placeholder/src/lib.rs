e02867157acdd3d97045d50f0fd33c59a3afb1b6
vyzo <vyzo@hackzen.org>
2023-01-12 21:21:41 +0200

    fix embry method error message

--- actors/placeholder/src/lib.rs
+++ actors/placeholder/src/lib.rs
@@ -3,6 +3,6 @@
 pub extern "C" fn invoke(_: u32) -> u32 {
     fvm_sdk::vm::abort(
         fvm_shared::error::ExitCode::USR_UNHANDLED_MESSAGE.value(),
-        Some("placeholder actors may only receive messages on method 0"),
+        Some("there is no contract deployed at this address; placeholder actors may only receive value transfers with method 0"),
     )
 }
