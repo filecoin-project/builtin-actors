02df0ad5219c6247fc93edab1dcc8545d3da9337
Melanie Riise <me@mriise.net>
2023-01-19 13:13:17 -0800

    EVM: Remove get_actor_type and allow precompile lookup table to have empty slots (#1080)
    * wip
    
    * Cleanup from wip:
    - merged types
    - fix clippy (remove unsafe!)
    - remove unused fn in native type
    
    * remove get_actor_type precompile, fix clippy
    
    * use work-around to use const instead of lazy init
    
    * rewrite docs for a bit better clarity
    
    * remove need for workaround
    
    * fmt

--- actors/evm/src/interpreter/precompiles/mod.rs
+++ actors/evm/src/interpreter/precompiles/mod.rs
@@ -7,60 +7,24 @@ use substrate_bn::{CurveError, FieldError, GroupError};
 use crate::reader::OverflowError;
 
 use super::{address::EthAddress, instructions::call::CallKind, System, U256};
-
 mod evm;
 mod fvm;
 
 use evm::{blake2f, ec_add, ec_mul, ec_pairing, ec_recover, identity, modexp, ripemd160, sha256};
-use fvm::{call_actor, call_actor_id, get_actor_type, lookup_delegated_address, resolve_address};
+use fvm::{call_actor, call_actor_id, lookup_delegated_address, resolve_address};
 
-// really I'd want to have context as a type parameter, but since the table we generate must have the same types (or dyn) its messy
-type PrecompileFn<RT> = unsafe fn(*mut System<RT>, &[u8], PrecompileContext) -> PrecompileResult;
+type PrecompileFn<RT> = fn(&mut System<RT>, &[u8], PrecompileContext) -> PrecompileResult;
 pub type PrecompileResult = Result<Vec<u8>, PrecompileError>;
 
 pub const NATIVE_PRECOMPILE_ADDRESS_PREFIX: u8 = 0xFE;
 
-macro_rules! precompiles {
-    ($($precompile:ident,)*) => {
-        mod trampolines {
-            use fil_actors_runtime::runtime::Runtime;
-            use crate::System;
-            use super::{PrecompileContext, PrecompileResult};
-            $(
-                #[inline(always)]
-                pub unsafe fn $precompile<RT: Runtime>(s: *mut System<RT>, inp: &[u8], ctx: PrecompileContext) -> PrecompileResult {
-                    super::$precompile(&mut *s, inp, ctx)
-                }
-            )*
-        }
-        [
-            $(trampolines::$precompile,)*
-        ]
-    }
-}
+struct PrecompileTable<RT: Runtime, const N: usize>([Option<PrecompileFn<RT>>; N]);
 
-/// Generates a list of precompile smart contracts, index + 1 is the address.
-const fn gen_evm_precompiles<RT: Runtime>() -> [PrecompileFn<RT>; 9] {
-    precompiles! {
-        ec_recover, // 0x01 ecrecover
-        sha256,     // 0x02 SHA2-256
-        ripemd160,  // 0x03 ripemd160
-        identity,   // 0x04 identity
-        modexp,     // 0x05 modexp
-        ec_add,     // 0x06 ecAdd
-        ec_mul,     // 0x07 ecMul
-        ec_pairing, // 0x08 ecPairing
-        blake2f,    // 0x09 blake2f
-    }
-}
-
-const fn gen_native_precompiles<RT: Runtime>() -> [PrecompileFn<RT>; 5] {
-    precompiles! {
-        resolve_address,            // 0xfe00..01 resolve_address
-        lookup_delegated_address,   // 0xfe00..02 lookup_delegated_address
-        call_actor,                 // 0xfe00..03 call_actor
-        get_actor_type,             // 0xfe00..04 get_actor_type
-        call_actor_id,              // 0xfe00..05 call_actor_id
+impl<RT: Runtime, const N: usize> PrecompileTable<RT, N> {
+    /// Tries to lookup Precompile, None if empty slot or out of bounds.
+    /// Last byte of precompile address - 1 is the index.
+    fn get(&self, index: usize) -> Option<PrecompileFn<RT>> {
+        self.0.get(index).and_then(|i| i.as_ref()).copied()
     }
 }
 
@@ -74,8 +38,27 @@ pub fn is_reserved_precompile_address(addr: &EthAddress) -> bool {
 pub struct Precompiles<RT>(PhantomData<RT>);
 
 impl<RT: Runtime> Precompiles<RT> {
-    const EVM_PRECOMPILES: [PrecompileFn<RT>; 9] = gen_evm_precompiles();
-    const NATIVE_PRECOMPILES: [PrecompileFn<RT>; 5] = gen_native_precompiles();
+    /// FEVM specific precompiles (0xfe prefix)
+    const NATIVE_PRECOMPILES: PrecompileTable<RT, 5> = PrecompileTable([
+        Some(resolve_address::<RT>),          // 0xfe00..01
+        Some(lookup_delegated_address::<RT>), // 0xfe00..02
+        Some(call_actor::<RT>),               // 0xfe00..03
+        None,                                 // 0xfe00..04 DISABLED
+        Some(call_actor_id::<RT>),            // 0xfe00..05
+    ]);
+
+    /// EVM specific precompiles
+    const EVM_PRECOMPILES: PrecompileTable<RT, 9> = PrecompileTable([
+        Some(ec_recover::<RT>), // 0x01 ecrecover
+        Some(sha256::<RT>),     // 0x02 SHA2-256
+        Some(ripemd160::<RT>),  // 0x03 ripemd160
+        Some(identity::<RT>),   // 0x04 identity
+        Some(modexp::<RT>),     // 0x05 modexp
+        Some(ec_add::<RT>),     // 0x06 ecAdd
+        Some(ec_mul::<RT>),     // 0x07 ecMul
+        Some(ec_pairing::<RT>), // 0x08 ecPairing
+        Some(blake2f::<RT>),    // 0x09 blake2f
+    ]);
 
     fn lookup_precompile(addr: &EthAddress) -> Option<PrecompileFn<RT>> {
         let [prefix, _m @ .., index] = addr.0;
@@ -86,7 +69,6 @@ impl<RT: Runtime> Precompiles<RT> {
                 0x00 => Self::EVM_PRECOMPILES.get(index),
                 _ => None,
             }
-            .copied()
         } else {
             None
         }
@@ -102,7 +84,7 @@ impl<RT: Runtime> Precompiles<RT> {
     ) -> PrecompileResult {
         // First, try to call the precompile, if defined.
         let result = Self::lookup_precompile(precompile_addr)
-            .map(|precompile_fn| unsafe { precompile_fn(system, input, context) })
+            .map(|precompile_fn| precompile_fn(system, input, context))
             .transpose()?
             .unwrap_or_default();
         // Then transfer the value. We do this second because we don't want to transfer if the
@@ -187,12 +169,6 @@ pub enum NativeType {
     StorageProvider = 4,
     EVMContract = 5,
     OtherTypes = 6,
-}
-
-impl NativeType {
-    fn word_vec(self) -> Vec<u8> {
-        U256::from(self as u32).to_bytes().to_vec()
-    }
 }
 
 #[cfg(test)]
