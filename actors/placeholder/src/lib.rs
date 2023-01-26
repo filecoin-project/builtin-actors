03d2d535c2cdd3bb11fd540a882a3f94419a2afc
Steven Allen <steven@stebalien.com>
2023-01-26 08:20:54 -0800

    feat: accept all methods in the placeholder (#1112)

--- actors/placeholder/src/lib.rs
+++ actors/placeholder/src/lib.rs
@@ -1,8 +1,5 @@
 #[cfg(feature = "fil-actor")]
 #[no_mangle]
 pub extern "C" fn invoke(_: u32) -> u32 {
-    fvm_sdk::vm::abort(
-        fvm_shared::error::ExitCode::USR_UNHANDLED_MESSAGE.value(),
-        Some("there is no contract deployed at this address; placeholder actors may only receive value transfers with method 0"),
-    )
+    0
 }
