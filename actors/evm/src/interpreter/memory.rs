df8e0eca684d877b26a884abc7f637598e87481a
mriise <me@mriise.net>
2023-01-27 14:27:55 -0800

    add docs and debug assert to memory grow

--- actors/evm/src/interpreter/memory.rs
+++ actors/evm/src/interpreter/memory.rs
@@ -16,12 +16,15 @@ impl Default for Memory {
 
 impl Memory {
     #[inline]
+    /// Resizes memory to `size` length, reserving extra WASM pages as-needed. 
+    /// TODO this should be renamed resize since it can also shrink memory.
     pub fn grow(&mut self, size: usize) {
         let cap = self.0.capacity();
         if size > cap {
             let required_pages = (size + PAGE_SIZE - 1) / PAGE_SIZE;
             self.0.reserve((PAGE_SIZE * required_pages) - self.0.len());
         }
+        debug_assert_eq!(size % 32, 0, "MSIZE depends on memory length being multiple of 32");
         self.0.resize(size, 0);
     }
 }
