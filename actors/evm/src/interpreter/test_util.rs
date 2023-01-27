c9943def4379973b272f4b66cb5c9882650749b7
Melanie Riise <me@mriise.net>
2023-01-26 23:44:02 -0800

    Evm unittest fixes (#1119)
    * cleanup types and macro
    
    * cleanup
    
    * move bytecode into tester
    
    * remove types and only use macro, expose only runtime and machine to test cases
    
    * fmt & remove closure
    
    * move dep to macro

--- actors/evm/src/interpreter/test_util.rs
+++ actors/evm/src/interpreter/test_util.rs
@@ -1,62 +1,31 @@
 #![cfg(test)]
 
-use fil_actors_runtime::test_utils::*;
-use fvm_shared::econ::TokenAmount;
-
-use crate::interpreter::address::*;
-use crate::interpreter::bytecode::*;
-use crate::interpreter::execution::*;
-use crate::interpreter::output::*;
-use crate::interpreter::system::*;
-
-use bytes::Bytes;
-
-pub type TestSystem<'r> = System<'r, MockRuntime>;
-pub type TestMachine<'a, 'r> = Machine<'a, 'r, MockRuntime>;
-
-pub struct TestEnv<'r> {
-    pub system: TestSystem<'r>,
-    pub state: ExecutionState,
-    pub bytecode: Bytecode,
-}
-
-pub struct Tester<'a, 'r> {
-    pub m: TestMachine<'a, 'r>,
-    pub env: TestEnv<'r>,
-}
-
-pub fn rt() -> MockRuntime {
-    MockRuntime::default()
-}
-
-pub fn env<'r>(rt: &'r mut MockRuntime, code: Vec<u8>) -> TestEnv<'r> {
-    let system = TestSystem::new(rt, false);
-    let state = ExecutionState::new(
-        EthAddress::from_id(1000),
-        EthAddress::from_id(1000),
-        TokenAmount::from_atto(0),
-        Bytes::default(),
-    );
-    let bytecode = Bytecode::new(code);
-    TestEnv { system, state, bytecode }
-}
-
-pub fn machine<'a, 'r>(env: &'a mut TestEnv<'r>) -> TestMachine<'a, 'r> {
-    TestMachine {
-        system: &mut env.system,
-        state: &mut env.state,
-        bytecode: &env.bytecode,
-        pc: 0,
-        output: Output::default(),
-    }
-}
-
 #[macro_export]
 macro_rules! do_test {
-    ($rt:ident, $env:ident, $m:ident, $code:expr, $body:block) => {
-        let mut $rt = test_util::rt();
-        let mut $env = test_util::env(&mut $rt, $code);
-        let mut $m = test_util::machine(&mut $env);
+    ($rt:ident, $machine:ident, $code:expr, $body:block) => {
+        use ::fil_actors_runtime::test_utils::MockRuntime;
+        use ::fvm_shared::econ::TokenAmount;
+        use $crate::interpreter::{execution::Machine, system::System, Output};
+        use $crate::{Bytecode, Bytes, EthAddress, ExecutionState};
+
+        let mut $rt = MockRuntime::default();
+        let mut state = ExecutionState::new(
+            EthAddress::from_id(1000),
+            EthAddress::from_id(1000),
+            TokenAmount::from_atto(0),
+            Bytes::default(),
+        );
+
+        let mut system = System::new(&mut $rt, false);
+        let bytecode = Bytecode::new($code);
+        let mut $machine = Machine {
+            system: &mut system,
+            state: &mut state,
+            bytecode: &bytecode,
+            pc: 0,
+            output: Output::default(),
+        };
+
         $body
     };
 }
