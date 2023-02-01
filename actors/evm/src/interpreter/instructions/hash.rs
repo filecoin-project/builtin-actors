300d83fd584a8033359e3935ecfe22c975fe318e
Melanie Riise <me@mriise.net>
2023-02-01 07:43:10 -0800

    EVM: Test keccak opcode (#1132)
    test keccak

--- actors/evm/src/interpreter/instructions/hash.rs
+++ actors/evm/src/interpreter/instructions/hash.rs
@@ -26,3 +26,62 @@ pub fn keccak256(
 
     Ok(U256::from_big_endian(&buf[..size]))
 }
+
+#[cfg(test)]
+mod test {
+    use fil_actors_runtime::runtime::Primitives;
+
+    use crate::{evm_unit_test, interpreter::U256, BytecodeHash};
+
+    #[test]
+    fn keccak256_large() {
+        for i in 0..12 {
+            let len = 1 << i; // 1 through 2^11 bytes length
+            println!("{}", len);
+            let v = vec![0xff; len];
+            let [a, b] = u16::try_from(len).unwrap().to_be_bytes();
+            evm_unit_test! {
+                (rt, m) {
+                    PUSH2;
+                    {a};
+                    {b};
+                    PUSH0;
+                    KECCAK256;
+                }
+
+                let expect = &m.system.rt.hash_64(fvm_shared::crypto::hash::SupportedHashes::Keccak256, &v).0[..32];
+
+                m.state.memory.grow(len);
+                m.state.memory[..len].copy_from_slice(&v);
+                m.step().expect("execution step failed");
+                m.step().expect("execution step failed");
+                m.step().expect("execution step failed");
+
+                assert_eq!(m.state.stack.pop().unwrap(), U256::from(expect));
+            };
+        }
+    }
+
+    #[test]
+    fn keccak256_ext() {
+        for (input, expect) in
+            [([0xfe].as_slice(), BytecodeHash::NATIVE_ACTOR), (&[], BytecodeHash::EMPTY)]
+        {
+            evm_unit_test! {
+                (rt, m) {
+                    PUSH1;
+                    {input.len() as u8};
+                    PUSH0;
+                    KECCAK256;
+                }
+                m.state.memory.grow(input.len());
+                m.state.memory[..input.len()].copy_from_slice(input);
+                m.step().expect("execution step failed");
+                m.step().expect("execution step failed");
+                m.step().expect("execution step failed");
+
+                assert_eq!(m.state.stack.pop().unwrap(), U256::from(expect));
+            };
+        }
+    }
+}
