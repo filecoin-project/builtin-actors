0354dce2e4d8eee94966b269776431f96370a9fd
sudo-shashank <shashank@chainsafe.io>
2023-02-15 20:47:07 +0530

    Fix clippy

--- actors/paych/src/lib.rs
+++ actors/paych/src/lib.rs
@@ -5,7 +5,7 @@ use fil_actors_runtime::runtime::builtins::Type;
 use fil_actors_runtime::runtime::{ActorCode, Runtime};
 use fil_actors_runtime::{
     actor_dispatch, actor_error, extract_send_result, resolve_to_actor_id, ActorDowncast,
-    ActorError, Array, AsActorError,
+    ActorError, Array,
 };
 use fvm_ipld_blockstore::Blockstore;
 use fvm_ipld_encoding::CBOR;
