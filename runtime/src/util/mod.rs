ea4237171717a76d798b30cc483f14fed0a1c158
Alex North <445306+anorth@users.noreply.github.com>
2023-09-01 08:24:52 +1000

    Compute batch returns for direct onboarding methods (#1393)

--- runtime/src/util/mod.rs
+++ runtime/src/util/mod.rs
@@ -1,9 +1,7 @@
 // Copyright 2019-2022 ChainSafe Systems
 // SPDX-License-Identifier: Apache-2.0, MIT
 
-pub use self::batch_return::BatchReturn;
-pub use self::batch_return::BatchReturnGen;
-pub use self::batch_return::FailCode;
+pub use self::batch_return::*;
 pub use self::downcast::*;
 pub use self::map::*;
 pub use self::mapmap::MapMap;
