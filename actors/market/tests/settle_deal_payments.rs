2dab472b1b63672c703e84291c10369cdbdd6ab3
Alex Su <alexanderytsu@gmail.com>
2023-09-05 01:29:01 +1000

    add test for manual settlement of legacy deals

--- actors/market/tests/settle_deal_payments.rs
+++ actors/market/tests/settle_deal_payments.rs
@@ -9,7 +9,7 @@ mod harness;
 use harness::*;
 use regex::Regex;
 
-const START_EPOCH: ChainEpoch = 50;
+const START_EPOCH: ChainEpoch = 0;
 const END_EPOCH: ChainEpoch = START_EPOCH + 200 * EPOCHS_IN_DAY;
 
 #[test]
@@ -58,4 +58,58 @@ fn timedout_deal_is_slashed_and_deleted() {
     cron_tick(&rt);
 
     check_state(&rt);
+}
+
+// TODO: Revisit and cleanup https://github.com/filecoin-project/builtin-actors/issues/1389
+#[test]
+fn can_manually_settle_deals_in_the_cron_queue() {
+    let rt = setup();
+    let addrs = MinerAddresses::default();
+    // create a legacy deal that is managed by cron
+    let deal_id = publish_and_activate_deal_legacy(
+        &rt,
+        CLIENT_ADDR,
+        &addrs,
+        START_EPOCH,
+        END_EPOCH,
+        0,
+        END_EPOCH,
+    );
+    let deal_proposal = get_deal_proposal(&rt, deal_id);
+
+    let client_before = get_balance(&rt, &CLIENT_ADDR);
+    let provider_before = get_balance(&rt, &addrs.provider);
+
+    // advance to some epoch while the deal is active
+    rt.set_epoch(START_EPOCH + 100);
+
+    // manually call settle_deal_payments
+    let ret = settle_deal_payments(&rt, addrs.provider, vec![deal_id]);
+    let payment = ret.settlements[0].payment.clone();
+    assert_eq!(&payment, &(&deal_proposal.storage_price_per_epoch * 100));
+
+    // assert incremental payment was performed correctly
+    let incremental_client_escrow = &client_before.balance - &payment;
+    let incremental_provider_escrow = &provider_before.balance + &payment;
+    let client_updated = get_balance(&rt, &CLIENT_ADDR);
+    let provider_updated = get_balance(&rt, &addrs.provider);
+    assert_eq!(&client_updated.balance, &incremental_client_escrow);
+    assert_eq!(&provider_updated.balance, &incremental_provider_escrow);
+
+    // advance to deal end epoch and call cron
+    rt.set_epoch(END_EPOCH);
+    cron_tick(&rt);
+
+    // payments were calculated correctly, accounting for incremental payment already made
+    let total_duration = END_EPOCH - START_EPOCH;
+    let total_payment = &deal_proposal.storage_price_per_epoch * total_duration;
+    let final_client_escrow = &client_before.balance - &total_payment;
+    let final_provider_escrow = &provider_before.balance + &total_payment;
+    let client_after = get_balance(&rt, &CLIENT_ADDR);
+    let provider_after = get_balance(&rt, &addrs.provider);
+    assert_eq!(&client_after.balance, &final_client_escrow);
+    assert_eq!(&provider_after.balance, &final_provider_escrow);
+
+    // cleaned up by cron
+    assert_deal_deleted(&rt, deal_id, deal_proposal)
 }
