81b39e54e0be3bcd04f310bcdb784fc6da42839f
nemo <nemo@protocol.ai>
2023-09-08 12:57:44 -0400

    style: rust fmt

--- actors/miner/tests/miner_actor_test_commitment.rs
+++ actors/miner/tests/miner_actor_test_commitment.rs
@@ -505,8 +505,7 @@ mod miner_actor_test_commitment {
                 ),
             );
             rt.reset();
-            precommit_params.seal_proof =
-                RegisteredSealProof::StackedDRG32GiBV1P1;
+            precommit_params.seal_proof = RegisteredSealProof::StackedDRG32GiBV1P1;
             h.pre_commit_sector_and_get(
                 &rt,
                 precommit_params,
