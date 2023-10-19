28355ca7f8b85653076d773e8df9c5f7bc9697e8
Aarsh Shah <aarshkshah1992@gmail.com>
2023-10-19 12:34:47 +0400

    rustfmt

--- integration_tests/src/tests/multisig_test.rs
+++ integration_tests/src/tests/multisig_test.rs
@@ -57,9 +57,7 @@ pub fn proposal_hash_test(v: &dyn VM) {
         params: RawBytes::default(),
     };
 
-
     let wrong_hash = compute_proposal_hash(&wrong_tx, v.primitives()).unwrap();
-
 
     let wrong_approval_params = TxnIDParams { id: TxnID(0), proposal_hash: wrong_hash.to_vec() };
     apply_code(
