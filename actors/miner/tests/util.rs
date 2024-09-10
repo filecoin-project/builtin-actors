22cc3c8680b96bc666c13d6229ab134a6c5f1b13
David Himmelstrup <david.himmelstrup@chainsafe.io>
2024-09-10 14:06:07 +0200

    remove unnecessary TODO items

--- actors/miner/tests/util.rs
+++ actors/miner/tests/util.rs
@@ -769,7 +769,6 @@ impl ActorHarness {
             quality_adj_power: self.network_qa_power.clone(),
             pledge_collateral: self.network_pledge.clone(),
             quality_adj_power_smoothed: self.epoch_qa_power_smooth.clone(),
-            // TODO: maybe this is where we setup the inputs to test different values of epoch and ramp_start to test update to pledge function
             ramp_start_epoch: 0,
             ramp_duration_epochs: 0,
         };
@@ -2338,8 +2337,6 @@ impl ActorHarness {
             &self.epoch_reward_smooth,
             &self.epoch_qa_power_smooth,
             &rt.total_fil_circ_supply(),
-            // TODO: zeros preserve original functionality, but we may need to update it here
-            //   to test the new pledge function properly
             0,
             0,
         )
