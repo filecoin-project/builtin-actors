8e2ba3de9bcba0d9b755b5e34d5e66b03fc34390
Rod Vagg <rod@vagg.org>
2024-12-05 17:00:13 +1100

    undo dead code removal

--- actors/evm/src/interpreter/precompiles/evm.rs
+++ actors/evm/src/interpreter/precompiles/evm.rs
@@ -498,7 +498,10 @@ mod tests {
             )
             .unwrap();
             let res = ec_add(&mut system, &input, PrecompileContext::default());
-            assert!(matches!(res, Err(PrecompileError::EcErr)));
+            assert!(matches!(
+                res,
+                Err(PrecompileError::EcErr(substrate_bn::CurveError::NotMember))
+            ));
         }
 
         const TESTDATA_PATH: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/precompile-testdata/");
@@ -606,7 +609,10 @@ mod tests {
             )
             .unwrap();
             let res = ec_mul(&mut system, &input, PrecompileContext::default());
-            assert!(matches!(res, Err(PrecompileError::EcErr)));
+            assert!(matches!(
+                res,
+                Err(PrecompileError::EcErr(substrate_bn::CurveError::NotMember))
+            ));
 
             let input = hex::decode(
                 "\
@@ -690,7 +696,10 @@ mod tests {
             )
             .unwrap();
             let res = ec_pairing(&mut system, &input, PrecompileContext::default());
-            assert!(matches!(res, Err(PrecompileError::EcErr)));
+            assert!(matches!(
+                res,
+                Err(PrecompileError::EcErr(substrate_bn::CurveError::NotMember))
+            ));
             // invalid input length
             let input = hex::decode(
                 "\
