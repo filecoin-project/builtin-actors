ac634245c9f7190e192e1096c76e241a1b38c7ac
Rod Vagg <rod@vagg.org>
2025-02-24 14:28:34 +1100

    fip-0100: make miner cron test harness less intelligent
    now needs explicit instructions on how much total burn, pledge change and
    power change it should expect

--- actors/miner/tests/deadline_cron.rs
+++ actors/miner/tests/deadline_cron.rs
@@ -16,7 +16,7 @@ use std::ops::Neg;
 mod util;
 use crate::util::*;
 
-// an expriration ~10 days greater than effective min expiration taking into account 30 days max
+// an expiration ~10 days greater than effective min expiration taking into account 30 days max
 // between pre and prove commit
 const DEFAULT_SECTOR_EXPIRATION: ChainEpoch = 220;
 
@@ -78,15 +78,22 @@ fn test_vesting_on_cron() {
         let st = h.get_state(&rt);
         rt.set_epoch(new_epoch);
         let new_deadline_info = st.deadline_info(rt.policy(), new_epoch + 1);
+        let vesting_now = immediately_vesting_funds(&rt, &st);
         let old_locked = st.locked_funds;
         h.on_deadline_cron(
             &rt,
-            CronConfig { expected_enrollment: new_deadline_info.last(), ..CronConfig::default() },
+            CronConfig {
+                pledge_delta: -vesting_now.clone(),
+                expected_enrollment: new_deadline_info.last(),
+                ..CronConfig::default()
+            },
         );
         let new_locked = h.get_state(&rt).locked_funds;
         if should_vest {
+            assert!(vesting_now.is_positive());
             assert_ne!(old_locked, new_locked);
         } else {
+            assert!(vesting_now.is_zero());
             assert_eq!(old_locked, new_locked);
         }
     };
@@ -143,8 +150,8 @@ fn sector_expires() {
         &rt,
         CronConfig {
             no_enrollment: true,
-            expired_sectors_power_delta: Some(power_delta),
-            expired_sectors_pledge_delta: initial_pledge.neg(),
+            power_delta: Some(power_delta),
+            pledge_delta: initial_pledge.neg(),
             ..CronConfig::default()
         },
     );
@@ -199,9 +206,9 @@ fn sector_expires_and_repays_fee_debt() {
         &rt,
         CronConfig {
             no_enrollment: true,
-            expired_sectors_power_delta: Some(power_delta),
-            expired_sectors_pledge_delta: initial_pledge.neg(),
-            repaid_fee_debt: initial_pledge.clone(),
+            power_delta: Some(power_delta),
+            burnt_funds: initial_pledge.clone(),
+            pledge_delta: initial_pledge.neg(),
             ..CronConfig::default()
         },
     );
@@ -229,7 +236,6 @@ fn detects_and_penalizes_faults() {
 
     let total_power = &unproven_power + &active_power;
     let all_sectors = [active_sectors.clone(), unproven_sectors].concat();
-    let daily_fee = daily_fee_for_sectors(&all_sectors);
 
     // add lots of funds so penalties come from vesting funds
     h.apply_rewards(&rt, BIG_REWARDS.clone(), TokenAmount::zero());
@@ -244,12 +250,14 @@ fn detects_and_penalizes_faults() {
     }
 
     // Skip to end of the deadline, cron detects sectors as faulty
+    let mut fee_payable = daily_fee_for_sectors(&all_sectors);
     let active_power_delta = active_power.neg();
     h.advance_deadline(
         &rt,
         CronConfig {
-            daily_fee: daily_fee.clone(),
-            detected_faults_power_delta: Some(active_power_delta),
+            burnt_funds: fee_payable.clone(),
+            pledge_delta: -fee_payable.clone(),
+            power_delta: Some(active_power_delta),
             ..Default::default()
         },
     );
@@ -284,10 +292,15 @@ fn detects_and_penalizes_faults() {
         &h.epoch_qa_power_smooth,
         &ongoing_pwr.qa,
     );
+    fee_payable += ongoing_penalty;
 
     h.advance_deadline(
         &rt,
-        CronConfig { daily_fee, continued_faults_penalty: ongoing_penalty, ..Default::default() },
+        CronConfig {
+            burnt_funds: fee_payable.clone(),
+            pledge_delta: -fee_payable,
+            ..Default::default()
+        },
     );
 
     // recorded faulty power is unchanged
@@ -344,9 +357,10 @@ fn test_cron_run_trigger_faults() {
     h.on_deadline_cron(
         &rt,
         CronConfig {
-            daily_fee,
+            burnt_funds: daily_fee.clone(),
+            pledge_delta: daily_fee.neg(),
+            power_delta: Some(power_delta_claim),
             expected_enrollment: next_cron,
-            detected_faults_power_delta: Some(power_delta_claim),
             ..CronConfig::default()
         },
     );
