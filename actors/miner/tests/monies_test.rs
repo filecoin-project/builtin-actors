f40b2baeecf66ef4b55a0ef93db818befe691f71
Hubert Bugaj <lesny.rumcajs+github@gmail.com>
2025-03-03 14:19:36 +0100

    test: `pledge_penalty_for_continued_fault` test

--- actors/miner/tests/monies_test.rs
+++ actors/miner/tests/monies_test.rs
@@ -1,6 +1,9 @@
 use fil_actor_miner::{expected_reward_for_power, pledge_penalty_for_continued_fault};
 use fil_actors_runtime::reward::FilterEstimate;
-use fvm_shared::bigint::{BigInt, Zero};
+use fvm_shared::{
+    bigint::{BigInt, Zero},
+    econ::TokenAmount,
+};
 use std::ops::Neg;
 
 #[test]
@@ -29,4 +32,29 @@ fn zero_power_means_zero_fault_penalty() {
     let penalty_for_zero_power_faulted =
         pledge_penalty_for_continued_fault(&reward_estimate, &power_estimate, &zero_qa_power);
     assert!(penalty_for_zero_power_faulted.is_zero());
+}
+
+// Test case introduced in FIP-0098.
+// `pledge_penalty_for_continued_fault` should work for aggregate power numbers above the possible QA power for any single sector. For instance, an aggregate of 10 sectors' power should return the same end result as summing the `pledge_penalty_for_continued_fault` of each sector individually.
+#[test]
+fn aggregate_power_pledge_penalty_for_continued_fault() {
+    let epoch_target_reward = BigInt::from(1_u64 << 50);
+    let network_qa_power = BigInt::from(1_u64 << 10);
+    let power_rate_of_change = BigInt::from(1_u64 << 10);
+    let reward_estimate = FilterEstimate::new(epoch_target_reward, BigInt::zero());
+    let power_estimate = FilterEstimate::new(network_qa_power, power_rate_of_change);
+
+    let multiple_sectors = 10;
+    let qa_power = BigInt::from(1_u64 << 36) * 10;
+
+    let aggregate_penalty = pledge_penalty_for_continued_fault(
+        &reward_estimate,
+        &power_estimate,
+        &(&qa_power * multiple_sectors),
+    );
+
+    let individual_penalties = multiple_sectors
+        * pledge_penalty_for_continued_fault(&reward_estimate, &power_estimate, &qa_power);
+    assert!(aggregate_penalty > TokenAmount::zero());
+    assert_eq!(aggregate_penalty, individual_penalties);
 }
